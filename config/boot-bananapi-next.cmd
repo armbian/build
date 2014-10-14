bd300d3577f2e2e275f2318081b20dfc5b165e82
Igor Pečovnik <igorpecovnik@users.noreply.github.com>
2014-10-14 10:41:16 +0200

    Update boot-bananapi-next.cmd

--- config/boot-bananapi-next.cmd
+++ config/boot-bananapi-next.cmd
@@ -1,5 +1,4 @@
-setenv bootargs console=ttyS0,115200 sunxi_g2d_mem_reserve=0 sunxi_ve_mem_reserve=0  sunxi_no_mali_mem_reserve r
-oot=/dev/mmcblk0p1 rootwait panic=10 ${extra}
+setenv bootargs console=ttyS0,115200 sunxi_g2d_mem_reserve=0 sunxi_ve_mem_reserve=0  sunxi_no_mali_mem_reserve root=/dev/mmcblk0p1 rootwait panic=10 ${extra}
 ext4load mmc 0 0x46000000 /boot/uImage
 ext4load mmc 0 0x49000000 /boot/sun7i-a20-bananapi.dtb
 env set fdt_high ffffffff
