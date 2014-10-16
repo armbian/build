c902c8379c5a7b6a95fd9002449275c9b606cae3
Igor Pečovnik <igorpecovnik@users.noreply.github.com>
2014-10-16 20:49:23 +0200

    Update sata-install.sh

--- scripts/sata-install.sh
+++ scripts/sata-install.sh
@@ -2,11 +2,19 @@
 #
 # Configuration
 
-DEST=/dev/sda1
+DEST=sda1
 FORMAT=yes
 
 #
 clear_console
+
+
+
+if [[ -z $(cat /proc/partitions | grep $DEST) ]]; then 
+	echo "Device not partitioned";
+	exit 0
+fi
+
 figlet -f banner "warning"
 #echo "Edit file !!!"; exit 0; # DELETE OR COMMENT THIS LINE TO CONTINUE
 #
@@ -19,6 +27,8 @@ if [ $(id -u) != "0" ]; then
     echo "Error: You must be root to run this script."
     exit 1
 fi
+
+MODEL=$(hdparm -i /dev/sda | perl -n -e 'print "$1\n" if (m/model=(.+?),/i);')
 
 cat > .install-exclude <<EOF
 /dev/*
@@ -34,9 +44,9 @@ EOF
 
 clear_console
 figlet -f banner "warning"
-echo "
+echo -e "
 
-This script might erase your Hard drive and copy content of SD card to it
+This script might erase your hard drive \e[31m($MODEL)\e[39m and copy content of SD card to it
 
 "
 
@@ -51,18 +61,22 @@ fi
 
 if [ "$FORMAT" == "yes" ]
 then
-  mkfs.ext4 $DEST
+  mkfs.ext4 /dev/$DEST
 fi
 
-mount $DEST /mnt
+mount /dev/$DEST /mnt
 
-sed -e 's,root=\/dev\/mmcblk0p1,root='"$DEST"',g' -i /boot/uEnv.ct
-sed -e 's,root=\/dev\/mmcblk0p1,root='"$DEST"',g' -i /boot/uEnv.cb2
+if [ -f "/boot/uEnv.ct" ]; then sed -e 's,root=\/dev\/mmcblk0p1,root='"\/dev\/$DEST"',g' -i /boot/uEnv.ct ; fi
+if [ -f "/boot/uEnv.cb2" ]; then sed -e 's,root=\/dev\/mmcblk0p1,root='"\/dev\/$DEST"',g' -i /boot/uEnv.cb2; fi
+if [ -f "/boot/uEnv.txt" ]; then sed -e 's,root=\/dev\/mmcblk0p1,root='"\/dev\/$DEST"',g' -i /boot/uEnv.txt; fi
 
-echo "Creating hard drive rootfs ... up to 5 min"
+
+echo "Creating hard drive rootfs ... can take several minutes to finish!"
 rsync -aH --exclude-from=.install-exclude  /  /mnt
 # change fstab
-sed -e 's,\/dev\/mmcblk0p1,'"$DEST"',g' -i /mnt/etc/fstab
+sed -e 's,\/dev\/mmcblk0p1,'"\/dev/$DEST"',g' -i /mnt/etc/fstab
+sed -i "s/data=writeback,//" /mnt/etc/fstab
+
 umount /mnt
 figlet -f banner "warning"
 echo "All done. Press a key to reboot! System needs SD card for boot process! Can't boot directly from hard drive"
