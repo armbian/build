0e86ebb8ef8a8659e27674e71ab5b70bf4f4787f
Igor Pecovnik <igor.pecovnik@gmail.com>
2015-04-20 07:01:47 +0200

    Dual kernel boot script

--- config/boot.cmd
+++ config/boot.cmd
@@ -1,4 +1,21 @@
 setenv bootargs console=tty1 root=/dev/mmcblk0p1 rootwait sunxi_ve_mem_reserve=0 sunxi_g2d_mem_reserve=0 sunxi_no_mali_mem_reserve sunxi_fb_mem_reserve=16 hdmi.audio=EDID:0 disp.screen0_output_mode=EDID:1280x720p60 panic=10 consoleblank=0
+#--------------------------------------------------------------------------------------------------------------------------------
+# Boot loader script to boot with different boot methods for old and new kernel
+#--------------------------------------------------------------------------------------------------------------------------------
+if ext4load mmc 0 0x00000000 /boot/.next
+then
+# sunxi mainline kernel
+#--------------------------------------------------------------------------------------------------------------------------------
+ext4load mmc 0 0x49000000 /boot/dtb/${fdtfile} 
+ext4load mmc 0 0x46000000 /boot/zImage
+env set fdt_high ffffffff
+bootz 0x46000000 - 0x49000000
+#--------------------------------------------------------------------------------------------------------------------------------
+else
+# sunxi android kernel
+#--------------------------------------------------------------------------------------------------------------------------------
 ext4load mmc 0 0x43000000 /boot/script.bin
 ext4load mmc 0 0x48000000 /boot/zImage
 bootz 0x48000000
+#--------------------------------------------------------------------------------------------------------------------------------
+fi
