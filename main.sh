47713304062b0c9f8373ab175b50331e74dbd650
Igor Pecovnik <igor.pecovnik@gmail.com>
2015-04-27 18:45:15 +0200

    Fixed Banana PRO build problem

--- main.sh
+++ main.sh
@@ -35,7 +35,7 @@ fi
 # Choose destination
 #--------------------------------------------------------------------------------------------------------------------------------
 if [ "$BOARD" == "" ]; then
-	BOARDS="Cubieboard A10 Cubieboard2 A20 Cubietruck A20 Lime A20 Lime2 A20 Micro A20 Bananapi A20 Bananapro A20 Lamobo-R1 A20 Orangepi A20 Hummingbird A31 Cubox-i imx6 Udoo imx6";
+	BOARDS="Cubieboard A10 Cubieboard2 A20 Cubietruck A20 Lime A20 Lime2 A20 Micro A20 Bananapi A20 Bananapipro A20 Lamobo-R1 A20 Orangepi A20 Hummingbird A31 Cubox-i imx6 Udoo imx6";
 	MYLIST=`for x in $BOARDS; do echo $x ""; done`
 	whiptail --title "Choose a board" --backtitle "" --menu "\nWhich one?" 18 30 8 $MYLIST 2>results    
 	BOARD=$(<results)
