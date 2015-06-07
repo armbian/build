cdab52b08bfb30f2118476abc13fd86b1636ba7e
Igor Pecovnik <igor.pecovnik@gmail.com>
2015-06-07 21:00:10 +0200

    Boot commands standardized due to fixes inside u-boot. Can boot from FAT or EXT2 with the same script. Only rootfs target need to be adjusted

--- config/boot-udoo-neo.cmd
+++ config/boot-udoo-neo.cmd
@@ -1,4 +1,8 @@
-﻿setenv bootargs root=/dev/mmcblk0p1 rootfstype=ext4 rootwait 
-setenv fdt_file /boot/dtb/imx6sx-udooneo.dtb
-run loadimage
-run mmcboot 
+﻿echo "Booting..."
+setenv fdt_file imx6sx-udooneo.dtb
+setenv bootargs root=/dev/mmcblk1p1 rootfstype=ext4 rootwait console=ttymxc0,115200
+ext2load mmc 0 ${loadaddr} ${prefix}zImage || fatload mmc 0 ${loadaddr} ${prefix}zImage
+ext2load mmc 0 ${fdt_addr} ${prefix}dtb/${fdt_file} || fatload mmc 0 ${fdt_addr} ${prefix}dtb/${fdt_file}
+bootz ${loadaddr} - ${fdt_addr}
+# Recompile: mkimage -C none -A arm -T script -d /boot/boot.cmd /boot/boot.scr 
+# (c) www.armbian.com
