b3db7538b0f071621e24c59d0fb86060b6fa7ac4
Igor Pecovnik <igor.pecovnik@gmail.com>
2015-09-01 19:28:02 +0200

    Typo mistakes

--- general.sh
+++ general.sh
@@ -50,9 +50,9 @@ download_host_packages (){
 # Download packages for host and install only if missing - Ubuntu 14.04 recommended                     
 #--------------------------------------------------------------------------------------------------------------------------------
 if [ ! -f "/etc/apt/sources.list.d/aptly.conf" ]; then
-echo "deb http://repo.aptly.info/ squeeze main" > /etc/apt/sources.list.d/aptly.conf
+echo "deb http://repo.aptly.info/ squeeze main" > /etc/apt/sources.list.d/aptly.list
 apt-key adv --keyserver keys.gnupg.net --recv-keys E083A3782A194991
-apt-get updates
+apt-get update
 fi
 
 IFS=" "
