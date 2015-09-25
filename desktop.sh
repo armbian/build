6833e0a09aedab7af787f6ca5ca05d4ea1d7256f
Igor Pecovnik <igor.pecovnik@gmail.com>
2015-09-25 21:06:15 +0200

    Packet installation moved to common functions.

--- desktop.sh
+++ desktop.sh
@@ -19,9 +19,11 @@ display_alert "Installing desktop" "XFCE" "info"
 umount $DEST/cache/sdcard/tmp >/dev/null 2>&1
 mount --bind $SRC/lib/bin/ $DEST/cache/sdcard/tmp
 
+# Declare office packages if any
 OFFICE_PACKETS="libreoffice-writer libreoffice-java-common"
 
 # Debian Wheezy
+#--------------------------------------------------------------------------------------------------------------------------------
 if [[ $RELEASE == "wheezy" ]]; then
 BASIC_PACKETS="xserver-xorg xserver-xorg-core xfonts-base xinit nodm x11-xserver-utils xfce4 mozo pluma wicd thunar-volman \
 galculator iceweasel libgnome2-perl gcj-jre-headless gtk2-engines gtk2-engines-murrine gtk2-engines-pixbuf libgtk2.0-bin \
@@ -34,6 +36,7 @@ chroot $DEST/cache/sdcard /bin/bash -c "tar xfz /tmp/wheezy-desktop.tgz -C /root
 fi
 
 # Debian Jessie
+#--------------------------------------------------------------------------------------------------------------------------------
 if [[ $RELEASE == "jessie" ]]; then
 BASIC_PACKETS="xserver-xorg xserver-xorg-core xfonts-base xinit nodm x11-xserver-utils xfce4 mozo pluma wicd thunar-volman \
 galculator iceweasel libgnome2-perl gcj-jre-headless gtk2-engines gtk2-engines-murrine gtk2-engines-pixbuf libgtk2.0-bin \
@@ -46,6 +49,7 @@ chroot $DEST/cache/sdcard /bin/bash -c "tar xfz /tmp/jessie-desktop.tgz -C /root
 fi
 
 # Ubuntu trusty
+#--------------------------------------------------------------------------------------------------------------------------------
 if [[ $RELEASE == "trusty" ]]; then
 BASIC_PACKETS="xserver-xorg xserver-xorg-core xfonts-base xinit nodm x11-xserver-utils xfce4 wicd thunar-volman galculator \
 libgnome2-perl gcj-jre-headless gtk2-engines gtk2-engines-murrine gtk2-engines-pixbuf libgtk2.0-bin \
@@ -58,23 +62,7 @@ test -d "$d" || mkdir -p "$d" && cp $SRC/lib/bin/armbian*.jpg "$d"
 chroot $DEST/cache/sdcard /bin/bash -c "tar xfz /tmp/trusty-desktop.tgz -C /root/"
 fi
 
-# Install packets
-i=0
-j=1
-declare -a PACKETS=($BASIC_PACKETS)
-skupaj=${#PACKETS[@]}
-while [[ $i -lt $skupaj ]]; do
-procent=$(echo "scale=2;($j/$skupaj)*100"|bc)
-		x=${PACKETS[$i]}	
-		if [ "$(chroot $DEST/cache/sdcard /bin/bash -c "apt-get -qq -y install $x >/tmp/install.log 2>&1 || echo 'Installation failed'" | grep 'Installation failed')" != "" ]; then 
-			echo -e "[\e[0;31m error \x1B[0m] Installation failed"
-			tail $DEST/cache/sdcard/tmp/install.log
-			exit
-		fi
-		printf '%.0f\n' $procent | dialog --gauge "Installing desktop" 7 50
-		i=$[$i+1]
-		j=$[$j+1]
-done
+install_packet "$BASIC_PACKETS" "Installing desktop"
 
 # Install custom icons and theme
 #cp $SRC/lib/bin/vibrancy-colors_2.4-trusty-Noobslab.com_all.deb /tmp/kernel
@@ -82,8 +70,8 @@ done
 chroot $DEST/cache/sdcard /bin/bash -c "dpkg -i /tmp/vibrancy-colors_2.4-trusty-Noobslab.com_all.deb >/dev/null 2>&1"
 chroot $DEST/cache/sdcard /bin/bash -c "unzip -qq /tmp/NumixHolo.zip -d /usr/share/themes"
 # cleanup
-chroot $DEST/cache/sdcard /bin/bash -c "apt-get -y autoremove"
-chroot $DEST/cache/sdcard /bin/bash -c "apt-get clean"
+chroot $DEST/cache/sdcard /bin/bash -c "apt-get -y autoremove >/dev/null 2>&1"
+chroot $DEST/cache/sdcard /bin/bash -c "apt-get clean >/dev/null 2>&1"
 
 umount $DEST/cache/sdcard/tmp >/dev/null 2>&1
 
@@ -98,26 +86,38 @@ sed "s/NODM_ENABLED=\(.*\)/NODM_ENABLED=true/g" -i $DEST/cache/sdcard/etc/defaul
  
 # Compile Turbo Frame buffer for sunxi
 if [[ $LINUXCONFIG == *sunxi* && $BRANCH != "next" ]]; then
- chroot $DEST/cache/sdcard /bin/bash -c "debconf-apt-progress -- apt-get -y install xorg-dev xutils-dev x11proto-dri2-dev xutils-dev libdrm-dev libvdpau-dev"
+ install_packet "xorg-dev xutils-dev x11proto-dri2-dev xutils-dev libdrm-dev libvdpau-dev" "Installing support libraries"
  # quemu bug walkaround
  git clone https://github.com/ssvb/xf86-video-fbturbo.git $DEST/cache/sdcard/tmp/xf86-video-fbturbo
  chroot $DEST/cache/sdcard /bin/bash -c "cd /tmp/xf86-video-fbturbo && autoreconf -vi"
  chroot $DEST/cache/sdcard /bin/bash -c "cd /tmp/xf86-video-fbturbo && ./configure --prefix=/usr"
  chroot $DEST/cache/sdcard /bin/bash -c "cd /tmp/xf86-video-fbturbo && make && make install && cp xorg.conf /etc/X11/xorg.conf"
+ 
  # compile video acceleration
+ # A
  git clone https://github.com/linux-sunxi/libvdpau-sunxi.git $DEST/cache/sdcard/tmp/libvdpau-sunxi
  chroot $DEST/cache/sdcard /bin/bash -c "cd /tmp/libvdpau-sunxi && make"
  d=$DEST/cache/sdcard/usr/lib/vdpau
  test -d "$d" || mkdir -p "$d" && cp $DEST/cache/sdcard/tmp/libvdpau-sunxi/libvdpau_sunxi.so.1 "$d"
  ln -s $d/libvdpau_sunxi.so $d/libvdpau_sunxi.so.1
- 
+
+ # B
+ # git clone https://github.com/Snaipe/libcsptr.git $DEST/cache/sdcard/usr/local/src/libcsptr
+ #	chroot $DEST/cache/sdcard /bin/bash -c "cd /usr/local/src/libcsptr && mkdir build && cd build && cmake -DCMAKE_INSTALL_PREFIX=/usr/local .. && make && make install"
+ #	chroot $DEST/cache/sdcard /bin/bash -c "cd /usr/local/src/libcsptr && make"
+ #	chroot $DEST/cache/sdcard /bin/bash -c "cd /usr/local/src/libcsptr && make install"
+ # 	display_alert "GIT clone" "libvdpau-sunxi" "info"
+ #	echo -e "[\e[0;34m git cloning \x1B[0m] libvdpau-sunxi ..."
+ #	git clone -b staging https://github.com/rellla/libvdpau-sunxi.git $DEST/cache/sdcard/usr/local/src/libvdpau-sunxi
+ #	chroot $DEST/cache/sdcard /bin/bash -c "cd /usr/local/src/libvdpau-sunxi && make clean && make $CTHREADS ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- && make install"
+
  # That we can just play
  echo "export VDPAU_DRIVER=sunxi" >> $DEST/cache/sdcard/etc/profile
  # enable memory reservations
  sed "s/sunxi_ve_mem_reserve=0 sunxi_g2d_mem_reserve=0 sunxi_no_mali_mem_reserve sunxi_fb_mem_reserve=16 //g" -i $DEST/cache/sdcard/boot/boot.cmd
  mkimage -C none -A arm -T script -d $DEST/cache/sdcard/boot/boot.cmd $DEST/cache/sdcard/boot/boot.scr >> /dev/null 
  # clean deb cache
- chroot $DEST/cache/sdcard /bin/bash -c "apt-get -y clean"	
+ chroot $DEST/cache/sdcard /bin/bash -c "apt-get -y clean >/dev/null 2>&1"	
 fi
 
 if [[ $LINUXCONFIG == *sunxi* ]]; then
