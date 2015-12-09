a636ff1b208db23107c57a8d07cc1ad02ba55bfc
Igor Pečovnik <igor.pecovnik@gmail.com>
2015-12-09 22:15:00 +0100

    Another debootstrap fix

--- deboostrap.sh
+++ deboostrap.sh
@@ -190,7 +190,7 @@ KILLPROC=$(ps -uax | pgrep ntpd |        tail -1); if [ -n "$KILLPROC" ]; then k
 KILLPROC=$(ps -uax | pgrep dbus-daemon | tail -1); if [ -n "$KILLPROC" ]; then kill -9 $KILLPROC; fi  
 
 display_alert "Closing deboostrap process and preparing cache." "" "info"
-tar cpf - --directory=$DEST/cache/sdcard/ --exclude=dev --exclude=proc --exclude=run --exclude=tmp --exclude=mnt --exclude=sys . \
+tar cpf - --directory=$DEST/cache/sdcard/ --exclude=dev/* --exclude=proc/* --exclude=run --exclude=tmp/* --exclude=mnt/* --exclude=sys/* . \
 | pigz > $DEST/cache/rootfs/$RELEASE.tgz
 fi
 #
