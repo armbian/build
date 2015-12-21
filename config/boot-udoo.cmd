b524751907a60ffa36984e10cec7b6f29ea30aa4
Igor Pečovnik <igor.pecovnik@gmail.com>
2015-12-21 10:17:44 +0100

    Udoo quad/dl u-boot patches

--- config/boot-udoo.cmd
+++ config/boot-udoo.cmd
@@ -1,5 +1,5 @@
 setenv bootargs root=/dev/mmcblk0p1 rootfstype=ext4 rootwait console=tty1 video=mxcfb0:dev=hdmi,1920x1080M@60,if=RGB24,bpp=32 rd.dm=0 rd.luks=0 rd.lvm=0 raid=noautodetect pci=nomsi ahci_imx.hotplug=1 quiet
-setenv fdt_file imx6q-udoo.dtb
+setenv fdt_file imx6q-udoo-hdmi.dtb
 ext2load mmc 0 ${fdt_addr} /boot/dtb/${fdt_file} || fatload mmc 0 ${fdt_addr} dtb/${fdt_file}
 ext2load mmc 0 ${loadaddr} /boot/${image} || fatload mmc 0 ${loadaddr} ${image}
 bootz ${loadaddr} - ${fdt_addr}
