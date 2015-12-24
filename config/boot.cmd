2846d9388afd6cca85cea3152022580891999da5
zador-blood-stained <zador-blood-stained@users.noreply.github.com>
2015-12-24 19:37:34 +0300

    Small changes Edited default boot.cmd

--- config/boot.cmd
+++ config/boot.cmd
@@ -1,6 +1,4 @@
-# if you want to boot jessie with systemd: setenv bootargs init=/bin/systemd console=tty1 ...
-# remember to disable ramlog prior switching to systemd
-setenv bootargs "console=tty1 root=/dev/mmcblk0p1 rootwait rootfstype=ext4 sunxi_ve_mem_reserve=0 sunxi_g2d_mem_reserve=0 sunxi_no_mali_mem_reserve sunxi_fb_mem_reserve=16 hdmi.audio=EDID:0 disp.screen0_output_mode=1920x1080p60 panic=10 consoleblank=0 enforcing=0 loglevel=1"
+setenv bootargs "console=tty1 root=/dev/mmcblk0p1 rootwait sunxi_ve_mem_reserve=0 sunxi_g2d_mem_reserve=0 sunxi_no_mali_mem_reserve sunxi_fb_mem_reserve=16 hdmi.audio=EDID:0 disp.screen0_output_mode=1920x1080p60 panic=10 consoleblank=0 enforcing=0 loglevel=1"
 #--------------------------------------------------------------------------------------------------------------------------------
 # Boot loader script to boot with different boot methods for old and new kernel
 #--------------------------------------------------------------------------------------------------------------------------------
