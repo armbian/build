58b06d134dd276185fa8cf0e2e128a0a69c74a37
Igor Pečovnik <igor.pecovnik@gmail.com>
2016-01-13 11:30:36 +0100

    Small fixes for Lemaker Guitar

--- config/boot-guitar.cmd
+++ config/boot-guitar.cmd
@@ -1,5 +1,8 @@
-setenv bootargs "earlyprintk clk_ignore_unused selinux=0 scandelay root=/dev/mmcblk0p2 rw console=tty0 rootfstype=ext4 console=ttyS3 loglevel=1 rootwait"
+# console=ttyS3 # serial
+setenv bootargs "earlyprintk clk_ignore_unused selinux=0 scandelay root=/dev/mmcblk0p2 rw console=tty1 rootfstype=ext4 loglevel=1 rootwait"
 setenv os_type linux
 fatload mmc 0:1 0x04000000 dtb/lemaker_guitar_bbb.dtb
 fatload mmc 0:1 0x7fc0 zImage
 bootz 0x7fc0 - 0x04000000
+# mkimage -C none -A arm -T script -d /boot/boot.cmd /boot/boot.scr 
+ 
