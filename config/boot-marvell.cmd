82be91cbc05eb037e461dcc94319b7f346b1f40e
Igor Pečovnik <igor.pecovnik@gmail.com>
2016-01-15 12:21:50 +0100

    Small tweaks to marvell boot script

--- config/boot-marvell.cmd
+++ config/boot-marvell.cmd
@@ -1,4 +1,6 @@
-setenv bootargs "selinux=0 scandelay root=/dev/mmcblk0p1 rw rootfstype=ext4 console=ttyS0,115200 loglevel=1 rootwait"
+setenv bootargs "selinux=0 cgroup_disable=memory scandelay root=/dev/mmcblk0p1 rw rootfstype=ext4 console=ttyS0,115200 loglevel=1 rootwait"
 ext2load mmc 0:1 ${fdtaddr} boot/dtb/armada-388-clearfog.dtb
 ext2load mmc 0:1 ${loadaddr} boot/zImage
 bootz ${loadaddr} - ${fdtaddr}
+# Recompile with:
+# mkimage -C none -A arm -T script -d /boot/boot.cmd /boot/boot.scr 
