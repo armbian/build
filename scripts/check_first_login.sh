5831a004e54135e0d4990e64ef0997c075868ca3
Igor Pecovnik <igor.pecovnik@gmail.com>
2016-01-20 09:12:38 +0100

    Small text change in alerts

--- scripts/check_first_login.sh
+++ scripts/check_first_login.sh
@@ -2,10 +2,8 @@
 # only do this for interactive shells
 if [ "$-" != "${-#*i}" ]; then
     if [ -f "$HOME/.not_logged_in_yet" ]; then
-		echo -e "\e[0;31mThank you for choosing Armbian. This is your first login and it's time for:\x1B[0m"
-        echo ""
-        echo -e "\e[0;32mSytem update: \x1B[0mapt-get upgrade \e[0;32mand deployment: \x1B[0mnand-sata-install"     
-        echo ""
+		echo -e "\e[0;31mThank you for choosing Armbian. Support: www.armbian.com\x1B[0m"
+		echo "" 
         rm -f "$HOME/.not_logged_in_yet"
     fi
 fi
--- scripts/check_first_login_reboot.sh
+++ scripts/check_first_login_reboot.sh
@@ -2,8 +2,8 @@
 # only do this for interactive shells
 if [ "$-" != "${-#*i}" ]; then
     if [ -f "/tmp/.reboot_required" ]; then
-        echo -e "[\e[0;32m Restart is required \x1B[0m]"
-        echo ""
+        echo -e "[\e[0;91m Kernel was updated, please reboot!\x1B[0m ]"
+		echo ""
         rm "/tmp/.reboot_required"
     fi
 fi
