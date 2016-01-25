ae937517db696f1b6950196af7356d2122a5bc91
Igor Pecovnik <igor.pecovnik@gmail.com>
2016-01-25 13:17:34 +0100

    MAC is not yet fixed in Uboot so we need to set it in boot script otherwise we gets random MAC on each boot

--- config/boot-marvell.cmd
+++ config/boot-marvell.cmd
@@ -1,4 +1,9 @@
-setenv bootargs "selinux=0 cgroup_disable=memory scandelay root=/dev/mmcblk0p1 rw rootfstype=ext4 console=ttyS0,115200 loglevel=1 rootwait"
+setenv ethaddr  00:50:43:84:fb:2f
+setenv eth1addr 00:50:43:25:fb:84
+setenv eth2addr 00:50:43:84:25:2f
+setenv eth3addr 00:50:43:0d:19:18
+#
+setenv bootargs "selinux=0 cgroup_disable=memory scandelay root=/dev/mmcblk0p1 rw rootfstype=ext4 console=ttyS0,115200 loglevel=7 rootwait"
 ext2load mmc 0:1 ${fdtaddr} boot/dtb/armada-388-clearfog.dtb
 ext2load mmc 0:1 ${loadaddr} boot/zImage
 bootz ${loadaddr} - ${fdtaddr}
