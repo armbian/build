4d962842cd3fc46d61d461a3a9059ff858ef3fd7
Igor Pecovnik <igor.pecovnik@gmail.com>
2016-01-30 19:31:48 +0100

    First run scripts added functionality. If there is not enough space, create SWAP area after reboot - when FS is expanded

--- scripts/check_first_login.sh
+++ scripts/check_first_login.sh
@@ -2,6 +2,7 @@
 # only do this for interactive shells
 if [ "$-" != "${-#*i}" ]; then
     if [ -f "$HOME/.not_logged_in_yet" ]; then
+		echo "" 
 		echo -e "\e[0;31mThank you for choosing Armbian. Support: www.armbian.com\x1B[0m"
 		echo "" 
         rm -f "$HOME/.not_logged_in_yet"
--- scripts/check_first_login_reboot.sh
+++ scripts/check_first_login_reboot.sh
@@ -2,6 +2,7 @@
 # only do this for interactive shells
 if [ "$-" != "${-#*i}" ]; then
     if [ -f "/tmp/.reboot_required" ]; then
+		echo "" 
         echo -e "[\e[0;91m Kernel was updated, please reboot!\x1B[0m ]"
 		echo ""
         rm "/tmp/.reboot_required"
