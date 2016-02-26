a3d2c3dca0eaec9e2d1901433387edb6861f40ca
Thomas Kaiser <github@kaiser-edv.de>
2016-02-26 13:08:22 +0100

    Add normal default user account to more necessary groups

--- common.sh
+++ common.sh
@@ -163,11 +163,11 @@ compile_sunxi_tools (){
 	display_alert "Compiling sunxi tools" "@host & target" "info"
 	cd $SOURCES/$MISC1_DIR
 	make -s clean >/dev/null 2>&1
-	rm -f sunxi-fexc sunxi-nand-part sunxi-fel sunxi-pio 2>/dev/null
+	rm -f sunxi-fexc sunxi-nand-part
 	make -s >/dev/null 2>&1
 	cp fex2bin bin2fex /usr/local/bin/
 	make -s clean >/dev/null 2>&1
-	rm -f sunxi-fexc sunxi-nand-part
+	rm -f sunxi-fexc sunxi-nand-part meminfo sunxi-fel sunxi-pio 2>/dev/null
 	make $CTHREADS 'sunxi-nand-part' CC=arm-linux-gnueabihf-gcc >> $DEST/debug/install.log 2>&1
 	make $CTHREADS 'sunxi-fexc' CC=arm-linux-gnueabihf-gcc >> $DEST/debug/install.log 2>&1
 	make $CTHREADS 'sunxi-fel' CC=arm-linux-gnueabihf-gcc >> $DEST/debug/install.log 2>&1
--- scripts/check_first_login.sh
+++ scripts/check_first_login.sh
@@ -7,12 +7,14 @@ if [ "$-" != "${-#*i}" ]; then
 		read username
 		RealUserName="$(echo "${username}" | tr '[:upper:]' '[:lower:]' | tr -d -c '[:alpha:]')"
 		adduser ${RealUserName} || reboot
-		usermod -aG sudo ${RealUserName}
+		for additionalgroup in sudo netdev audio video dialout plugdev ; do
+			usermod -aG ${additionalgroup} ${RealUserName}
+		done
 		rm -f "$HOME/.not_logged_in_yet"
+		echo -e "\nYour accout ${RealUserName} has been created and is sudo enabled.\n"
 		# check whether desktop environment has to be considered
 		if [ -f /etc/init.d/nodm ] ; then 
 			sed -i "s/NODM_USER=root/NODM_USER=${RealUserName}/" /etc/default/nodm
-			usermod -aG netdev ${RealUserName}
 			update-rc.d nodm enable >/dev/null 2>&1
 			echo -e "\n\e[1m\e[39mReboot necessary...\x1B[0m\n"
 			reboot
