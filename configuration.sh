f3cae88a4cebbbe88f60be15d64f76a21cbf5e62
zador-blood-stained <zador-blood-stained@users.noreply.github.com>
2016-02-26 19:28:17 +0300

    Bugfix for configuration.sh

--- configuration.sh
+++ configuration.sh
@@ -547,13 +547,13 @@ if [[ $BUILD_DESKTOP == yes ]]; then
 	# release specific desktop packages
 	case $RELEASE in
 		wheezy)
-		PACKAGE_LIST_DESKTOP="mozo pluma iceweasel icedove"
+		PACKAGE_LIST_DESKTOP="$PACKAGE_LIST_DESKTOP mozo pluma iceweasel icedove"
 		;;
 		jessie)
-		PACKAGE_LIST_DESKTOP="mozo pluma iceweasel libreoffice-writer libreoffice-java-common icedove"
+		PACKAGE_LIST_DESKTOP="$PACKAGE_LIST_DESKTOP mozo pluma iceweasel libreoffice-writer libreoffice-java-common icedove"
 		;;
 		trusty)
-		PACKAGE_LIST_DESKTOP="libreoffice-writer libreoffice-java-common thunderbird firefox gnome-icon-theme-full tango-icon-theme gvfs-backends"
+		PACKAGE_LIST_DESKTOP="$PACKAGE_LIST_DESKTOP libreoffice-writer libreoffice-java-common thunderbird firefox gnome-icon-theme-full tango-icon-theme gvfs-backends"
 		;;
 	esac
 	# hardware acceleration support packages
