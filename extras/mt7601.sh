d5ad7425a8303a99b75ad43532fa1f7fb7ef0665
zador-blood-stained <zador-blood-stained@users.noreply.github.com>
2016-04-24 17:10:39 +0300

    Rtl8192cu GCC 5 compatibility patch

--- extras/mt7601.sh
+++ extras/mt7601.sh
@@ -11,54 +11,57 @@ install_mt7601()
 {
 	local plugin_repo="https://github.com/porjo/mt7601/"
 	local plugin_dir="mt7601"
-	
+
 	fetch_from_github "$plugin_repo" "$plugin_dir"
 
 	cd $SOURCES/$plugin_dir
-	cat >> fix_build.patch << _EOF_
-diff --git a/src/dkms.conf b/src/dkms.conf
-new file mode 100644
-index 0000000..7563b5a
---- /dev/null
-+++ b/src/dkms.conf
-@@ -0,0 +1,8 @@
-+PACKAGE_NAME="mt7601-sta-dkms"
-+PACKAGE_VERSION="3.0.0.4"
-+CLEAN="make clean"
-+BUILT_MODULE_NAME[0]="mt7601Usta"
-+BUILT_MODULE_LOCATION[0]="./os/linux/"
-+DEST_MODULE_LOCATION[0]="/kernel/drivers/net/wireless"
-+AUTOINSTALL=yes
-+MAKE[0]="make -j4 KERNELVER=\$kernelver"
-diff --git a/src/include/os/rt_linux.h b/src/include/os/rt_linux.h
-index 3726b9e..b8be886 100755
---- a/src/include/os/rt_linux.h
-+++ b/src/include/os/rt_linux.h
-@@ -279,7 +279,7 @@ typedef struct file* RTMP_OS_FD;
- 
- typedef struct _OS_FS_INFO_
- {
--#if LINUX_VERSION_CODE >= KERNEL_VERSION(3,12,0)
-+#if LINUX_VERSION_CODE >= KERNEL_VERSION(3,4,0)
- 	uid_t				fsuid;
- 	gid_t				fsgid;
- #else
-diff --git a/src/os/linux/rt_linux.c b/src/os/linux/rt_linux.c
-index 1b6a631..c336611 100755
---- a/src/os/linux/rt_linux.c
-+++ b/src/os/linux/rt_linux.c
-@@ -51,7 +51,7 @@
- #define RT_CONFIG_IF_OPMODE_ON_STA(__OpMode)
- #endif
- 
--ULONG RTDebugLevel = RT_DEBUG_TRACE;
-+ULONG RTDebugLevel = 0;
- ULONG RTDebugFunc = 0;
- 
- #ifdef OS_ABL_FUNC_SUPPORT
-_EOF_
 
-	patch -f -s -p1 -r - <fix_build.patch >/dev/null
+	# DKMS support patch
+	rm src/dkms.conf
+
+	patch --batch --silent -N -p1 <<-'EOF'
+	diff --git a/src/dkms.conf b/src/dkms.conf
+	new file mode 100644
+	index 0000000..7563b5a
+	--- /dev/null
+	+++ b/src/dkms.conf
+	@@ -0,0 +1,8 @@
+	+PACKAGE_NAME="mt7601-sta-dkms"
+	+PACKAGE_VERSION="3.0.0.4"
+	+CLEAN="make clean"
+	+BUILT_MODULE_NAME[0]="mt7601Usta"
+	+BUILT_MODULE_LOCATION[0]="./os/linux/"
+	+DEST_MODULE_LOCATION[0]="/kernel/drivers/net/wireless"
+	+AUTOINSTALL=yes
+	+MAKE[0]="make -j4 KERNELVER=\$kernelver"
+	diff --git a/src/include/os/rt_linux.h b/src/include/os/rt_linux.h
+	index 3726b9e..b8be886 100755
+	--- a/src/include/os/rt_linux.h
+	+++ b/src/include/os/rt_linux.h
+	@@ -279,7 +279,7 @@ typedef struct file* RTMP_OS_FD;
+	 
+	 typedef struct _OS_FS_INFO_
+	 {
+	-#if LINUX_VERSION_CODE >= KERNEL_VERSION(3,12,0)
+	+#if LINUX_VERSION_CODE >= KERNEL_VERSION(3,4,0)
+	 	uid_t				fsuid;
+	 	gid_t				fsgid;
+	 #else
+	diff --git a/src/os/linux/rt_linux.c b/src/os/linux/rt_linux.c
+	index 1b6a631..c336611 100755
+	--- a/src/os/linux/rt_linux.c
+	+++ b/src/os/linux/rt_linux.c
+	@@ -51,7 +51,7 @@
+	 #define RT_CONFIG_IF_OPMODE_ON_STA(__OpMode)
+	 #endif
+	 
+	-ULONG RTDebugLevel = RT_DEBUG_TRACE;
+	+ULONG RTDebugLevel = 0;
+	 ULONG RTDebugFunc = 0;
+	 
+	 #ifdef OS_ABL_FUNC_SUPPORT
+	EOF
+
 	cd src
 	make -s ARCH=$ARCHITECTURE CROSS_COMPILE="$CROSS_COMPILE" clean >> $DEST/debug/compilation.log 2>&1
 	make -s -j4 ARCH=$ARCHITECTURE CROSS_COMPILE="$CROSS_COMPILE" LINUX_SRC=$SOURCES/$LINUXSOURCEDIR/ >> $DEST/debug/compilation.log 2>&1
