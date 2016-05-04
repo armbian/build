f3435fb0fb84373fe61db2b907cbb8599b9dcd95
zador-blood-stained <zador-blood-stained@users.noreply.github.com>
2016-05-04 22:41:24 +0300

    Update Pine64 boot script

--- config/boot-pine64.cmd
+++ config/boot-pine64.cmd
@@ -1,11 +1,9 @@
 setenv fdt_filename "${pine64_model}.dtb"
-run load_dtb
-fatload mmc 0:1 ${kernel_addr} Image
-run load_initrd
+setenv kernel_filename Image
 
-setenv bootargs "console=ttyS0,115200n8 no_console_suspend earlycon=uart,mmio32,0x01c28000 mac_addr=${ethaddr} root=${root} rootwait panic=10 consoleblank=0 enforcing=0 loglevel=7"
+setenv bootargs "console=ttyS0,115200n8 no_console_suspend earlycon=uart,mmio32,0x01c28000 mac_addr=${ethaddr} root=${root} rootwait panic=10 consoleblank=0 enforcing=0 loglevel=2"
 
-run boot_kernel
+run load_dtb load_kernel load_initrd boot_kernel
 
 # Recompile with:
-# mkimage -C none -A arm -T script -d /boot/boot.cmd /boot/boot.scr 
+# mkimage -C none -A arm -T script -d /boot/boot.cmd /boot/boot.scr
