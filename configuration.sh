5b05e265720918c8af1ea8ee1cba0660ec8cbdd3
zador-blood-stained <zador-blood-stained@users.noreply.github.com>
2016-05-17 11:45:30 +0300

    Update mainline u-boot to v2016.05

--- configuration.sh
+++ configuration.sh
@@ -77,18 +77,6 @@ esac
 
 # temporary hacks/overrides
 case $LINUXFAMILY in
-	sun4i|sun5i|sun6i|sun7i)
-	# latest stable u-boot v2016.03 broke gmac on sun7i, fixing it for DEFAULT and NEXT
-	[[ $BRANCH == default || $BRANCH == next ]] && BOOTBRANCH='v2016.01'
-	# fix for olimex boards
-	[[ ($BRANCH == default || $BRANCH == next) && ($BOARD == lime* || $BOARD == micro) ]] && BOOTBRANCH='v2016.05-rc1'
-	;;
-
-	sun8i)
-	# fix compilation of sun8i-dev u-boot
-	[[ $BRANCH == dev ]] && BOOTBRANCH='v2016.05-rc1'
-	;;
-
 	pine64)
 	# fix for u-boot needing armhf GCC 4.8
 	UBOOT_COMPILER="arm-linux-gnueabihf-"
