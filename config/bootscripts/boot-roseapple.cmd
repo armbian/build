e5f3c5b7d439950d95479297975a0c7658f5ba15
Igor Pecovnik <igor.pecovnik@gmail.com>
2016-09-13 09:52:55 +0200

    Initrd support for S500 based boards (Roseapple and Guitar). Tested on both

--- config/bootscripts/boot-roseapple.cmd
+++ config/bootscripts/boot-roseapple.cmd
@@ -1,6 +1,7 @@
 setenv fdt_high "0xffffffff" 
 setenv bootargs "earlyprintk clk_ignore_unused selinux=0 scandelay root=/dev/mmcblk0p2 rw console=tty1 rootfstype=ext4 loglevel=1 rootwait" 
-fatload mmc 0:1 0x04000000 dtb/actduino_bubble_gum_sdboot_linux.dtb 
-fatload mmc 0:1 0x00008000 zImage
-bootz 0x00008000 - 0x04000000
+fatload mmc 0:1 ${fdt_addr_r} dtb/actduino_bubble_gum_sdboot_linux.dtb 
+fatload mmc 0:1 ${ramdisk_addr_r} uInitrd
+fatload mmc 0:1 ${kernel_addr_r} zImage
+bootz ${kernel_addr_r} ${ramdisk_addr_r} ${fdt_addr_r}
 # mkimage -C none -A arm -T script -d /boot/boot.cmd /boot/boot.scr 
