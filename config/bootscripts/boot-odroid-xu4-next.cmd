a82dc77aee0c286de600192365922dc74741185f
Guillaume Romagny <golfromeo-fr@users.noreply.github.com>
2016-10-04 10:40:04 +0200

    XU4 next: single partition boot (no headache version) (#486)

--- config/bootscripts/boot-odroid-xu4-next.cmd
+++ config/bootscripts/boot-odroid-xu4-next.cmd
@@ -1,93 +1,24 @@
-# This is an example file to generate boot.scr - a boot script for U-Boot
-# Generate boot.scr:
-# ./tools/mkimage -c none -A arm -T script -d autoboot.cmd boot.scr
+# a boot script for U-Boot / Odroid XU4
 #
-# It requires a list of environment variables to be defined before load:
+# It requires a list of environment variables to be defined before load (in includes files uboot/.../exy*.h):
 # platform dependent: boardname, fdtfile, console
 # system dependent: mmcbootdev, mmcbootpart, mmcrootdev, mmcrootpart, rootfstype
 #
-setenv fdtaddr     "40800000"
-setenv initrdname  "uInitrd"
-setenv initrdaddr  "42000000"
-setenv mmcrootdev  "1"
-setenv loaddtb     "load mmc ${mmcbootdev}:${mmcbootpart} ${fdtaddr} ${fdtfile}"
-setenv loadinitrd  "load mmc ${mmcbootdev}:${mmcbootpart} ${initrdaddr} ${initrdname}"
-setenv loadkernel  "load mmc ${mmcbootdev}:${mmcbootpart} '${kerneladdr}' '${kernelname}'"
-setenv kernel_args "setenv bootargs ${console} root=/dev/mmcblk${mmcrootdev}p${mmcrootpart} rootfstype=${rootfstype} rootwait earlyprintk ${opts}"
 
-#### Routine: check_dtb - check that target.dtb exists on boot partition
-setenv check_dtb "
-if test -e mmc '${mmcbootdev}':'${mmcbootpart}' '${fdtfile}'; then
-        run loaddtb;
-        setenv fdt_addr ${fdtaddr};
-else
-        echo Warning! Booting without DTB: '${fdtfile}'!;
-        setenv fdt_addr;
-fi;"
+setenv kerneladdr       0x40800000
+setenv initrdaddr       0x42000000
+setenv ftdaddr          0x44000000
 
-#### Routine: check_ramdisk - check that uInitrd exists on boot partition
-setenv check_ramdisk "
-if test -e mmc '${mmcbootdev}':'${mmcbootpart}' '${initrdname}'; then
-        echo "Found ramdisk image.";
-        run loadinitrd;
-        setenv initrd_addr ${initrdaddr};
-else
-        echo Warning! Booting without RAMDISK: '${initrdname}'!;
-        setenv initrd_addr -;
-fi;"
+setenv consolecfg       "console=tty1 console=ttySAC2,115200n8"
 
-#### Routine: boot_fit - check that env $boardname is set and boot proper config of ITB image
-setenv setboot_fit "
-if test -e '${boardname}'; then
-        setenv fdt_addr ;
-        setenv initrd_addr ;
-        setenv kerneladdr  0x42000000;
-        setenv kernelname  Image.itb;
-        setenv itbcfg      "\"#${boardname}\"";
-        setenv imgbootcmd  bootm;
-else
-        echo Warning! Variable: \$boardname is undefined!;
-fi"
+setenv rootfs           "/dev/mmcblk1p1"
 
-#### Routine: setboot_uimg - prepare env to boot uImage
-setenv setboot_uimg "
-        setenv kerneladdr 0x40007FC0;
-        setenv kernelname uImage;
-        setenv itbcfg     ;
-        setenv imgbootcmd bootm;
-        run check_dtb;
-        run check_ramdisk;"
+setenv bootargs "${consolecfg} root=${rootfs} rootfstype=${rootfstype} rootwait rw earlyprintk ${opts}";
+load mmc ${mmcbootdev}:${mmcbootpart} ${kerneladdr} /boot/zImage;
+load mmc ${mmcbootdev}:${mmcbootpart} ${initrdaddr} /boot/uInitrd;
+load mmc ${mmcbootdev}:${mmcbootpart} ${ftdaddr} /boot/dtb/exynos5422-odroidxu4.dtb;
 
-#### Routine: setboot_zimg - prepare env to boot zImage
-setenv setboot_zimg "
-        setenv kerneladdr 0x40007FC0;
-        setenv kernelname zImage;
-        setenv itbcfg     ;
-        setenv imgbootcmd bootz;
-        run check_dtb;
-        run check_ramdisk;"
+bootz ${kerneladdr} ${initrdaddr} ${ftdaddr};
 
-#### Routine: boot_img - boot the kernel after env setup
-setenv boot_img "
-        run loadkernel;
-        run kernel_args;
-        '${imgbootcmd}' '${kerneladdr}${itbcfg}' '${initrd_addr}' '${fdt_addr}';"
-
-#### Routine: autoboot - choose proper boot path
-setenv autoboot "
-if test -e mmc 0:${mmcbootpart} Image.itb; then
-        echo Found kernel image: Image.itb;
-        run setboot_fit;
-        run boot_img;
-elif test -e mmc 0:${mmcbootpart} zImage; then
-        echo Found kernel image: zImage;
-        run setboot_zimg;
-        run boot_img;
-elif test -e mmc 0:${mmcbootpart} uImage; then
-        echo Found kernel image: uImage;
-        run setboot_uimg;
-        run boot_img;
-fi;"
-
-#### Execute the defined autoboot macro
-run autoboot
+# Generate boot.scr:
+# mkimage -c none -A arm -T script -d boot.cmd boot.scr
