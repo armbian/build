2f5325c0ce1f82bb0263082b70d3e244ec6c3782
Daniel (Fourdee) <daniel_haze@hotmail.com>
2016-10-05 17:42:28 +0100

    v6

--- distributions.sh
+++ distributions.sh
@@ -100,7 +100,7 @@ install_common()
 
 	display_alert "Installing headers" "$HEADERS_TMP" "info"
 	chroot $CACHEDIR/sdcard /bin/bash -c "dpkg -i /tmp/debs/${HEADERS_TMP}_${REVISION}_${ARCH}.deb" >> $DEST/debug/install.log 2>&1
-	
+
 	# install firmware
 	#if [[ -f $CACHEDIR/sdcard/tmp/debs/${FW_TMP}_${REVISION}_${ARCH}.deb ]]; then
 	#	display_alert "Installing firmware" "$FW_TMP" "info"
@@ -135,6 +135,10 @@ install_common()
 
 	# enable verbose kernel messages on first boot
 	touch $CACHEDIR/sdcard/boot/.verbose
+
+	# copy "first run automated config, optional user configured"
+ 	cp $SRC/lib/config/armbian_first_run.txt $CACHEDIR/sdcard/boot/armbian_first_run.txt
+
 }
 
 install_distribution_specific()
