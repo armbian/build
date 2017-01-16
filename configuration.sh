04a369383437d9ea1387ca5cd375e09dcff502eb
Igor Pecovnik <igor.pecovnik@gmail.com>
2017-01-16 14:50:06 +0100

    Desktop fine tuning - disable panel opacity, adding shadows to windows, changing donate URL to "donate page" instead of paypal URL, adding lightweight installer: Software boutique (currently not fully usable in arm64 - database is empty)

--- configuration.sh
+++ configuration.sh
@@ -139,7 +139,7 @@ PACKAGE_LIST_ADDITIONAL="alsa-utils btrfs-tools dosfstools hddtemp iotop iozone3
 PACKAGE_LIST_DESKTOP="xserver-xorg xserver-xorg-video-fbdev gvfs-backends gvfs-fuse xfonts-base xinit nodm x11-xserver-utils xfce4 lxtask xterm mirage thunar-volman galculator \
 	gtk2-engines gtk2-engines-murrine gtk2-engines-pixbuf libgtk2.0-bin gcj-jre-headless xfce4-screenshooter libgnome2-perl gksu bluetooth \
 	network-manager-gnome xfce4-notifyd gnome-keyring gcr libgck-1-0 libgcr-3-common p11-kit pasystray pavucontrol pulseaudio \
-	paman pavumeter pulseaudio-module-gconf pulseaudio-module-bluetooth blueman libpam-gnome-keyring libgl1-mesa-dri mpv"
+	paman pavumeter pulseaudio-module-gconf pulseaudio-module-bluetooth blueman libpam-gnome-keyring libgl1-mesa-dri mpv mate-utils ubuntu-mate-welcome mate-settings-daemon"
 
 PACKAGE_LIST_EXCLUDE="xfce4-mixer"
 
