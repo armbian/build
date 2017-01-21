5aff3a355a9400740b6b07d287254c937e0dbce1
zador-blood-stained <zador-blood-stained@users.noreply.github.com>
2017-01-21 23:19:03 +0300

    Fix and add missing file for cubox boot script

--- config/bootscripts/boot-cubox.cmd
+++ config/bootscripts/boot-cubox.cmd
@@ -8,14 +8,20 @@ run autodetectfdt
 
 # default values
 setenv rootdev "/dev/mmcblk0p1"
-
-setenv load_addr "0x10800000"
-setenv ramdisk_addr 0x14800000
 setenv verbosity "1"
 setenv console "display"
 setenv disp_mode "1920x1080m60"
 
-if load mmc 0 ${load_addr} /boot/armbianEnv.txt || load mmc 0 ${load_addr} armbianEnv.txt; then
+# additional values
+setenv load_addr "0x10800000"
+setenv ramdisk_addr "0x14800000"
+
+# next/dev kernels have another DT file name
+if ext2load mmc 0 0x00000000 /boot/.next || ext2load mmc 0 0x00000000 .next; then
+	setenv fdt_file "imx6q-cubox-i.dtb"
+fi
+
+if ext2load mmc 0 ${load_addr} /boot/armbianEnv.txt || ext2load mmc 0 ${load_addr} armbianEnv.txt; then
 	env import -t ${load_addr} ${filesize}
 fi
 
@@ -23,9 +29,11 @@ if test "${console}" = "display" || test "${console}" = "both"; then setenv cons
 if test "${console}" = "serial" || test "${console}" = "both"; then setenv consoleargs "${consoleargs} console=ttymxc0,115200"; fi
 
 setenv bootargs "root=${rootdev} rootfstype=ext4 rootwait ${consoleargs} video=mxcfb0:dev=hdmi,${disp_mode},if=RGB24,bpp=32 rd.dm=0 rd.luks=0 rd.lvm=0 raid=noautodetect pci=nomsi vt.global_cursor_default=0 loglevel=${verbosity} ${extraargs}"
-ext2load mmc 0 ${fdt_addr} /boot/dtb/${fdt_file} || fatload mmc 0 ${fdt_addr} /dtb/${fdt_file} || ext4load mmc 0 ${fdt_addr} /dtb/${fdt_file}
-ext2load mmc 0 ${ramdisk_addr} /boot/uInitrd || fatload mmc 0 ${ramdisk_addr} uInitrd || ext4load mmc 0 ${ramdisk_addr} uInitrd
-ext2load mmc 0 ${loadaddr} /boot/zImage || fatload mmc 0 ${loadaddr} zImage || ext4load mmc 0 ${loadaddr} zImage
+ext2load mmc 0 ${fdt_addr} /boot/dtb/${fdt_file} || fatload mmc 0 ${fdt_addr} /dtb/${fdt_file} || ext2load mmc 0 ${fdt_addr} /dtb/${fdt_file}
+ext2load mmc 0 ${ramdisk_addr} /boot/uInitrd || fatload mmc 0 ${ramdisk_addr} uInitrd || ext2load mmc 0 ${ramdisk_addr} uInitrd
+ext2load mmc 0 ${loadaddr} /boot/zImage || fatload mmc 0 ${loadaddr} zImage || ext2load mmc 0 ${loadaddr} zImage
+
 bootz ${loadaddr} ${ramdisk_addr} ${fdt_addr}
+
 # Recompile with:
-# mkimage -C none -A arm -T script -d /boot/boot.cmd /boot/boot.scr 
+# mkimage -C none -A arm -T script -d /boot/boot.cmd /boot/boot.scr
