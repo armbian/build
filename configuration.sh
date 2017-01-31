e52d363a8b4c2c12358cda74a4f322c827de59d7
zador-blood-stained <zador-blood-stained@users.noreply.github.com>
2017-01-31 17:48:32 +0300

    Add standard firmware package to Xenial images

--- configuration.sh
+++ configuration.sh
@@ -152,7 +152,7 @@ case $RELEASE in
 	PACKAGE_LIST_DESKTOP="$PACKAGE_LIST_DESKTOP mozo pluma iceweasel policykit-1-gnome eject"
 	;;
 	xenial)
-	PACKAGE_LIST_RELEASE="man-db wget nano"
+	PACKAGE_LIST_RELEASE="man-db wget nano linux-firmware"
 	PACKAGE_LIST_DESKTOP="$PACKAGE_LIST_DESKTOP thunderbird firefox gnome-icon-theme-full tango-icon-theme language-selector-gnome paprefs numix-gtk-theme"
 	[[ $ARCH == armhf ]] && PACKAGE_LIST_DESKTOP="$PACKAGE_LIST_DESKTOP mate-utils ubuntu-mate-welcome mate-settings-daemon"
 	;;
