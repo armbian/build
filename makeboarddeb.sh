6735c5e5404c3962d5b8ef479d438d956a2fa63c
Igor Pecovnik <igor.pecovnik@gmail.com>
2017-04-06 10:59:52 +0200

    Add armbian-config to MOTD instead.

--- makeboarddeb.sh
+++ makeboarddeb.sh
@@ -233,7 +233,6 @@ create_board_package()
 	install -m 755 $SRC/lib/scripts/update-motd.d/* $destination/etc/update-motd.d/
 	install -m 755 $SRC/lib/scripts/check_first_login_reboot.sh 	$destination/etc/profile.d
 	install -m 755 $SRC/lib/scripts/check_first_login.sh 			$destination/etc/profile.d
-	install -m 755 $SRC/lib/scripts/check_load_configurator.sh		$destination/etc/profile.d
 
 	install -m 755 $SRC/lib/scripts/apt-updates $destination/usr/lib/armbian/apt-updates
 
--- scripts/check_first_login_reboot.sh
+++ scripts/check_first_login_reboot.sh
@@ -2,7 +2,13 @@
 
 # only do this for interactive shells
 if [ "$-" != "${-#*i}" ]; then
-    if [ -f "/var/run/.reboot_required" ]; then
-        printf "\n[\e[0;91m Kernel was updated, please reboot\x1B[0m ]\n"
-    fi
+	printf "\n"
+	if [ -f "/var/run/.reboot_required" ]; then
+		printf "[\e[0;91m Kernel was updated, please reboot\x1B[0m ]"
+	fi
+	if [ -f "/usr/bin/armbian-config" ]; then
+		printf "[ Execute \e[0;91marmbian-config\x1B[0m to change some system settings ]\n\n"
+		elif [ -f "/var/run/.reboot_required" ]; then
+		printf "\n\n"
+	fi
 fi
