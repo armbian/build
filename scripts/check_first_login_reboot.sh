76aa377cd44b38009f56ba64eb64968041879884
Igor Pecovnik <igor.pecovnik@gmail.com>
2017-04-09 12:35:46 +0200

    Change text for armbian-config

--- scripts/check_first_login_reboot.sh
+++ scripts/check_first_login_reboot.sh
@@ -7,7 +7,7 @@ if [ "$-" != "${-#*i}" ]; then
 		printf "[\e[0;91m Kernel was updated, please reboot\x1B[0m ]"
 	fi
 	if [ -f "/usr/bin/armbian-config" ]; then
-		printf "[ Execute \e[0;91marmbian-config\x1B[0m to change some system settings ]\n\n"
+		printf "[\e[0;31m General system configuration\x1B[0m: \e[1marmbian-config\e[0m ]\n\n"
 		elif [ -f "/var/run/.reboot_required" ]; then
 		printf "\n\n"
 	fi
