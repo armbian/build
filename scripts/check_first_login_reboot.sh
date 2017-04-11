00ed67e03d03a7ae536e7209a0dac19322a916a4
Igor Pecovnik <igor.pecovnik@gmail.com>
2017-04-11 18:12:33 +0200

    Moving armbian-config to FAQ motd

--- scripts/check_first_login_reboot.sh
+++ scripts/check_first_login_reboot.sh
@@ -4,11 +4,6 @@
 if [ "$-" != "${-#*i}" ]; then
 	printf "\n"
 	if [ -f "/var/run/.reboot_required" ]; then
-		printf "[\e[0;91m Kernel was updated, please reboot\x1B[0m ]"
-	fi
-	if [ -f "/usr/bin/armbian-config" ]; then
-		printf "[\e[0;31m General system configuration\x1B[0m: \e[1marmbian-config\e[0m ]\n\n"
-		elif [ -f "/var/run/.reboot_required" ]; then
-		printf "\n\n"
+		printf "[\e[0;91m Kernel was updated, please reboot\x1B[0m ]\n\n"
 	fi
 fi
