7e1df657dcf3f88a56698539b5d628e64db82985
zador-blood-stained <zador-blood-stained@users.noreply.github.com>
2017-04-23 22:31:28 +0300

    Revert "Add CRC32 hash to image name to let Etcher verify download integrity"
    https://github.com/igorpecovnik/lib/commit/a23979ef8af8abf208feaaec38f597b414e83e97#commitcomment-21871965
    
    This reverts commit a23979ef8af8abf208feaaec38f597b414e83e97.

--- debootstrap-ng.sh
+++ debootstrap-ng.sh
@@ -471,16 +471,13 @@ create_image()
 	losetup -d $LOOP
 	rm -rf --one-file-system $CACHEDIR/$DESTIMG $CACHEDIR/$MOUNT
 	mkdir -p $CACHEDIR/$DESTIMG
-	# calculate CRC32 for OS image verification with Etcher
-	crc32checksum=$(crc32 $CACHEDIR/${SDCARD}.raw)
-	cp $CACHEDIR/$SDCARD/etc/armbian.txt $CACHEDIR/$DESTIMG/
-	mv $CACHEDIR/${SDCARD}.raw $CACHEDIR/$DESTIMG/${version}_${crc32checksum}.img
+	cp $CACHEDIR/$SDCARD/etc/armbian.txt $CACHEDIR/$DESTIMG
+	mv $CACHEDIR/${SDCARD}.raw $CACHEDIR/$DESTIMG/${version}.img
 	if [[ $BUILD_ALL != yes ]]; then
-		mv $CACHEDIR/$DESTIMG/${version}_${crc32checksum}.img $DEST/images/
+		mv $CACHEDIR/$DESTIMG/${version}.img $DEST/images/${version}.img
 		rm -rf $CACHEDIR/$DESTIMG
 	fi
-	
-	display_alert "Done building" "$DEST/images/${version}_${crc32checksum}.img" "info"
+	display_alert "Done building" "$DEST/images/${version}.img" "info"
 
 } #############################################################################
 
