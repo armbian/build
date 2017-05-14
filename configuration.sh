b30fcea3f95804175199bc1865a7e39cdf07cf73
zador-blood-stained <zador-blood-stained@users.noreply.github.com>
2017-05-14 17:59:04 +0300

    Update sunxi-next branch to 4.11

--- configuration.sh
+++ configuration.sh
@@ -39,7 +39,7 @@ else
 	MAINLINE_KERNEL_SOURCE='git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git'
 fi
 # allow upgrades for same major.minor versions
-ARMBIAN_MAINLINE_KERNEL_VERSION='4.10'
+ARMBIAN_MAINLINE_KERNEL_VERSION='4.11'
 MAINLINE_KERNEL_BRANCH=tag:v$(wget -qO- https://www.kernel.org/finger_banner | awk '{print $NF}' | grep -oE "^${ARMBIAN_MAINLINE_KERNEL_VERSION//./\\.}\.?[[:digit:]]*" | tail -1)
 MAINLINE_KERNEL_DIR='linux-vanilla'
 
