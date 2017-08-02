9cbcaa49fb5c2b0cf1a62e354a8bfd2db4682034
Igor Pecovnik <igor.pecovnik@gmail.com>
2017-08-02 13:03:46 +0200

    Missing packages, two for armbian-config, other for Stretch

--- lib/configuration.sh
+++ lib/configuration.sh
@@ -120,7 +120,7 @@ PACKAGE_LIST_DESKTOP="xserver-xorg xserver-xorg-video-fbdev gvfs-backends gvfs-f
 	gtk2-engines gtk2-engines-murrine gtk2-engines-pixbuf libgtk2.0-bin gcj-jre-headless xfce4-screenshooter libgnome2-perl gksu bluetooth \
 	network-manager-gnome xfce4-notifyd gnome-keyring gcr libgck-1-0 libgcr-3-common p11-kit pasystray pavucontrol pulseaudio \
 	paman pavumeter pulseaudio-module-gconf pulseaudio-module-bluetooth blueman libpam-gnome-keyring libgl1-mesa-dri mpv \
-	libreoffice-writer libreoffice-style-tango libreoffice-gtk policykit-1 fbi profile-sync-daemon"
+	libreoffice-writer libreoffice-style-tango libreoffice-gtk policykit-1 fbi profile-sync-daemon expect rcconf"
 
 # Release specific packages
 case $RELEASE in
@@ -134,7 +134,7 @@ case $RELEASE in
 	[[ $ARCH == armhf ]] && PACKAGE_LIST_DESKTOP="$PACKAGE_LIST_DESKTOP mate-utils ubuntu-mate-welcome mate-settings-daemon"
 	;;
 	stretch)
-	PACKAGE_LIST_RELEASE="man-db less kbd"
+	PACKAGE_LIST_RELEASE="man-db less kbd net-tools"
 	;;
 esac
 
