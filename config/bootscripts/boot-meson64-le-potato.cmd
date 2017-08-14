1e54241c860e58f4c1308e4c22b6edd2c19897f7
Tonymac32 <tonymckahan@gmail.com>
2017-08-14 00:46:58 -0400

    bootscript meson64 le potato cleanup
    boots 3.14 and 4.13 kernels, 4.13 not functional as yet, sits at login

--- config/bootscripts/boot-meson64-le-potato.cmd
+++ config/bootscripts/boot-meson64-le-potato.cmd
@@ -1,11 +1,27 @@
-if test -e mmc 0:1 boot/.next; then setenv condev "console=ttyAML0,115200n8"; else setenv condev "console=ttyS0,115200n8 console=tty0"; fi
+if test -e mmc 0:1 boot/.next; 
+	then setenv condev "console=ttyAML0,115200n8"; 
+	else setenv condev "console=ttyS0,115200n8 earlyprintk=aml-uart,0xc81004c0"; fi
 
-setenv bootargs "root=/dev/mmcblk0p1 ${condev} hdmimode=${hdmimode} hdmitx=cecf logo=osd1,loaded,${fb_addr},${hdmimode} initrd=${initrd_start},${initrd_size}"
-${bloader} ${loadaddr} /boot/zImage
-${bloader} ${dtb_mem_addr} /boot/dtb/gxl_p212_2g.dtb
-fdt addr ${dtb_mem_addr}
+if test -e mmc 0:1 boot/.next; 
+	then setenv rootdev "/dev/mmcblk1p1"; 
+	else setenv rootdev "/dev/mmcblk0p1"; fi
+
+if test -e mmc 0:1 boot/.next; 
+	then setenv bootargs "root=${rootdev} rootwait rootflags=data=writeback rw rootfstype=ext4 ${condev} no_console_suspend consoleblank=0 fsck.repair=yes loglevel=5 net.ifnames=0";
+	else setenv bootargs "root=${rootdev} rootwait rootflags=data=writeback rw rootfstype=ext4 ${condev} no_console_suspend consoleblank=0 hdmimode=1080p60hz m_bpp=24 loglevel=5 net.ifnames=0"; fi
+
 ${bloader} ${initrd_start} /boot/uInitrd
 
-if test -e mmc 0:1 boot/.next; then ${bloader} ${dtb_mem_addr} boot/dtb/amlogic/meson-gxl-s905x-libretech-cc.dtb; fi
-if test -e mmc 0:1 boot/.next; then ${bloader} 0x01080000 boot/uImage; fi
-if test -e mmc 0:1 boot/.next; then bootm 0x01080000 ${initrd_start} ${dtb_mem_addr}; else booti ${loadaddr} ${initrd_start} ${dtb_mem_addr}; fi
+if test -e mmc 0:1 boot/.next; 
+	then ${bloader} ${dtb_mem_addr} boot/dtb/amlogic/meson-gxl-s905x-libretech-cc.dtb; 
+	else ${bloader} ${dtb_mem_addr} boot/dtb/meson-gxl-s905x-libretech-cc.dtb; fi
+
+fdt addr ${dtb_mem_addr}
+
+if test -e mmc 0:1 boot/.next; 
+	then ${bloader} 0x01080000 boot/uImage; 
+	else ${bloader} ${loadaddr} /boot/zImage; fi
+
+if test -e mmc 0:1 boot/.next; 
+	then bootm 0x01080000 ${initrd_start} ${dtb_mem_addr}; 
+	else booti ${loadaddr} ${initrd_start} ${dtb_mem_addr}; fi
