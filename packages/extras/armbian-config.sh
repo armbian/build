fd9a64d6d405266ca2bd7b1a50a0996f2a1d5cd6
Igor Pecovnik <igor.pecovnik@gmail.com>
2018-01-03 14:22:55 +0100

    Add dependencies to armbian-config so we don't need to check and install this

--- packages/extras/armbian-config.sh
+++ packages/extras/armbian-config.sh
@@ -24,7 +24,7 @@ compile_armbian-config()
 	Architecture: all
 	Maintainer: $MAINTAINER <$MAINTAINERMAIL>
 	Replaces: armbian-bsp
-	Depends: bash, bc, expect, rcconf, dialog
+	Depends: bash, bc, expect, rcconf, dialog, debconf-utils, unzip, build-essential, html2text, apt-transport-https, html2text
 	Recommends: network-manager, armbian-bsp
 	Section: utils
 	Priority: optional
