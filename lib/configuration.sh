7fccf5dfa4cbd6570ed977b07cc56b3404e3cd3e
Thomas Kaiser <github@kaiser-edv.de>
2018-02-12 13:20:58 +0100

    Prevent Bionic builds with outdated kernels
    https://forum.armbian.com/topic/6420-preparing-for-ubuntu-1804/?do=findComment&comment=49148

--- lib/configuration.sh
+++ lib/configuration.sh
@@ -75,6 +75,7 @@ if [[ -f $SRC/userpatches/sources/$LINUXFAMILY.conf ]]; then
 fi
 
 [[ $RELEASE == stretch && $CAN_BUILD_STRETCH != yes ]] && exit_with_error "Building Debian Stretch images with selected kernel is not supported"
+[[ $RELEASE == bionic && $CAN_BUILD_STRETCH != yes ]] && exit_with_error "Building Ubuntu Bionic images with selected kernel is not supported"
 
 [[ -n $ATFSOURCE && -z $ATF_USE_GCC ]] && exit_with_error "Error in configuration: ATF_USE_GCC is unset"
 [[ -z $UBOOT_USE_GCC ]] && exit_with_error "Error in configuration: UBOOT_USE_GCC is unset"
