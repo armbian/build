e51115a6b5d61342d596c784bd60b4929c20cc85
Staars <Baars@gmx.de>
2018-03-08 18:17:25 +0100

    Use serialcon from u-boot-environment
    Only useful with other patch, that writes SERIALCON to /boot/armbianEnv.txt at compile time.

--- config/bootscripts/boot-rk3328.cmd
+++ config/bootscripts/boot-rk3328.cmd
@@ -22,7 +22,7 @@ fi
 if test "${logo}" = "disabled"; then setenv logo "logo.nologo"; fi
 
 if test "${console}" = "display" || test "${console}" = "both"; then setenv consoleargs "console=tty1"; fi
-if test "${console}" = "serial" || test "${console}" = "both"; then setenv consoleargs "${consoleargs} console=ttyFIQ0,1500000"; fi
+if test "${console}" = "serial" || test "${console}" = "both"; then setenv consoleargs "${consoleargs} console=${serialcon},1500000"; fi
 
 # get PARTUUID of first partition on SD/eMMC the boot script was loaded from
 if test "${devtype}" = "mmc"; then part uuid mmc ${devnum}:1 partuuid; fi
