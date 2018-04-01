17f67e5300793c7c1caf0315c177cdc62ca3199f
Alexandre Aubin <alexandre.aubin@cern.ch>
2018-04-01 15:29:44 +0200

    Advice to run postinstall at first boot

--- userpatches/overlay/check_first_login.sh
+++ userpatches/overlay/check_first_login.sh
@@ -5,8 +5,13 @@
 if [ -n "$BASH_VERSION" ] && [ "$-" != "${-#*i}" ]; then
 	while [ -f "/root/.not_logged_in_yet" ]; do
 		rm "/root/.not_logged_in_yet"
+        printf ""
+        printf "Congratulation on setting up your YunoHost server !\n"
+        printf "\n"
+        printf "To finish the installation, you should run :\n"
+        printf "   yunohost tools postinstall \n"
+        printf ""
 	done
-    printf "Hello from the first boot script !\n"
     # Display reboot recommendation if necessary
     if [[ -f /var/run/resize2fs-reboot ]]; then
         printf "\n\n\e[0;91mWarning: a reboot is needed to finish resizing the filesystem \x1B[0m \n"
