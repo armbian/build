4742f349df43c52f98c3c729cb9382dae3eb6056
Igor Pecovnik <igor.pecovnik@gmail.com>
2018-04-08 21:57:27 +0200

    Remove deprecated dependancy

--- packages/extras/armbian-config.sh
+++ packages/extras/armbian-config.sh
@@ -24,7 +24,7 @@ compile_armbian-config()
 	Architecture: all
 	Maintainer: $MAINTAINER <$MAINTAINERMAIL>
 	Replaces: armbian-bsp
-	Depends: bash, bc, expect, rcconf, dialog, unzip, build-essential, apt-transport-https, libpam-google-authenticator
+	Depends: bash, bc, expect, dialog, unzip, build-essential, apt-transport-https, libpam-google-authenticator
 	Recommends: network-manager, armbian-bsp
 	Section: utils
 	Priority: optional
