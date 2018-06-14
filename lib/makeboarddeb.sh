349bd61037eebadb178cf21304e2af507cf12368
Igor Pečovnik <igor.pecovnik@gmail.com>
2018-06-14 15:35:43 +0000

    Wrong services setup at package install. fixing

--- lib/makeboarddeb.sh
+++ lib/makeboarddeb.sh
@@ -102,7 +102,7 @@ create_board_package()
 	if [ remove = "\$1" ] || [ abort-install = "\$1" ]; then
 		dpkg-divert --package linux-${RELEASE}-root-${DEB_BRANCH}${BOARD} --remove --rename \
 			--divert /etc/mpv/mpv-dist.conf /etc/mpv/mpv.conf
-		systemctl disable armbian-optimize-hardware.service armbian-ram-logging.service armbian-resize-filesystem.service armbian-setup-hardware.service >/dev/null 2>&1
+		systemctl disable armbian-hardware-monitor.service armbian-hardware-optimize.service armbian-zram-config.service armbian-ram-logging.service >/dev/null 2>&1
 	fi
 	exit 0
 	EOF
@@ -127,7 +127,7 @@ create_board_package()
 		mv /etc/systemd/system/log2ram.service /etc/systemd/system/log2ram.dpkg-old
 	fi
 
-	systemctl --no-reload enable armbian-firstrun.service armbian-firstrun-config.service armbian-optimize-hardware.service armbian-ram-logging.service armbian-resize-filesystem.service armbian-setup-hardware.service >/dev/null 2>&1
+	systemctl --no-reload enable armbian-hardware-monitor.service armbian-hardware-optimize.service armbian-zram-config.service armbian-ram-logging.service >/dev/null 2>&1
 	exit 0
 	EOF
 
