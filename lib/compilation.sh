d227269c16a31ef3ee75160337fec89f5846acd8
Igor Pečovnik <igor.pecovnik@gmail.com>
2018-07-04 13:14:45 +0000

    Adjust patched for 4.17.y / sunxi-next - adjust both configurations - removing FAT support from u-boot (breaks if you try to save)
    Tested those boards:
    
    Cubietruck: wlan fails http://ix.io/1fYS USB OK, HDMI yes
    Bananapi R40: http://ix.io/1fZm USB OK, HDMI yes
    Lime A64: USB no, HDMI no, wireless buggy, eMMC yes
    Orangepi prime H5: OK http://ix.io/1fZJ DVFS no
    Orangepi2e: DVFS OK, HDMI OK, net OK, wifi OK, eMMC ok,  http://ix.io/1fZT

--- lib/compilation.sh
+++ lib/compilation.sh
@@ -149,6 +149,7 @@ compile_uboot()
 		[[ -f .config ]] && sed -i 's/CONFIG_LOCALVERSION=""/CONFIG_LOCALVERSION="-armbian"/g' .config
 		[[ -f .config ]] && sed -i 's/CONFIG_LOCALVERSION_AUTO=.*/# CONFIG_LOCALVERSION_AUTO is not set/g' .config
 		if [[ $BOOTBRANCH == "tag:v2018".* ]]; then
+			[[ -f .config ]] && sed -i 's/^.*CONFIG_ENV_IS_IN_FAT.*/# CONFIG_ENV_IS_IN_FAT is not set/g' .config
 			[[ -f .config ]] && sed -i 's/^.*CONFIG_ENV_IS_IN_EXT4.*/CONFIG_ENV_IS_IN_EXT4=y/g' .config
 			[[ -f .config ]] && sed -i 's/^.*CONFIG_ENV_IS_IN_MMC.*/# CONFIG_ENV_IS_IN_MMC is not set/g' .config
 			[[ -f .config ]] && sed -i 's/^.*CONFIG_ENV_IS_NOWHERE.*/# CONFIG_ENV_IS_NOWHERE is not set/g' .config | echo "# CONFIG_ENV_IS_NOWHERE is not set" >> .config
