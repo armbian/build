5d57dbea3ad2e3fe44911ba0c1289751db02a94b
Igor Pecovnik <igor.pecovnik@gmail.com>
2018-09-03 10:21:07 +0200

    OrangePi win GMAC is very unstable on gigabit. Limit it down to 100Mb solve problems + enlarge desktop image size for 5%

--- lib/debootstrap-ng.sh
+++ lib/debootstrap-ng.sh
@@ -362,7 +362,7 @@ prepare_partitions()
 				# Hardcoded overhead +25% is needed for desktop images,
 				# for CLI it could be lower. Align the size up to 4MiB
 				if [[ $BUILD_DESKTOP == yes ]]; then
-					local sdsize=$(bc -l <<< "scale=0; ((($imagesize * 1.25) / 1 + 0) / 4 + 1) * 4")
+					local sdsize=$(bc -l <<< "scale=0; ((($imagesize * 1.30) / 1 + 0) / 4 + 1) * 4")
 				else
 					local sdsize=$(bc -l <<< "scale=0; ((($imagesize * 1.20) / 1 + 0) / 4 + 1) * 4")
 				fi
