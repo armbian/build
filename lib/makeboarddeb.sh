05d926de1fd236988b1d906fc89c21f141d01d60
Igor Pecovnik <igor.pecovnik@gmail.com>
2018-09-04 10:41:30 +0200

    Tiny bugfix. Bionic related. Upgrade stalls since file is removed. Fixed with this commit.

--- lib/makeboarddeb.sh
+++ lib/makeboarddeb.sh
@@ -84,7 +84,6 @@ create_board_package()
 	[ -f "/etc/update-motd.d/98-autoreboot-warn" ] && rm /etc/update-motd.d/98-autoreboot-warn
 	[ -f "/etc/update-motd.d/99-point-to-faq" ] && rm /etc/update-motd.d/99-point-to-faq
 	# Remove Ubuntu junk
-	[ -f "/etc/update-motd.d/50-motd-news" ] && rm /etc/update-motd.d/50-motd-news
 	[ -f "/etc/update-motd.d/80-esm" ] && rm /etc/update-motd.d/80-esm
 	[ -f "/etc/update-motd.d/80-livepatch" ] && rm /etc/update-motd.d/80-livepatch
 	# Remove distro unattended-upgrades config
