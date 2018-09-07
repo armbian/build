9c7ce48f2b6cef71080ee6f04cca6f521f98df96
Igor Pecovnik <igor.pecovnik@gmail.com>
2018-09-07 15:02:11 +0200

    Make Espressobin boot script CRYPTOROOT compatible

--- config/bootscripts/boot-espressobin.cmd
+++ config/bootscripts/boot-espressobin.cmd
@@ -9,7 +9,6 @@ setenv verbosity "1"
 setenv rootfstype "ext4"
 
 # additional values
-setenv initrd_image "boot/uInitrd"
 setenv ethaddr "F0:AD:4E:03:64:7F"
 
 load ${boot_interface} ${devnum}:1 ${scriptaddr} ${prefix}armbianEnv.txt
@@ -17,13 +16,13 @@ env import -t ${scriptaddr} ${filesize}
 
 setenv bootargs "$console root=${rootdev} rootfstype=${rootfstype} rootwait loglevel=${verbosity} usb-storage.quirks=${usbstoragequirks} mtdparts=spi0.0:1536k(uboot),64k(uboot-environment),-(reserved) ${extraargs}"
 
-setenv fdt_name_a boot/dtb/marvell/armada-3720-community.dtb
-setenv fdt_name_b boot/dtb/marvell/armada-3720-espressobin.dtb
+setenv fdt_name_a dtb/marvell/armada-3720-community.dtb
+setenv fdt_name_b dtb/marvell/armada-3720-espressobin.dtb
 
-ext4load $boot_interface 0:1 $kernel_addr $image_name
-ext4load $boot_interface 0:1 $initrd_addr $initrd_image
-ext4load $boot_interface 0:1 $fdt_addr $fdt_name_a
-ext4load $boot_interface 0:1 $fdt_addr $fdt_name_b
+ext4load $boot_interface 0:1 $kernel_addr ${prefix}$image_name
+ext4load $boot_interface 0:1 $initrd_addr ${prefix}$initrd_image
+ext4load $boot_interface 0:1 $fdt_addr ${prefix}$fdt_name_a
+ext4load $boot_interface 0:1 $fdt_addr ${prefix}$fdt_name_b
 
 booti $kernel_addr $initrd_addr $fdt_addr
 # mkimage -C none -A arm -T script -d /boot/boot.cmd /boot/boot.scr
