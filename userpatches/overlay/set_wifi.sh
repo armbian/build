82ab2479dd8401541f4df7daa8aa76e7a9c0c0c4
jab-r <jonathan@openhealth.org>
2018-10-09 00:03:46 -0400

    Create set_wifi.sh

---
+++ userpatches/overlay/set_wifi.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# NOTE: It is copied to /tmp directory inside the image
+# and executed there inside chroot environment
+# so don't reference any files that are not already installed
+
+# NOTE: If you want to transfer files between chroot and host
+# userpatches/overlay directory on host is bind-mounted to /tmp/overlay in chroot
+
+
+Main() {
+  read -p "WiFi SSID: " ssid_net
+  read -p "WiFi Password: " ssid_pwd
+  sed -i "s/ssid=[\"]*[\"]/ssid=[\"]$ssid_net[\"]/" /etc/wpa_supplicant/wpa_supplicant.conf
+  sed -i "s/psk=[\"]*[\"]/psk=[\"]$ssid_pwd[\"]/" /etc/wpa_supplicant/wpa_supplicant.conf
+} #Main
