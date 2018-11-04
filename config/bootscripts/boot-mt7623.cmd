1b60a94e8c625c50da369162344052753ed2e64a
chwe17 <weberc18@gmail.com>
2018-11-04 16:41:11 +0100

    remove unneeded stuff, apply forgotten stuff

--- config/bootscripts/boot-mt7623.cmd
+++ config/bootscripts/boot-mt7623.cmd
@@ -5,7 +5,7 @@
 setenv root "/dev/mmcblk1p1"
 setenv rootfs "ext4"
 setenv verbosity "1"
-setenv bootargs initcall_debug console=ttyS2,115200n1 root=${root} rw rootfstype=${rootfs} rootwait audit=0 loglevel=${verbosity}
+setenv bootargs "initcall_debug console=ttyS2,115200n1 root=${root} rw rootfstype=${rootfs} rootwait audit=0 loglevel=${verbosity}"
 ext4load mmc ${mmcnum}:${mmcpart} ${fdtaddr} ${mmcfdtfile}
 ext4load mmc ${mmcnum}:${mmcpart} ${rdaddr} ${mmcinitrdfile}
 ext4load mmc ${mmcnum}:${mmcpart} ${kernel_addr_r} ${mmckernfile}
