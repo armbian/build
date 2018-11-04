eb479cea4e4823e308648306de28fbf86dfab68a
Igor Pecovnik <igor.pecovnik@gmail.com>
2018-11-04 21:28:10 +0100

    - drop dev and next, move into default. There is no intention to provide stock kernel - fixed board description, renamed to WIP, where it can be merged later - packing boot firmware to the u-boot package - docker dependencies (aufs will be added later. not essential) - attached to 4.19.y - loading armbianEnv.txt - UUID support (tested) - enabled ZRAM (tested) - enabled eMMC install (not quite working properly yet) - Bionic has some issues with systemd networking.

--- config/bootscripts/boot-mt7623.cmd
+++ config/bootscripts/boot-mt7623.cmd
@@ -2,14 +2,25 @@
 #
 # Please edit /boot/armbianEnv.txt to set supported parameters
 #
-setenv root "/dev/mmcblk1p1"
+setenv rootdev "/dev/mmcblk1p1"
 setenv rootfs "ext4"
 setenv verbosity "1"
-setenv bootargs "initcall_debug console=ttyS2,115200n1 root=${root} rw rootfstype=${rootfs} rootwait audit=0 loglevel=${verbosity}"
-ext4load mmc ${mmcnum}:${mmcpart} ${fdtaddr} ${mmcfdtfile}
-ext4load mmc ${mmcnum}:${mmcpart} ${rdaddr} ${mmcinitrdfile}
-ext4load mmc ${mmcnum}:${mmcpart} ${kernel_addr_r} ${mmckernfile}
-echo "Booting ${mmckernfile} ${mmcinitrdfile} ${mmcfdtfile} from: mmc ${mmcnum}:${mmcpart} using bootargs=${bootargs}"
+setenv devtype "mmc"
+setenv prefix "boot/"
+
+echo "Boot script loaded from device ${devnum}"
+
+if test -e ${devtype} ${devnum} ${prefix}armbianEnv.txt; then
+	load ${devtype} ${devnum} ${kernel_addr_r} ${prefix}armbianEnv.txt
+	env import -t ${kernel_addr_r} ${filesize}
+fi
+
+
+setenv bootargs "initcall_debug console=ttyS2,115200n1 root=${rootdev} rw rootfstype=${rootfs} rootwait audit=0 loglevel=${verbosity}"
+ext4load ${devtype} ${devnum}:${mmcpart} ${fdtaddr} ${mmcfdtfile}
+ext4load ${devtype} ${devnum}:${mmcpart} ${rdaddr} ${mmcinitrdfile}
+ext4load ${devtype} ${devnum}:${mmcpart} ${kernel_addr_r} ${mmckernfile}
+echo "Booting ${mmckernfile} ${mmcinitrdfile} ${mmcfdtfile} from: ${devtype} ${devnum}:${mmcpart} using bootargs=${bootargs}"
 bootz ${kernel_addr_r} ${rdaddr} ${fdtaddr}
 # Recompile with:
 # mkimage -C none -A arm -T script -d /boot/boot.cmd /boot/boot.scr
