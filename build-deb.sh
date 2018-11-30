8658ffeb281b958788d3d582646c1552f12527e9
Igor Pecovnik <igor.pecovnik@gmail.com>
2018-11-30 14:22:00 +0100

    New features: - overlay directory is not copied but merge mounted with overlayfs - package is not made if it already exists in apt.armbian.com - variables RFC, submodules relocate - DEBIAN scripting is added always, empty example from TEMPLATE directory - added example of armbian.build which uses for post preparing the packaga if repository is not ready to overlay (armbian-config as example). - small cleanup

--- build-deb.sh
+++ build-deb.sh
@@ -4,10 +4,12 @@ SRC="$(dirname "$(realpath "${BASH_SOURCE}")")"
 # fallback for Trusty
 [[ -z "${SRC}" ]] && SRC="$(pwd)"
 
-REVISION="5.67$SUBREVISION" # all boards have same revision
-ROOTPWD="1234" # Must be changed @first login
-[[ -z $MAINTAINER ]] && MAINTAINER="Igor Pecovnik" # deb signature
-[[ -z $MAINTAINERMAIL ]] && MAINTAINERMAIL="igor.pecovnik@****l.com" # deb signature
+REPOSITORY_PACKAGES="`wget -qO- https://apt.armbian.com/.packages.txt`"
+
+REVISION="5.67" # all boards have same revision
+MAINTAINER="Igor Pecovnik" # deb signature
+MAINTAINERMAIL="igor@armbian.com" # deb signature
+EXTERNAL_NEW="prebuilt"
 
 #echo $SRC
 DEST=$SRC/output
--- debs/go.sh
+++ debs/go.sh
@@ -1,75 +1,152 @@
+# Copyright (c) 2015 Igor Pecovnik, igor.pecovnik@gma**.com
+#
+# This file is licensed under the terms of the GNU General Public
+# License version 2. This program is licensed "as is" without any
+# warranty of any kind, whether express or implied.
+
+# This file is a part of the Armbian build script
+# https://github.com/armbian/build/
+
+# Functions:
+# find_deb_configs
+
+# cycle in deb subdirectories and look for armbian.config where are stored definitions to create DEB file
+#
 find_deb_configs(){
-IFS=$'\n'
-names=()
-dirs=( $(find debs -maxdepth 2 -mindepth 2 -not -path "*/TEMPLATE/*" ) )
 
-#echo $dirs
+	IFS=$'\n'
+	names=()
+	dirs=( $(find debs -maxdepth 2 -mindepth 2 -not -path "*/TEMPLATE/*" ) )
 
-# required for "for" command
-shopt -s nullglob dotglob
+	# required for "for" command
+	shopt -s nullglob dotglob
 
-# check subdirectories for debian package definitions
-for dir in "${dirs[@]}"; do
-	for config in ${dir%%:*}/armbian.config; do
-	#echo $config
-		names+=($(basename $config))
-		if [[ -f ${dir%%:*}/$names ]]; then
-			local location_source="$SRC/${dir%%:*}/"
-			local location_destination="$SRC/.tmp/${dir%%:*}/"
-			create_deb_control $location_source $location_destination
-
-			#echo $SRC/.tmp/${dir%%:*}/$names
-		fi
-	done
+	# check subdirectories for debian package definitions
+	for dir in "${dirs[@]}"; do
+		for config in ${dir%%:*}/armbian.config; do
+			names+=($(basename $config))
+				if [[ -f ${dir%%:*}/$names ]]; then
+					local location_lowerdir="$SRC/${dir%%:*}/"
+					local location_upperdir="$SRC/.tmp/.upperdir/${dir%%:*}/"
+					local location_workdir="$SRC/.tmp/.workdir/${dir%%:*}/"
+					local location_merged="$SRC/.tmp/.merged/${dir%%:*}/"
+					create_deb_package $location_lowerdir $location_upperdir $location_workdir $location_merged
+				fi
+		done
 done
 }
 
+
+
+
+# Helper for creating DEBIAN package scripts
+#
 function process_line()
 {
+local filename="$4/DEBIAN/"$(echo $2 | sed -e "s/^armbian.//")
 if [[ -f $1/$2 ]]; then
-	local filename="$3$PACKAGE/DEBIAN/"$(echo $2 | sed -e "s/^armbian.//")
 	postinst=$(bash $1/$2)
+else
+	postinst=$(bash $SRC/debs/TEMPLATE/$2)
+fi
 	while read -r line; do
     echo "$line" >> $filename
 	done <<< "$postinst"
 	chmod +x $filename
-fi
 }
 
-function create_deb_control ()
+
+
+
+# Create the package
+#
+function create_deb_package ()
 {
-	echo $1 $2
-	unset PACKAGE ARCH SECTION PRIORITY DEPENDS PROVIDES RECOMMENDS REPOSITORY
-	[[ -f $1/armbian.config ]] && source $1/armbian.config
-	[[ -z $PACKAGE ]] && echo "Error. Package name must be defined" && return 1
-	[[ -z $DESCRIPTION ]] && DESCRIPTION="Unnamed Armbian package"
-	[[ -n $REPOSITORY ]] && REPOSITORY=$REPOSITORY"/"
-	[[ $2 != "/" ]] && rm -rf $2 && mkdir -p $2/$PACKAGE $2/$PACKAGE/DEBIAN
-	[[ -d $1/overlay ]] && cp -rp $1/overlay/. $2/$PACKAGE/
-	local packagesize=$(du -sx --exclude DEBIAN	$2 | awk '{ print $1 }')
+	# $1 = sources directory
+	# $2 = destination
+	# $3 = work directory
+	# $4 = merged directory
 
-	process_line $1 "armbian.preinst" 	$2
-	process_line $1 "armbian.postinst"	$2
-	process_line $1 "armbian.prerm" 	$2
-	process_line $1 "armbian.postrm" 	$2
+	# reset variables
+	unset ARMBIAN_PKG_PACKAGE ARMBIAN_PKG_ARCH ARMBIAN_PKG_SECTION ARMBIAN_PKG_PRIORITY ARMBIAN_PKG_DEPENDS ARMBIAN_PKG_PROVIDES \
+	ARMBIAN_PKG_RECOMMENDS ARMBIAN_PKG_CONFLICTS ARMBIAN_PKG_REPLACES ARMBIAN_PKG_REPOSITORY ARMBIAN_PKG_DESCRIPTION \
+	ARMBIAN_PKG_MAINTAINER ARMBIAN_PKG_MAINTAINERMAIL
 
-	echo "Package: ${PACKAGE}"																	>  $2/$PACKAGE/DEBIAN/control
-	echo "Version: ${REVISION}" 																>> $2/$PACKAGE/DEBIAN/control
-	echo "Architecture: ${ARCH}" 																>> $2/$PACKAGE/DEBIAN/control
-	echo "Maintainer: ${MAINTAINER} ${MAINTAINERMAIL}" 											>> $2/$PACKAGE/DEBIAN/control
-	echo "Installed-Size: ${packagesize}" 														>> $2/$PACKAGE/DEBIAN/control
-	echo "Section: ${SECTION}"																	>> $2/$PACKAGE/DEBIAN/control
-	echo "Priority: ${PRIORITY}" 																>> $2/$PACKAGE/DEBIAN/control
-	[[ -n $DEPENDS ]] && 		echo "Depends: $(echo ${DEPENDS} | tr " " ,)"					>> $2/$PACKAGE/DEBIAN/control
-	[[ -n $PROVIDES ]] && 		echo "Provides: $(echo ${PROVIDES} | tr " " ,)"					>> $2/$PACKAGE/DEBIAN/control
-	[[ -n $RECOMMENDS ]] && 	echo "Recommends: $(echo ${RECOMMENDS} | tr " " ,)"				>> $2/$PACKAGE/DEBIAN/control
-	[[ -n $CONFLICTS ]] && 		echo "Conflicts: $(echo ${CONFLICTS} | tr " " ,)"				>> $2/$PACKAGE/DEBIAN/control
-	[[ -n $REPLACES ]] && 		echo "Replaces: $(echo ${REPLACES} | tr " " ,)"					>> $2/$PACKAGE/DEBIAN/control
-	echo -e "Homepage: ${HOMEPAGE}"																>> $2/$PACKAGE/DEBIAN/control
-	echo -e "Description: ${DESCRIPTION}\n"														>> $2/$PACKAGE/DEBIAN/control
+	# defaults
+	ARMBIAN_PKG_REVISION=$REVISION
+	ARMBIAN_PKG_MAINTAINER=$MAINTAINER
+	ARMBIAN_PKG_MAINTAINERMAIL=$MAINTAINERMAIL
+	ARMBIAN_PKG_HOMEPAGE="https://www.armbian.com"
+	ARMBIAN_PKG_ARCH=all
+	ARMBIAN_PKG_PRIORITY=optional
 
-	fakeroot dpkg-deb -b $2/$PACKAGE $DEST/debs/${REPOSITORY}$PACKAGE.deb
+	# read package configuration
+	source $1/armbian.config
+
+	local upperdir="$2${ARMBIAN_PKG_PACKAGE}_${ARMBIAN_PKG_REVISION}_${ARMBIAN_PKG_ARCH}"
+	local workdir="$3${ARMBIAN_PKG_PACKAGE}_${ARMBIAN_PKG_REVISION}_${ARMBIAN_PKG_ARCH}"
+	local mergeddir="$4${ARMBIAN_PKG_PACKAGE}_${ARMBIAN_PKG_REVISION}_${ARMBIAN_PKG_ARCH}"
+
+	# check if package already exists in repository
+	[[ -n $(echo $REPOSITORY_PACKAGES | grep "${ARMBIAN_PKG_PACKAGE}_${ARMBIAN_PKG_REVISION}_${ARMBIAN_PKG_ARCH}") && $EXTERNAL_NEW != "compile" ]] && echo "${ARMBIAN_PKG_PACKAGE}_${ARMBIAN_PKG_REVISION}_${ARMBIAN_PKG_ARCH} exists. Skip building" && return 1
+
+	# package name is mandatory
+	[[ -z $ARMBIAN_PKG_PACKAGE ]] && echo "Error in $1 Package name must be defined" && return 1
+	# overlay is mandatory
+	[[ ! -d $1overlay ]] && echo "Error in $1overlay Overlay directory is missing" && return 1
+
+	# description failover
+	[[ -z $ARMBIAN_PKG_DESCRIPTION ]] && ARMBIAN_PKG_DESCRIPTION="Unnamed Armbian package"
+
+	# add slash to the variable if subdirectory is defined
+	[[ -n $ARMBIAN_PKG_REPOSITORY ]] && ARMBIAN_PKG_REPOSITORY+="/"
+
+	# re-create directories
+	if [[ $upperdir != "/" && $workdir != "/" && $mergeddir != "/" ]]; then
+		rm -rf $upperdir $workdir $mergeddir
+		mkdir -p $upperdir/DEBIAN $workdir $mergeddir
+		# destination debs subdirectories if needed
+		mkdir -p $DEST/debs/${ARMBIAN_PKG_REPOSITORY}
+	fi
+
+	# merge directories with overlay so we don't need to copy anything
+	mount -t overlay overlay -olowerdir=$1overlay,upperdir=${upperdir},workdir=${workdir} ${mergeddir}
+
+	# execute package custom build script if defined
+	[[ -d $1/sources && -f $1/armbian.build ]] && source $1/armbian.build
+
+	# calculate package size
+	local packagesize=$(du -sx --exclude DEBIAN	$mergeddir | awk '{ print $1 }')
+
+	echo "Packagin sources: $mergeddir"
+
+	# compile DEBIAN scripts
+	process_line $1 "armbian.preinst" 	$2 $upperdir
+	process_line $1 "armbian.postinst"	$2 $upperdir
+	process_line $1 "armbian.prerm" 	$2 $upperdir
+	process_line $1 "armbian.postrm" 	$2 $upperdir
+
+	# create DEBIAN control file
+	echo "Package: ${ARMBIAN_PKG_PACKAGE}"																		>  $upperdir/DEBIAN/control
+	echo "Version: ${ARMBIAN_PKG_REVISION}" 																	>> $upperdir/DEBIAN/control
+	echo "Architecture: ${ARMBIAN_PKG_ARCH}" 																	>> $upperdir/DEBIAN/control
+	echo "Maintainer: ${ARMBIAN_PKG_MAINTAINER} ${ARMBIAN_PKG_MAINTAINERMAIL}" 									>> $upperdir/DEBIAN/control
+	echo "Installed-Size: ${packagesize}" 																		>> $upperdir/DEBIAN/control
+	echo "Section: ${ARMBIAN_PKG_SECTION}"																		>> $upperdir/DEBIAN/control
+	echo "Priority: ${ARMBIAN_PKG_PRIORITY}" 																	>> $upperdir/DEBIAN/control
+	[[ -n $ARMBIAN_PKG_DEPENDS ]] && 		echo "Depends: $(echo ${ARMBIAN_PKG_DEPENDS} | tr " " ,)"			>> $upperdir/DEBIAN/control
+	[[ -n $ARMBIAN_PKG_PROVIDES ]] && 		echo "Provides: $(echo ${ARMBIAN_PKG_PROVIDES} | tr " " ,)"			>> $upperdir/DEBIAN/control
+	[[ -n $ARMBIAN_PKG_RECOMMENDS ]] && 	echo "Recommends: $(echo ${ARMBIAN_PKG_RECOMMENDS} | tr " " ,)"		>> $upperdir/DEBIAN/control
+	[[ -n $ARMBIAN_PKG_CONFLICTS ]] && 		echo "Conflicts: $(echo ${ARMBIAN_PKG_CONFLICTS} | tr " " ,)"		>> $upperdir/DEBIAN/control
+	[[ -n $ARMBIAN_PKG_REPLACES ]] && 		echo "Replaces: $(echo ${ARMBIAN_PKG_REPLACES} | tr " " ,)"			>> $upperdir/DEBIAN/control
+	[[ -n $ARMBIAN_PKG_HOMEPAGE ]] &&		echo "Homepage: ${ARMBIAN_PKG_HOMEPAGE}"							>> $upperdir/DEBIAN/control
+	echo -e "Description: ${ARMBIAN_PKG_DESCRIPTION}\n"															>> $upperdir/DEBIAN/control
+	[[ -n $DEBUG_PKG ]] && cat $upperdir/DEBIAN/control
+	# build the package and save in the output/debs directories
+	fakeroot dpkg-deb -b $mergeddir $DEST/debs/${ARMBIAN_PKG_REPOSITORY}${ARMBIAN_PKG_PACKAGE}_${ARMBIAN_PKG_REVISION}_${ARMBIAN_PKG_ARCH}.deb
+	umount -l $mergeddir
+	# cleanup
+	if [[ $upperdir != "/" && $workdir != "/" && $mergeddir != "/" ]]; then rm -rf $upperdir $workdir $mergeddir; fi
 }
 
 find_deb_configs
-#create_deb_control
