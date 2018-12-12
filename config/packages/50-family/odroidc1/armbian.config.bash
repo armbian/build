470098efc682cec39ecc838c839c949b5d06d5fa
Igor Pecovnik <igor.pecovnik@gmail.com>
2018-12-12 19:11:52 +0100

    Added Odroid XU4 and C1

---
+++ config/packages/50-family/odroidc1/armbian.config.bash
@@ -0,0 +1,2 @@
+ARMBIAN_PKG_PACKAGE=armbian-family-odroidc1
+ARMBIAN_PKG_SECTION=utils
---
+++ config/packages/50-family/odroidxu4/armbian.config.bash
@@ -0,0 +1,3 @@
+ARMBIAN_PKG_PACKAGE=armbian-family-odroidxu4
+ARMBIAN_PKG_SECTION=kernel
+ARMBIAN_PKG_DESCRIPTION="Armbian tweaks for $FAMILY"
