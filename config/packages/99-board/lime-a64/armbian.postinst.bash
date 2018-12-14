71357d964af0c68ef769c9514d9838af9274c3e3
Igor Pecovnik <igor.pecovnik@gmail.com>
2018-12-14 09:36:13 +0100

    Enable Bluetooth on Lime A64

--- config/packages/99-board/lime-a64/armbian.postinst.bash
+++ config/packages/99-board/lime-a64/armbian.postinst.bash
@@ -7,11 +7,8 @@ cat <<EOF
 # read config
 [ -f /etc/armbian-release ] && . /etc/armbian-release
 
-
-if [ "\$BRANCH" = "default" ]; then
 # enable services
 systemctl --no-reload enable lime-a64-bluetooth.service >/dev/null 2>&1
-fi
 
 exit 0
 EOF
