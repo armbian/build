b9491fff731cd01ae75b33791157111c186d4441
Igor Pecovnik <igor.pecovnik@gmail.com>
2018-12-16 11:06:20 +0100

    Adjust MT7623 for packaging, added HDMI patches but couldn't get it to work.

--- config/packages/03-common/10-armbian-firmware/armbian.config.bash
+++ config/packages/03-common/10-armbian-firmware/armbian.config.bash
@@ -1,3 +1,4 @@
+ARMBIAN_PKG_INSTALL=no
 ARMBIAN_PKG_PACKAGE=armbian-firmware
 ARMBIAN_PKG_SECTION=kernel
 ARMBIAN_PKG_REPLACES="linux-firmware firmware-brcm80211 firmware-samsung firmware-realtek armbian-firmware-full"
