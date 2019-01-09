589979932b39d132a3e5837b2decbaed08c5f0f5
Aditya Prayoga <aditya@kobol.io>
2019-01-09 12:44:44 +0800

    family: mvebu: Move the family tweak into armbian-mvebu
    As the readme stated
    
    * armbian-bsp (this is now the same with one exception. It doesn't
    execute family_tweaks and family_tweaks_bsp. Those functions are getting
    manually implemented into the packages below)
    
    Move the implementation into armbian-mvebu package

--- config/packages/50-family/dedicated/mvebu/armbian.build.bash
+++ config/packages/50-family/dedicated/mvebu/armbian.build.bash
@@ -6,3 +6,6 @@ mkdir -p ${upperdir}/usr/sbin
 
 install -m 755 ${sectiondir}scripts/nand-sata-install/nand-sata-install ${upperdir}/usr/sbin
 install -m 644 ${sectiondir}scripts/nand-sata-install/exclude.txt ${upperdir}/usr/lib/nand-sata-install
+
+# Family tweaks
+chroot $SDCARD /bin/bash -c "apt-get -y -qq remove --auto-remove linux-sound-base alsa-base alsa-utils bluez>/dev/null 2>&1"
