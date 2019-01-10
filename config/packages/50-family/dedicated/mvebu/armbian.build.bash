54e8144747967a6a14e8ea4ff21a099656249af8
Aditya Prayoga <aditya@kobol.io>
2019-01-10 17:48:40 +0800

    family: mvebu: Differentiate between Ubuntu and Debian tweak
    Debian does not have linux-sound-base package therefore it failed to
    remove any of the package listed.

--- config/packages/50-family/dedicated/mvebu/armbian.build.bash
+++ config/packages/50-family/dedicated/mvebu/armbian.build.bash
@@ -8,4 +8,13 @@ install -m 755 ${sectiondir}scripts/nand-sata-install/nand-sata-install ${upperd
 install -m 644 ${sectiondir}scripts/nand-sata-install/exclude.txt ${upperdir}/usr/lib/nand-sata-install
 
 # Family tweaks
-chroot $SDCARD /bin/bash -c "apt-get -y -qq remove --auto-remove linux-sound-base alsa-base alsa-utils bluez>/dev/null 2>&1"
+case $RELEASE in
+	"jessie"|"stretch")
+		PACKAGE_TO_REMOVE="alsa-base alsa-utils bluez"
+		;;
+	"xenial"|"bionic")
+		PACKAGE_TO_REMOVE="linux-sound-base alsa-base alsa-utils bluez"
+		;;
+esac
+
+chroot $SDCARD /bin/bash -c "apt-get -y -qq remove --auto-remove $PACKAGE_TO_REMOVE >/dev/null 2>&1"
