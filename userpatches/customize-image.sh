0f1d6ddeb40fa86d2139542aedb517896099fc3f
Alexandre Aubin <alexandre.aubin@cern.ch>
2019-01-15 19:43:27 +0100

    Update armbian override stuff to get rid of annoying useless stuff for yunohost use case

--- userpatches/customize-image.sh
+++ userpatches/customize-image.sh
@@ -16,39 +16,34 @@ LINUXFAMILY=$2
 BOARD=$3
 BUILD_DESKTOP=$4
 
-Main() {
-    case $RELEASE in
-        jessie)
-            ;;
-        stretch)
-            InstallYunohostStretch
-            ;;
-    esac
-} # Main
+# We don't want the damn network-manager :/
+apt remove network-manager
+cat << EOF >> /etc/apt/preferences
+Package: network-manager
+Pin: release *
+Pin-Priority: -1
+EOF
 
-InstallYunohostStretch()
-{
-    # Override the first login script with our own (we don't care about desktop
-    # stuff + we don't want the user to manually create a user)
-    cp /tmp/overlay/check_first_login.sh /etc/profile.d/check_first_login.sh
-    cp /tmp/overlay/check_yunohost_is_installed.sh /etc/profile.d/check_yunohost_is_installed.sh
-    cp /tmp/overlay/armbian-motd /etc/default/armbian-motd
-    touch /root/.not_logged_in_yet
+# Override the first login script with our own (we don't care about desktop
+# stuff + we don't want the user to manually create a user)
+rm /etc/profile.d/armbian-check-first-login.sh
+cp /tmp/overlay/check_first_login.sh /etc/profile.d/check_first_login.sh
+cp /tmp/overlay/check_yunohost_is_installed.sh /etc/profile.d/check_yunohost_is_installed.sh
+cp /tmp/overlay/armbian-motd /etc/default/armbian-motd
+dpkg-divert --divert /root/armbian-check-first-login.sh --rename /etc/profile.d/armbian-check-first-login.sh
+dpkg-divert --divert /root/armbian-motd --rename /etc/default/armbian-motd
+touch /root/.not_logged_in_yet
 
-    # Avahi and mysql/mariadb needs to do some stuff which conflicts with the
-    # "change the root password asap" so we disable it temporarily....
-    chage -d 99999999 root
+# Avahi and mysql/mariadb needs to do some stuff which conflicts with the
+# "change the root password asap" so we disable it temporarily....
+chage -d 99999999 root
 
-    # Go to tmp and run the install script
-    cd /tmp/
-    wget -O install_yunohost https://install.yunohost.org/stretch
-    chmod +x /tmp/install_yunohost
-    ./install_yunohost -a
+# Go to tmp and run the install script
+cd /tmp/
+wget -O install_yunohost https://install.yunohost.org/stretch
+chmod +x /tmp/install_yunohost
+./install_yunohost -a
 
-    chage -d 0 root
-
-    # FIXME !!! Should clean stuff here (c.f. clean_image in script)
-    apt clean
-}
-
-Main "$@"
+# Clean stuff
+chage -d 0 root
+apt clean
