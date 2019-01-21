8c8f07dc2eb23cc36301e00e7f6a35c5ef113e39
Alexandre Aubin <alex.aubin@mailoo.org>
2019-01-21 19:51:55 +0100

    Not sure what I am doing but movide the file override after the yunohost install might be better

--- userpatches/customize-image.sh
+++ userpatches/customize-image.sh
@@ -16,7 +16,7 @@ LINUXFAMILY=$2
 BOARD=$3
 BUILD_DESKTOP=$4
 
-# Downgrade kernel to 4.14 because 4.19 is causing issues with Wifi drivers
+# Downgrade kernel to 4.14 because 4.19 is causing issues with Wifi drivers so far
 apt install linux-image-next-sunxi=5.67 -y --allow-downgrades || exit -1
 apt-mark hold linux-image-next-sunxi
 
@@ -29,6 +29,14 @@ Pin: release *
 Pin-Priority: -1
 EOF
 
+# Avahi and mysql/mariadb needs to do some stuff which conflicts with the
+# "change the root password asap" so we disable it temporarily....
+chage -d 99999999 root
+
+# Run the install script
+curl https://install.yunohost.org/stretch | bash -s -- -a
+rm /var/log/yunohost-installation*
+
 # Override the first login script with our own (we don't care about desktop
 # stuff + we don't want the user to manually create a user)
 rm /etc/profile.d/armbian-check-first-login.sh
@@ -38,14 +46,6 @@ cp /tmp/overlay/armbian-motd /etc/default/armbian-motd
 dpkg-divert --divert /root/armbian-check-first-login.sh --rename /etc/profile.d/armbian-check-first-login.sh
 dpkg-divert --divert /root/armbian-motd --rename /etc/default/armbian-motd
 touch /root/.not_logged_in_yet
-
-# Avahi and mysql/mariadb needs to do some stuff which conflicts with the
-# "change the root password asap" so we disable it temporarily....
-chage -d 99999999 root
-
-# Run the install script
-curl https://install.yunohost.org/stretch | bash -s -- -a
-rm /var/log/yunohost-installation*
 
 # Clean stuff
 chage -d 0 root
