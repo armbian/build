26cf4510eb5dd191b23ae07edff489faa663f8d5
Alexandre Aubin <alex.aubin@mailoo.org>
2019-02-10 01:07:33 +0100

    Improve comment idk

--- userpatches/customize-image.sh
+++ userpatches/customize-image.sh
@@ -31,12 +31,12 @@ EOF
 echo "auto eth0" > /etc/network/interfaces.d/eth0.conf
 echo "iface eth0 inet dhcp" >> /etc/network/interfaces.d/eth0.conf
 
-# Backports are evil
-sed -i '/backport/ s/^deb/#deb/' /etc/apt/sources.list
-
-# resolvconf stuff
+# Make sure resolv.conf points to DNSmasq (somehow networkmanager might have broke this)
 rm /etc/resolv.conf
 ln -s /etc/resolvconf/run/resolv.conf /etc/resolv.conf
+
+# Backports are evil
+sed -i '/backport/ s/^deb/#deb/' /etc/apt/sources.list
 
 # Avahi and mysql/mariadb needs to do some stuff which conflicts with the
 # "change the root password asap" so we disable it temporarily....
