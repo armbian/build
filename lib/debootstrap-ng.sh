4ba17f1003821e642db361412f0d71660dbd2489
may <mzhboy@gmail.com>
2019-05-07 01:26:04 +0800

    remove tmpfs mount on /tmp
    A zram block device will mount on /tmp by
    /usr/lib/armbian/armbian-zram-config.
    
    And instead, mount /media as tmpfs.

--- lib/debootstrap-ng.sh
+++ lib/debootstrap-ng.sh
@@ -469,7 +469,7 @@ prepare_partitions()
 		echo "UUID=$(blkid -s UUID -o value ${LOOP}p${bootpart}) /boot ${mkfs[$bootfs]} defaults${mountopts[$bootfs]} 0 2" >> $SDCARD/etc/fstab
 	fi
 	[[ $ROOTFS_TYPE == nfs ]] && echo "/dev/nfs / nfs defaults 0 0" >> $SDCARD/etc/fstab
-	echo "tmpfs /tmp tmpfs defaults,nosuid 0 0" >> $SDCARD/etc/fstab
+	echo "tmpfs /media tmpfs defaults,nosuid 0 0" >> $SDCARD/etc/fstab
 
 	# stage: adjust boot script or boot environment
 	if [[ -f $SDCARD/boot/armbianEnv.txt ]]; then
