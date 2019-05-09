ba9e9f7189677946e2ae96ff1bec7a21fa322aec
may <mzhboy@gmail.com>
2019-05-10 05:39:38 +0800

    display $fscreateopt

--- lib/debootstrap-ng.sh
+++ lib/debootstrap-ng.sh
@@ -438,6 +438,7 @@ prepare_partitions()
 		dpkg --compare-versions $VER 'gt' $kernel_support_space_cache_v2 && btrfs_space_cache_version=v2
 		local btrfs_cmp_default=lzo
                 [[ $ROOTFS_TYPE == btrfs ]] && local fscreateopt="-o compress=${BTRFS_COMPRESSION:-lzo},space_cache=${btrfs_space_cache_version}"
+		display_alert 'fscreateopt' "$fscreateopt" 'info'
 		unset btrfs_space_cache_version
 		unset kernel_support_space_cache_v2
 		mount ${fscreateopt} $rootdevice $MOUNT/
