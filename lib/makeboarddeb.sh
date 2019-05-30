bffdfcd7f03698ad7e115bb6d9563804bd980315
Manuel Rüger <manuel@rueg.eu>
2019-05-30 22:19:53 +0200

    Apply shellcheck recommendations (#1389)
    * compile.sh: Apply shellcheck recommendations
    
    * lib/upgrade.sh: Apply shellcheck recommendations
    
    * lib/main.sh: Apply shellcheck recommendations
    
    * lib/desktop.sh: Apply shellcheck recommendations
    
    * lib/makeboarddeb.sh: Apply shellcheck recommendations

--- lib/makeboarddeb.sh
+++ lib/makeboarddeb.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # Copyright (c) 2015 Igor Pecovnik, igor.pecovnik@gma**.com
 #
 # This file is licensed under the terms of the GNU General Public
@@ -17,31 +18,31 @@ create_board_package()
 	display_alert "Creating board support package" "$BOARD $BRANCH" "info"
 
 	local destination=$SRC/.tmp/${RELEASE}/${CHOSEN_ROOTFS}_${REVISION}_${ARCH}
-	rm -rf $destination
-	mkdir -p $destination/DEBIAN
+	rm -rf "${destination}"
+	mkdir -p "${destination}"/DEBIAN
 
 	# install copy of boot script & environment file
 	local bootscript_src=${BOOTSCRIPT%%:*}
 	local bootscript_dst=${BOOTSCRIPT##*:}
 
-	mkdir -p $destination/usr/share/armbian/
-	cp $SRC/config/bootscripts/$bootscript_src $destination/usr/share/armbian/$bootscript_dst
+	mkdir -p "${destination}"/usr/share/armbian/
+	cp "${SRC}/config/bootscripts/${bootscript_src}" "${destination}/usr/share/armbian/${bootscript_dst}"
 	[[ -n $BOOTENV_FILE && -f $SRC/config/bootenv/$BOOTENV_FILE ]] && \
-		cp $SRC/config/bootenv/$BOOTENV_FILE $destination/usr/share/armbian/armbianEnv.txt
+		cp "${SRC}/config/bootenv/${BOOTENV_FILE}" "${destination}"/usr/share/armbian/armbianEnv.txt
 
 	# add configuration for setting uboot environment from userspace with: fw_setenv fw_printenv
 	if [[ -n $UBOOT_FW_ENV ]]; then
 		UBOOT_FW_ENV=($(tr ',' ' ' <<< "$UBOOT_FW_ENV"))
-		mkdir -p $destination/etc
-		echo "# Device to access      offset           env size" > $destination/etc/fw_env.config
-		echo "/dev/mmcblk0	${UBOOT_FW_ENV[0]}	${UBOOT_FW_ENV[1]}" >> $destination/etc/fw_env.config
+		mkdir -p "${destination}"/etc
+		echo "# Device to access      offset           env size" > "${destination}"/etc/fw_env.config
+		echo "/dev/mmcblk0	${UBOOT_FW_ENV[0]}	${UBOOT_FW_ENV[1]}" >> "${destination}"/etc/fw_env.config
 	fi
 
 	# Replaces: base-files is needed to replace /etc/update-motd.d/ files on Xenial
 	# Replaces: unattended-upgrades may be needed to replace /etc/apt/apt.conf.d/50unattended-upgrades
 	# (distributions provide good defaults, so this is not needed currently)
 	# Depends: linux-base is needed for "linux-version" command in initrd cleanup script
-	cat <<-EOF > $destination/DEBIAN/control
+	cat <<-EOF > "${destination}"/DEBIAN/control
 	Package: linux-${RELEASE}-root-${DEB_BRANCH}${BOARD}
 	Version: $REVISION
 	Architecture: $ARCH
@@ -59,7 +60,7 @@ create_board_package()
 	EOF
 
 	# set up pre install script
-	cat <<-EOF > $destination/DEBIAN/preinst
+	cat <<-EOF > "${destination}"/DEBIAN/preinst
 	#!/bin/sh
 	[ "\$1" = "upgrade" ] && touch /var/run/.reboot_required
 	[ -d "/boot/bin.old" ] && rm -rf /boot/bin.old
@@ -137,7 +138,7 @@ create_board_package()
 	EOF
 
 	if [[ $FORCE_BOOTSCRIPT_UPDATE == yes ]]; then
-		cat <<-EOF >> $destination/DEBIAN/preinst
+		cat <<-EOF >> "${destination}"/DEBIAN/preinst
 
 		# create a bootscript backup
 		if [ -f /etc/armbian-release ]; then
@@ -150,16 +151,16 @@ create_board_package()
 		exit 0
 		EOF
 	else
-		cat <<-EOF >> $destination/DEBIAN/preinst
+		cat <<-EOF >> "${destination}"/DEBIAN/preinst
 
 		exit 0
 		EOF
 	fi
 
-	chmod 755 $destination/DEBIAN/preinst
+	chmod 755 "${destination}"/DEBIAN/preinst
 
 	# postrm script
-	cat <<-EOF > $destination/DEBIAN/postrm
+	cat <<-EOF > "${destination}"/DEBIAN/postrm
 	#!/bin/sh
 	if [ remove = "\$1" ] || [ abort-install = "\$1" ]; then
 		dpkg-divert --package linux-${RELEASE}-root-${DEB_BRANCH}${BOARD} --remove --rename \
@@ -169,10 +170,10 @@ create_board_package()
 	exit 0
 	EOF
 
-	chmod 755 $destination/DEBIAN/postrm
+	chmod 755 "${destination}"/DEBIAN/postrm
 
 	# set up post install script
-	cat <<-EOF > $destination/DEBIAN/postinst
+	cat <<-EOF > "${destination}"/DEBIAN/postinst
 	#!/bin/sh
 	#
 	# ${BOARD} BSP post installation script
@@ -206,7 +207,7 @@ create_board_package()
 		[ -f /boot/boot.cmd ] && mkimage -C none -A arm -T script -d /boot/boot.cmd /boot/boot.scr  >/dev/null 2>&1
 	EOF
 	if [[ $FORCE_BOOTSCRIPT_UPDATE == yes ]]; then
-		cat <<-EOF >> $destination/DEBIAN/postinst
+		cat <<-EOF >> "${destination}"/DEBIAN/postinst
 		else
 			echo "Updating bootscript"
 
@@ -219,7 +220,7 @@ create_board_package()
 		EOF
 	fi
 
-	cat <<-EOF >> $destination/DEBIAN/postinst
+	cat <<-EOF >> "${destination}"/DEBIAN/postinst
 	fi
 	# now cleanup and remove old ramlog service
 	systemctl disable log2ram.service >/dev/null 2>&1
@@ -252,24 +253,24 @@ create_board_package()
 	exit 0
 	EOF
 
-	chmod 755 $destination/DEBIAN/postinst
+	chmod 755 "${destination}"/DEBIAN/postinst
 
 	# won't recreate files if they were removed by user
 	# TODO: Add proper handling for updated conffiles
-	#cat <<-EOF > $destination/DEBIAN/conffiles
+	#cat <<-EOF > "${destination}"/DEBIAN/conffiles
 	#EOF
 
 	# copy common files from a premade directory structure
-	rsync -a $SRC/packages/bsp/common/* $destination/
+	rsync -a "${SRC}"/packages/bsp/common/* "${destination}"/
 
 	# trigger uInitrd creation after installation, to apply
 	# /etc/initramfs/post-update.d/99-uboot
-	cat <<-EOF > $destination/DEBIAN/triggers
+	cat <<-EOF > "${destination}"/DEBIAN/triggers
 	activate update-initramfs
 	EOF
 
 	# configure MIN / MAX speed for cpufrequtils
-	cat <<-EOF > $destination/etc/default/cpufrequtils
+	cat <<-EOF > "${destination}"/etc/default/cpufrequtils
 	# WARNING: this file will be replaced on board support package (linux-root-...) upgrade
 	ENABLE=true
 	MIN_SPEED=$CPUMIN
@@ -278,7 +279,7 @@ create_board_package()
 	EOF
 
 	# armhwinfo, firstrun, armbianmonitor, etc. config file
-	cat <<-EOF > $destination/etc/armbian-release
+	cat <<-EOF > "${destination}"/etc/armbian-release
 	# PLEASE DO NOT EDIT THIS FILE
 	BOARD=$BOARD
 	BOARD_NAME="$BOARD_NAME"
@@ -294,62 +295,62 @@ create_board_package()
 	EOF
 
 	# this is required for NFS boot to prevent deconfiguring the network on shutdown
-	[[ $RELEASE == xenial || $RELEASE == stretch || $RELEASE == bionic || $RELEASE == buster || $RELEASE == disco ]] && sed -i 's/#no-auto-down/no-auto-down/g' $destination/etc/network/interfaces.default
+	[[ $RELEASE == xenial || $RELEASE == stretch || $RELEASE == bionic || $RELEASE == buster || $RELEASE == disco ]] && sed -i 's/#no-auto-down/no-auto-down/g' "${destination}"/etc/network/interfaces.default
 
 	if [[ ( $LINUXFAMILY == sun*i || $LINUXFAMILY == pine64 ) && $BRANCH == default ]]; then
 		# add mpv config for vdpau_sunxi
-		mkdir -p $destination/etc/mpv/
-		cp $SRC/packages/bsp/mpv/mpv_sunxi.conf $destination/etc/mpv/mpv.conf
-		echo "export VDPAU_OSD=1" > $destination/etc/profile.d/90-vdpau.sh
-		chmod 755 $destination/etc/profile.d/90-vdpau.sh
+		mkdir -p "${destination}"/etc/mpv/
+		cp "${SRC}"/packages/bsp/mpv/mpv_sunxi.conf "${destination}"/etc/mpv/mpv.conf
+		echo "export VDPAU_OSD=1" > "${destination}"/etc/profile.d/90-vdpau.sh
+		chmod 755 "${destination}"/etc/profile.d/90-vdpau.sh
 	fi
 
 	if [[ $LINUXFAMILY == sunxi* && $BRANCH != default ]]; then
 		# add mpv config for x11 output - slow, but it works compared to no config at all
 		# TODO: Test which output driver is better with DRM
-		mkdir -p $destination/etc/mpv/
-		cp $SRC/packages/bsp/mpv/mpv_mainline.conf $destination/etc/mpv/mpv.conf
+		mkdir -p "${destination}"/etc/mpv/
+		cp "${SRC}"/packages/bsp/mpv/mpv_mainline.conf "${destination}"/etc/mpv/mpv.conf
 	fi
 
 	case $RELEASE in
 	jessie)
-		mkdir -p $destination/etc/NetworkManager/dispatcher.d/
-		install -m 755 $SRC/packages/bsp/99disable-power-management $destination/etc/NetworkManager/dispatcher.d/
+		mkdir -p "${destination}"/etc/NetworkManager/dispatcher.d/
+		install -m 755 "${SRC}"/packages/bsp/99disable-power-management "${destination}"/etc/NetworkManager/dispatcher.d/
 	;;
 
 	xenial)
-		mkdir -p $destination/usr/lib/NetworkManager/conf.d/
-		cp $SRC/packages/bsp/zz-override-wifi-powersave-off.conf $destination/usr/lib/NetworkManager/conf.d/
+		mkdir -p "${destination}"/usr/lib/NetworkManager/conf.d/
+		cp "${SRC}"/packages/bsp/zz-override-wifi-powersave-off.conf "${destination}"/usr/lib/NetworkManager/conf.d/
 		if [[ $BRANCH == default ]]; then
 			# this is required only for old kernels
 			# not needed for Stretch since there will be no Stretch images with kernels < 4.4
-			mkdir -p $destination/lib/systemd/system/haveged.service.d/
-			cp $SRC/packages/bsp/10-no-new-privileges.conf $destination/lib/systemd/system/haveged.service.d/
+			mkdir -p "${destination}"/lib/systemd/system/haveged.service.d/
+			cp "${SRC}"/packages/bsp/10-no-new-privileges.conf "${destination}"/lib/systemd/system/haveged.service.d/
 		fi
 	;;
 
 	stretch)
-		mkdir -p $destination/usr/lib/NetworkManager/conf.d/
-		cp $SRC/packages/bsp/zz-override-wifi-powersave-off.conf $destination/usr/lib/NetworkManager/conf.d/
-		cp $SRC/packages/bsp/10-override-random-mac.conf $destination/usr/lib/NetworkManager/conf.d/
+		mkdir -p "${destination}"/usr/lib/NetworkManager/conf.d/
+		cp "${SRC}"/packages/bsp/zz-override-wifi-powersave-off.conf "${destination}"/usr/lib/NetworkManager/conf.d/
+		cp "${SRC}"/packages/bsp/10-override-random-mac.conf "${destination}"/usr/lib/NetworkManager/conf.d/
 	;;
 
 	bionic)
-		mkdir -p $destination/usr/lib/NetworkManager/conf.d/
-		cp $SRC/packages/bsp/zz-override-wifi-powersave-off.conf $destination/usr/lib/NetworkManager/conf.d/
-		cp $SRC/packages/bsp/10-override-random-mac.conf $destination/usr/lib/NetworkManager/conf.d/
+		mkdir -p "${destination}"/usr/lib/NetworkManager/conf.d/
+		cp "${SRC}"/packages/bsp/zz-override-wifi-powersave-off.conf "${destination}"/usr/lib/NetworkManager/conf.d/
+		cp "${SRC}"/packages/bsp/10-override-random-mac.conf "${destination}"/usr/lib/NetworkManager/conf.d/
 	;;
 
 	buster)
-                mkdir -p $destination/usr/lib/NetworkManager/conf.d/
-                cp $SRC/packages/bsp/zz-override-wifi-powersave-off.conf $destination/usr/lib/NetworkManager/conf.d/
-                cp $SRC/packages/bsp/10-override-random-mac.conf $destination/usr/lib/NetworkManager/conf.d/
+                mkdir -p "${destination}"/usr/lib/NetworkManager/conf.d/
+                cp "${SRC}"/packages/bsp/zz-override-wifi-powersave-off.conf "${destination}"/usr/lib/NetworkManager/conf.d/
+                cp "${SRC}"/packages/bsp/10-override-random-mac.conf "${destination}"/usr/lib/NetworkManager/conf.d/
         ;;
 
 	disco)
-		mkdir -p $destination/usr/lib/NetworkManager/conf.d/
-		cp $SRC/packages/bsp/zz-override-wifi-powersave-off.conf $destination/usr/lib/NetworkManager/conf.d/
-		cp $SRC/packages/bsp/10-override-random-mac.conf $destination/usr/lib/NetworkManager/conf.d/
+		mkdir -p "${destination}"/usr/lib/NetworkManager/conf.d/
+		cp "${SRC}"/packages/bsp/zz-override-wifi-powersave-off.conf "${destination}"/usr/lib/NetworkManager/conf.d/
+		cp "${SRC}"/packages/bsp/10-override-random-mac.conf "${destination}"/usr/lib/NetworkManager/conf.d/
 	;;
 
 	esac
@@ -358,17 +359,17 @@ create_board_package()
 	[[ $(type -t family_tweaks_bsp) == function ]] && family_tweaks_bsp
 
 	# add some summary to the image
-	fingerprint_image "$destination/etc/armbian.txt"
+	fingerprint_image "${destination}/etc/armbian.txt"
 
 	# fixing permissions (basic), reference: dh_fixperms
-	find $destination -print0 2>/dev/null | xargs -0r chown --no-dereference 0:0
-	find $destination ! -type l -print0 2>/dev/null | xargs -0r chmod 'go=rX,u+rw,a-s'
+	find "${destination}" -print0 2>/dev/null | xargs -0r chown --no-dereference 0:0
+	find "${destination}" ! -type l -print0 2>/dev/null | xargs -0r chmod 'go=rX,u+rw,a-s'
 
 	# create board DEB file
 	display_alert "Building package" "$CHOSEN_ROOTFS" "info"
-	fakeroot dpkg-deb -b $destination ${destination}.deb >> $DEST/debug/install.log 2>&1
-	mkdir -p $DEST/debs/$RELEASE/
-	mv ${destination}.deb $DEST/debs/$RELEASE/
+	fakeroot dpkg-deb -b "${destination}" "${destination}.deb" >> "${DEST}"/debug/install.log 2>&1
+	mkdir -p "${DEST}/debs/${RELEASE}/"
+	mv "${destination}.deb" "${DEST}/debs/${RELEASE}/"
 	# cleanup
-	rm -rf $destination
+	rm -rf "${destination}"
 }
--- lib/upgrade.sh
+++ lib/upgrade.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Copyright (c) 2015-2017 Igor Pecovnik, igor.pecovnik@gma**.com
 # Copyright (c) 2015-2017 other Armbian contributors (https://github.com/armbian/build/graphs/contributors)
 #
@@ -13,12 +15,12 @@
 TTY_X=$(($(stty size | awk '{print $2}')-6)) # determine terminal width
 TTY_Y=$(($(stty size | awk '{print $1}')-6)) # determine terminal height
 
-if [[ $(basename $0) == main.sh ]]; then
+if [[ $(basename "$0") == main.sh ]]; then
 	echo "Please use compile.sh to start the build process"
-	exit -1
+	exit 255
 fi
 
-if [[ $(basename $0) == compile.sh ]]; then
+if [[ $(basename "$0") == compile.sh ]]; then
 	# assuming dialog was installed already since this is supposed to be shown only on upgrade from previous versions
 	dialog --title "Directory structure change notice" --colors --msgbox "Build script directory structure was changed to simplify the build environment setup, \
 	simplify upgrading in the future and improve compatibility with containers like Docker and Vagrant
@@ -28,5 +30,5 @@ if [[ $(basename $0) == compile.sh ]]; then
 	Default build configuration is defined in \Z1config-default.conf\Zn (created on first build script run)
 
 	Sorry for the inconvenience" $TTY_Y $TTY_X
-	exit -1
+	exit 255
 fi
