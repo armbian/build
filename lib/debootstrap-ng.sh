95977fa6f5fa64a235f5aec7300cd3351104a23f
Igor Pecovnik <igor.pecovnik@gmail.com>
2019-06-14 20:29:07 +0200

    [Build script] Reverting image creation improvement due to (random?) failing. truncate sometimes resoult in fs corruption or is not possible to clearly unmount loop device. Need deeper inspection or simply reverting to previous working solution
    Signed-off-by: Igor Pecovnik <igor.pecovnik@gmail.com>

--- lib/debootstrap-ng.sh
+++ lib/debootstrap-ng.sh
@@ -387,7 +387,8 @@ prepare_partitions()
 
 	# stage: create blank image
 	display_alert "Creating blank image for rootfs" "$sdsize MiB" "info"
-	truncate --size=${sdsize}M ${SDCARD}.raw
+	# truncate --size=${sdsize}M ${SDCARD}.raw # sometimes results in fs corruption, revert to previous know to work solution
+	dd if=/dev/zero bs=1M status=none count=$sdsize | pv -p -b -r -s $(( $sdsize * 1024 * 1024 )) | dd status=none of=${SDCARD}.raw
 
 	# stage: calculate boot partition size
 	local bootstart=$(($OFFSET * 2048))
