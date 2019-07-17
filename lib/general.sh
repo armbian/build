568b8d7d558109f8538f699213c2ff1114e2318a
Igor Pecovnik <igor.pecovnik@gmail.com>
2019-07-17 19:36:13 +0200

    build script: make rootfs cache hash (minor x.y*) version dependent to not keep too old caches and that cache can be forcefully rebuild only by changing minor version number. If cache is rebuild only with new packages version, hash stays the same, while we in some cases need to change filename which is served over torrents.

--- lib/general.sh
+++ lib/general.sh
@@ -117,7 +117,7 @@ exit_with_error()
 get_package_list_hash()
 {
 	( printf '%s\n' $PACKAGE_LIST | sort -u; printf '%s\n' $PACKAGE_LIST_EXCLUDE | sort -u; echo "$ROOTFSCACHE_VERSION" ) \
-		| md5sum | cut -d' ' -f 1
+		| echo "${REVISION::-1}" | md5sum | cut -d' ' -f 1
 }
 
 # create_sources_list <release> <basedir>
