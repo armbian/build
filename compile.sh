d02ac5b3687f2a2de4be3b176f09af32ba10d101
Zhang Ning <832666+zhangn1985@users.noreply.github.com>
2019-09-29 15:34:39 +0800

    git: do not update/checkout with root privileges
    Signed-off-by: Zhang Ning <832666+zhangn1985@users.noreply.github.com>

--- compile.sh
+++ compile.sh
@@ -35,6 +35,51 @@ if [[ $EUID != 0 && "$1" != vagrant ]]; then
 	exit $?
 fi
 
+update_src() {
+	cd "${SRC}" || exit
+	if [[ ! -f $SRC/.ignore_changes ]]; then
+		echo -e "[\e[0;32m o.k. \x1B[0m] This script will try to update"
+		git pull
+		CHANGED_FILES=$(git diff --name-only)
+		if [[ -n $CHANGED_FILES ]]; then
+			echo -e "[\e[0;35m warn \x1B[0m] Can't update since you made changes to: \e[0;32m\n${CHANGED_FILES}\x1B[0m"
+			while true; do
+				echo -e "Press \e[0;33m<Ctrl-C>\x1B[0m or \e[0;33mexit\x1B[0m to abort compilation, \e[0;33m<Enter>\x1B[0m to ignore and continue, \e[0;33mdiff\x1B[0m to display changes"
+				read -r
+				if [[ "$REPLY" == "diff" ]]; then
+					git diff
+				elif [[ "$REPLY" == "exit" ]]; then
+					exit 1
+				elif [[ "$REPLY" == "" ]]; then
+					break
+				else
+					echo "Unknown command!"
+				fi
+			done
+		else
+			git checkout "${LIB_TAG:-master}"
+		fi
+	fi
+}
+
+TMPFILE=`mktemp`
+echo SRC=$SRC > $TMPFILE
+echo LIB_TAG=$LIB_TAG >> $TMPFILE
+declare -f update_src >> $TMPFILE
+echo update_src >> $TMPFILE
+
+#do not update/checkout git with root privileges to messup files onwership.
+#due to in docker/VM, we can't su to a normal user, so do not update/checkout git.
+if [[ `systemd-detect-virt` == 'none' ]]; then
+	if [[ $EUID == 0 ]]; then
+		su `stat --format=%U $SRC/.git` -c bash $TMPFILE
+	else
+		bash $TMPFILE
+	fi
+fi
+
+rm $TMPFILE
+
 # Check for Vagrant
 if [[ "$1" == vagrant && -z "$(which vagrant)" ]]; then
 	display_alert "Vagrant not installed." "Installing"
@@ -135,30 +180,6 @@ for i in "$@"; do
 		eval "$parameter=\"$value\""
 	fi
 done
-
-if [[ ! -f $SRC/.ignore_changes ]]; then
-	echo -e "[\e[0;32m o.k. \x1B[0m] This script will try to update"
-	git pull
-	CHANGED_FILES=$(git diff --name-only)
-	if [[ -n $CHANGED_FILES ]]; then
-		echo -e "[\e[0;35m warn \x1B[0m] Can't update since you made changes to: \e[0;32m\n${CHANGED_FILES}\x1B[0m"
-		while true; do
-			echo -e "Press \e[0;33m<Ctrl-C>\x1B[0m or \e[0;33mexit\x1B[0m to abort compilation, \e[0;33m<Enter>\x1B[0m to ignore and continue, \e[0;33mdiff\x1B[0m to display changes"
-			read -r
-			if [[ "$REPLY" == "diff" ]]; then
-				git diff
-			elif [[ "$REPLY" == "exit" ]]; then
-				exit 1
-			elif [[ "$REPLY" == "" ]]; then
-				break
-			else
-				echo "Unknown command!"
-			fi
-		done
-	else
-		git checkout "${LIB_TAG:-master}"
-	fi
-fi
 
 if [[ $BUILD_ALL == yes || $BUILD_ALL == demo ]]; then
 	# shellcheck source=lib/build-all-ng.sh
