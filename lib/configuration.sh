712640bfb88c9e9fa76865f8be74c9f467b627b4
Igor Pečovnik <igorpecovnik@users.noreply.github.com>
2019-11-01 13:16:36 +0100

    We will build Ubuntu 19.10 rootfs cache as well

--- lib/configuration.sh
+++ lib/configuration.sh
@@ -24,7 +24,7 @@ ROOTFSCACHE_VERSION=13
 CHROOT_CACHE_VERSION=7
 BUILD_REPOSITORY_URL=$(git remote get-url $(git remote 2>/dev/null) 2>/dev/null)
 BUILD_REPOSITORY_COMMIT=$(git describe --match=d_e_a_d_b_e_e_f --always --dirty 2>/dev/null)
-ROOTFS_CACHE_MAX=30 # max number of rootfs cache, older ones will be cleaned up
+ROOTFS_CACHE_MAX=36 # max number of rootfs cache, older ones will be cleaned up
 
 if [[ $BETA == yes ]]; then
 	DEB_STORAGE=$DEST/debs-beta
