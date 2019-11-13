934fb61b634092703a2fab737e6813edc74f8467
Igor Pecovnik <igor.pecovnik@gmail.com>
2019-11-13 23:54:19 +0100

    Merge Odroid N2 into meson64

--- lib/compilation-prepare.sh
+++ lib/compilation-prepare.sh
@@ -41,7 +41,7 @@ compilation_prepare()
 		process_patch_file "${SRC}/patch/misc/general-packaging-4.4.y-rk3399.patch"                "applying"
 	fi
 
-	if [[ $version == "4.9."* ]] && [[ "$LINUXFAMILY" == odroidn2 ]]; then
+	if [[ $version == "4.9."* ]] && [[ "$LINUXFAMILY" == meson64 ]]; then
 		display_alert "Adjustin" "packaging" "info"
 		cd ${SRC}/cache/sources/${LINUXSOURCEDIR}
 		process_patch_file "${SRC}/patch/misc/general-packaging-4.9.y.patch"                "applying"
