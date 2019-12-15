ee87f34563e51296be4de26364340260dd78f5f9
Igor Pecovnik <igor.pecovnik@gmail.com>
2019-12-15 08:19:11 +0100

    Bullseye: add browser, adjust versioning, (temporally) remove non existing repository

--- lib/configuration.sh
+++ lib/configuration.sh
@@ -24,7 +24,7 @@ ROOTFSCACHE_VERSION=14
 CHROOT_CACHE_VERSION=7
 BUILD_REPOSITORY_URL=$(git remote get-url $(git remote 2>/dev/null) 2>/dev/null)
 BUILD_REPOSITORY_COMMIT=$(git describe --match=d_e_a_d_b_e_e_f --always --dirty 2>/dev/null)
-ROOTFS_CACHE_MAX=36 # max number of rootfs cache, older ones will be cleaned up
+ROOTFS_CACHE_MAX=42 # max number of rootfs cache, older ones will be cleaned up
 
 if [[ $BETA == yes ]]; then
 	DEB_STORAGE=$DEST/debs-beta
@@ -226,7 +226,7 @@ case $RELEASE in
 		DEBOOTSTRAP_LIST+=" rng-tools"
 		[[ -z $BUILD_MINIMAL || $BUILD_MINIMAL == no ]] && PACKAGE_LIST_RELEASE="man-db kbd net-tools gnupg2 dirmngr networkd-dispatcher"
 		PACKAGE_LIST_DESKTOP+=" paprefs dbus-x11 numix-icon-theme"
-		PACKAGE_LIST_DESKTOP_RECOMMENDS+=" system-config-printer-common system-config-printer"
+		PACKAGE_LIST_DESKTOP_RECOMMENDS+=" firefox-esr system-config-printer-common system-config-printer"
 	;;
 
 
--- lib/distributions.sh
+++ lib/distributions.sh
@@ -421,7 +421,7 @@ install_distribution_specific()
 
 		;;
 
-	stretch|buster|bullseye)
+	stretch|buster)
 
 			# remove doubled uname from motd
 			[[ -f $SDCARD/etc/update-motd.d/10-uname ]] && rm "${SDCARD}"/etc/update-motd.d/10-uname
@@ -430,6 +430,20 @@ install_distribution_specific()
 
 		;;
 
+	bullseye)
+
+			# remove doubled uname from motd
+			[[ -f $SDCARD/etc/update-motd.d/10-uname ]] && rm "${SDCARD}"/etc/update-motd.d/10-uname
+			# rc.local is not existing but one might need it
+			install_rclocal
+			# fix missing versioning
+			[[ $(grep -L "VERSION_ID" "${SDCARD}"/etc/os-release) ]] && echo 'VERSION_ID="11"' >> "${SDCARD}"/etc/os-release
+			[[ $(grep -L "VERSION" "${SDCARD}"/etc/os-release) ]] && echo 'VERSION="11 (bullseye)"' >> "${SDCARD}"/etc/os-release
+
+			# remove security updates repository since it does not exists yet
+			sed '/security/ d' -i "${SDCARD}"/etc/apt/sources.list
+
+		;;
 	bionic|eoan|focal)
 
 			# remove doubled uname from motd
