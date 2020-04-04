3479b590e0f7d16c4bea0c69e21609c05ef6a86c
Igor Pecovnik <igor.pecovnik@gmail.com>
2020-04-04 22:30:22 +0200

    Improve images uploading
    Select faster SSH chiper to speed up a bit
    
    Signed-off-by: Igor Pecovnik <igor.pecovnik@gmail.com>

--- lib/build-all-ng.sh
+++ lib/build-all-ng.sh
@@ -104,13 +104,13 @@ pack_upload ()
 	if [[ $COMPRESS_OUTPUTIMAGE == *xz* ]]; then
 		display_alert "Compressing" "$DEST/images/${version}.img.xz" "info"
 		pixz -3 < $DESTIMG/${version}.img > ${DESTIMG}/${version}.img.xz
-		find $DESTIMG -type f -name '${version}.img' -print0 | xargs -0 rm --
+		rm ${DESTIMG}/${version}.img
 	fi
 
 	if [[ -n "${SEND_TO_SERVER}" ]]; then
 		ssh "${SEND_TO_SERVER}" "mkdir -p ${SEND_TO_LOCATION}${BOARD}/{archive,nightly}" &
 		display_alert "Uploading" "Please wait!" "info"
-		nice -n 19 bash -c "rsync -arP --info=progress2,stats1 --ignore-existing --remove-source-files --prune-empty-dirs $DESTIMG/ -e 'ssh -p 22' ${SEND_TO_SERVER}:${SEND_TO_LOCATION}${BOARD}/${subdir}" &
+		nice -n 19 bash -c "rsync -arP --info=progress2 --ignore-existing --remove-source-files --prune-empty-dirs $DESTIMG/ -e 'ssh -T -c aes128-ctr -o Compression=no -x -p 22' ${SEND_TO_SERVER}:${SEND_TO_LOCATION}${BOARD}/${subdir}" &
 	else
 		mv $DESTIMG/*.* $DEST/images/
 	fi
