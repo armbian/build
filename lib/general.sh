f78591db5be444c8de6ff6c0cbac477fac451634
Werner <EvilOlaf@users.noreply.github.com>
2020-04-25 21:25:05 +0200

    Fixes building on Linux Mint Tricia (Bionic based) (#1914)
    * Replace apt with apt-get unless in chroot
    * Improve support for Buster and Mint Debbie
    
    Co-authored-by: Igor Pečovnik <igorpecovnik@users.noreply.github.com>

--- lib/general.sh
+++ lib/general.sh
@@ -759,13 +759,13 @@ prepare_host_basic()
 	# need lsb_release to decide what to install
 	if [[ $(dpkg-query -W -f='${db:Status-Abbrev}\n' lsb-release 2>/dev/null) != *ii* ]]; then
 		display_alert "Installing package" "lsb-release"
-		apt -q update && apt install -q -y --no-install-recommends lsb-release
+		apt-get -q update && apt-get install -q -y --no-install-recommends lsb-release
 	fi
 
 	# need to install dialog if person is starting with a interactive mode
 	if [[ $(dpkg-query -W -f='${db:Status-Abbrev}\n' dialog 2>/dev/null) != *ii* ]]; then
 		display_alert "Installing package" "dialog"
-		apt -q update && apt install -q -y --no-install-recommends dialog
+		apt-get -q update && apt-get install -q -y --no-install-recommends dialog
 	fi
 
 }
@@ -837,7 +837,7 @@ prepare_host()
 		exit_with_error "Windows subsystem for Linux is not a supported build environment"
 	fi
 
-	if [[ -z $codename || "focal" == "$codename" || "eoan" == "$codename" ]]; then
+	if [[ -z $codename || "focal" == "$codename" || "eoan" == "$codename"  || "debbie" == "$codename"  || "buster" == "$codename" ]]; then
 	    hostdeps="${hostdeps/lib32ncurses5 lib32tinfo5/lib32ncurses6 lib32tinfo6}"
 	fi
 
@@ -886,9 +886,9 @@ prepare_host()
 
 	if [[ ${#deps[@]} -gt 0 ]]; then
 		display_alert "Installing build dependencies"
-		apt -q update
-		apt -y upgrade
-		apt -q -y --no-install-recommends install -o Dpkg::Options::='--force-confold' "${deps[@]}" | tee -a $DEST/debug/hostdeps.log
+		apt-get -q update
+		apt-get -y upgrade
+		apt-get -q -y --no-install-recommends install -o Dpkg::Options::='--force-confold' "${deps[@]}" | tee -a $DEST/debug/hostdeps.log
 		update-ccache-symlinks
 	fi
 
@@ -899,7 +899,7 @@ prepare_host()
 	fi
 
 	if [[ $(dpkg-query -W -f='${db:Status-Abbrev}\n' 'zlib1g:i386' 2>/dev/null) != *ii* ]]; then
-		apt install -qq -y --no-install-recommends zlib1g:i386 >/dev/null 2>&1
+		apt-get install -qq -y --no-install-recommends zlib1g:i386 >/dev/null 2>&1
 	fi
 
 	# enable arm binary format so that the cross-architecture chroot environment will work
