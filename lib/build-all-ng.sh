e57aaf6556e07223635be12034bc58369852efad
Igor Pecovnik <igor.pecovnik@gmail.com>
2020-05-10 12:21:21 +0200

    Internal switch - make the same nightly images as for stable one. For making RC targets.

--- lib/build-all-ng.sh
+++ lib/build-all-ng.sh
@@ -19,6 +19,7 @@
 
 
 if [[ $BETA == "yes" ]];  then STABILITY="beta";	else STABILITY="stable"; fi
+if [[ $MAKE_ALL_BETA == "yes" ]]; then STABILITY="stable"; fi
 if [[ -z $KERNEL_ONLY ]]; then KERNEL_ONLY="yes"; fi
 if [[ -z $MULTITHREAD ]]; then MULTITHREAD=0; fi
 if [[ -z $START ]]; then START=0; fi
