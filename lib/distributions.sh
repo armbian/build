68235cf5ed43a12afe972138b2d4eb31c1e273e2
Igor Pecovnik <igor.pecovnik@gmail.com>
2020-06-10 19:09:36 +0200

    Check if selinux config is present
    This suppresses error
    
    Signed-off-by: Igor Pecovnik <igor.pecovnik@gmail.com>

--- lib/distributions.sh
+++ lib/distributions.sh
@@ -97,7 +97,7 @@ install_common()
 
 	# disable selinux by default
 	mkdir -p "${SDCARD}"/selinux
-	sed "s/^SELINUX=.*/SELINUX=disabled/" -i "${SDCARD}"/etc/selinux/default
+	[[ -f "${SDCARD}"/etc/selinux/default ]] && sed "s/^SELINUX=.*/SELINUX=disabled/" -i "${SDCARD}"/etc/selinux/default
 
 	# remove Ubuntu's legal text
 	[[ -f $SDCARD/etc/legal ]] && rm "${SDCARD}"/etc/legal
