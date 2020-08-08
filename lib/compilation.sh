armbian/build

commit bce24a2e8d95e7e60a92d5234d343965634571a1

delete Checking out sources from compile_uboot() (#1999)
At this point the repository is already clean,
and the next line is not needed.

Fix issues #1952

lib/compilation.sh
@@ -140,9 +140,6 @@ compile_uboot()
 		local target_patchdir=$(cut -d';' -f2 <<< $target)
 		local target_files=$(cut -d';' -f3 <<< $target)
 
-		display_alert "Checking out sources"
-		git checkout -f -q HEAD
-
 		if [[ $CLEAN_LEVEL == *make* ]]; then
 			display_alert "Cleaning" "$BOOTSOURCEDIR" "info"
 			(cd $SRC/cache/sources/$BOOTSOURCEDIR; make clean > /dev/null 2>&1)

commit 80be55bf733e9e3ce49644797f94167edae28d2f

Fixed running compile_uboot directly - broken since bce24a2

lib/compilation.sh
@@ -155,12 +155,9 @@ compile_uboot()
 		target_patchdir=$(cut -d';' -f2 <<< "${target}")
 		target_files=$(cut -d';' -f3 <<< "${target}")
 
-		# only checkout when we go several times over the source
-		if [[ $target_cycles -ge 1 ]]; then
-			display_alert "Checking out to clean sources"
-			git checkout -f -q HEAD
-		fi
-		target_cycles=$((target_cycles+1))
+		# needed for multiple targets and for calling compile_uboot directly
+		display_alert "Checking out to clean sources"
+		git checkout -f -q HEAD
 
 		if [[ $CLEAN_LEVEL == *make* ]]; then
 			display_alert "Cleaning" "$BOOTSOURCEDIR" "info"