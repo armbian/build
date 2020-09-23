b2800dbd256e757d229df2dcee73b3d03761d92e
lanefu <lanefu@users.noreply.github.com>
2020-09-23 01:03:45 +0000

    WIP: Pinebookpro tweaks (#2208)
    Pinebook-Pro Support:
    suspend to idle via key and lid close
    brightness keys
    volume control keys
    wifi, bluetooth frimware
    touchpad tweaks
    keyboard config
    armbian-hardware-optimization
    bootsplash
    Co-authored-by: Britt Dodd <britt@reactornet.net>

--- lib/desktop.sh
+++ lib/desktop.sh
@@ -146,4 +146,11 @@ desktop_postinstall ()
 		echo "disp_mem_reserves=on" >> "${SDCARD}"/boot/armbianEnv.txt
 		echo "extraargs=cma=96M" >> "${SDCARD}"/boot/armbianEnv.txt
 	fi
+
+        if [[ $BOARD == "pinebook-pro" ]]; then
+                # powerconfig, touchpad, and special keys
+	        cp $SRC/packages/bsp/pinebook-pro/xfce4-power-manager.xml ${SDCARD}/etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml/
+		cp $SRC/packages/bsp/pinebook-pro/pointers.xml ${SDCARD}/etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml/
+		cp $SRC/packages/bsp/pinebook-pro/xfce4-keyboard-shortcuts.xml ${SDCARD}/etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml/
+	fi
 }
