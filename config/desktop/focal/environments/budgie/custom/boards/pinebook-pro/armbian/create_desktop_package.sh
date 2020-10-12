84c09bcadf5d91f946751edf3837aed9974ef644
richneese <r.neese@gmail.com>
2020-10-12 13:18:43 -0400

    fixed a typo in the file asouund to asound

--- config/desktop/focal/environments/budgie/custom/boards/pinebook-pro/armbian/create_desktop_package.sh
+++ config/desktop/focal/environments/budgie/custom/boards/pinebook-pro/armbian/create_desktop_package.sh
@@ -1,5 +1,5 @@
 # install custom xorg for pinebook-pro
-cp -R "${SRC}"/packages/bsp/pinebook-pro/xorg.conf "${destination}"/etc/X11/xorg.conf.d/
+cp -R "${SRC}"/packages/bsp/pinebook-pro/xorg.conf "${destination}"/etc/X11/
 
 # install custom asound state for pinebook-pro
-cp -R "${SRC}"/packages/asouund.state/ "${destination}"/etc/
+cp -R "${SRC}"/packages/asound.state/ "${destination}"/etc/
