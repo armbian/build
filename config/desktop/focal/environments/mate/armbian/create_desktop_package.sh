6c92e6ba712a9287811b8543634b4b82fa19b54a
richneese <r.neese@gmail.com>
2020-10-13 09:03:02 -0400

    Fixing paths in the mate install per request

--- config/desktop/focal/environments/mate/armbian/create_desktop_package.sh
+++ config/desktop/focal/environments/mate/armbian/create_desktop_package.sh
@@ -3,14 +3,14 @@ cp -R "${SRC}"/packages/blobs/desktop/lightdm "${destination}"/etc/armbian
 
 # install default desktop settings
 mkdir -p "${destination}"/etc/skel
-cp -R "${SRC}"/packages/blobs/desktop/skel/. "${destination}"/etc/skel
+cp -R "${SRC}"/packages/blobs/desktop/skel_lxqt/. "${destination}"/etc/skel
 
 # install logo for login screen
 cp "${SRC}"/packages/blobs/desktop/icons/armbian.png "${destination}"/usr/share/pixmaps
 
 # install wallpapers
-mkdir -p "${destination}"/usr/share/backgrounds/xfce/
-cp "${SRC}"/packages/blobs/desktop/wallpapers/armbian*.jpg "${destination}"/usr/share/backgrounds/xfce/
+mkdir -p "${destination}"/usr/share/backgrounds/armbian/
+cp "${SRC}"/packages/blobs/desktop/wallpapers/armbian*.jpg "${destination}"/usr/share/backgrounds/armbian/
 
 # add loading desktop splash service
 mkdir -p "${destination}"/etc/systemd/system/
