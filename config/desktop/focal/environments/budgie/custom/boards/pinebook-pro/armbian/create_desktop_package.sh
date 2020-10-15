caaee8f8f563c717ac44bed85754698ec4f23bf4
richneese <r.neese@gmail.com>
2020-10-15 11:07:14 -0400

    Revert "Merge branch 'testing' into dde-desktop"
    This reverts commit 0e0460ec60e9480f269a7ef3a4bc25b89359865f.

--- config/desktop/focal/environments/budgie/custom/boards/pinebook-pro/armbian/create_desktop_package.sh
+++
@@ -1,5 +0,0 @@
-# install custom xorg for pinebook-pro
-cp -R "${SRC}"/packages/bsp/pinebook-pro/xorg.conf "${destination}"/etc/X11/
-
-# install custom asound state for pinebook-pro
-cp -R "${SRC}"/packages/asound.state/ "${destination}"/etc/
