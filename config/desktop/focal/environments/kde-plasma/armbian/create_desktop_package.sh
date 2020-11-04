e72ef1b158c203f22b101a2e4eb5040cbe7be1c5
Igor Pecovnik <igor.pecovnik@gmail.com>
2020-11-04 07:40:03 +0100

    Add links to LXDE, KDE and KDE plasma
    Groovy desktop

--- config/desktop/focal/environments/kde-plasma/armbian/create_desktop_package.sh
+++ config/desktop/focal/environments/kde-plasma/armbian/create_desktop_package.sh
@@ -2,8 +2,8 @@
 cp -R "${SRC}"/packages/blobs/desktop/lightdm "${destination}"/etc/armbian
 
 # install default desktop settings
-mkdir -p "${destination}"/etc/skel
-cp -R "${SRC}"/packages/blobs/desktop/skel/. "${destination}"/etc/skel
+#mkdir -p "${destination}"/etc/skel
+#cp -R "${SRC}"/packages/blobs/desktop/skel/. "${destination}"/etc/skel
 
 # install logo for login screen
 cp "${SRC}"/packages/blobs/desktop/icons/armbian.png "${destination}"/usr/share/pixmaps
@@ -11,7 +11,3 @@ cp "${SRC}"/packages/blobs/desktop/icons/armbian.png "${destination}"/usr/share/
 # install wallpapers
 mkdir -p "${destination}"/usr/share/backgrounds/armbian/
 cp "${SRC}"/packages/blobs/desktop/wallpapers/armbian*.jpg "${destination}"/usr/share/backgrounds/armbian/
-
-# add loading desktop splash service
-mkdir -p "${destination}"/etc/systemd/system/
-cp "${SRC}"/packages/blobs/desktop/desktop-splash/desktop-splash.service "${destination}"/etc/systemd/system/desktop-splash.service
