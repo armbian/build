991d0ecb2dec49e1652ae255c7fafc8db339bf65
richneese <r.neese@gmail.com>
2020-11-05 06:30:49 -0500

    removed old dde dir was relaced by deepin

--- config/desktop/focal/environments/dde/custom/boards/nanopct4/armbian/create_desktop_package.sh
+++
@@ -1,7 +0,0 @@
-# install custom xorg for pinebook-pro
-cp -R "${SRC}"/packages/bsp/rk3399/xorg.conf "${destination}"/etc/X11/
-
-# install custom asound state for pinebook-pro
-cp -R "${SRC}"/packages/asound.state/ "${destination}"/etc/
-
-	
