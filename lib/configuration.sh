4307935dbc722bc4ba2f58b671a6c51f7f07d694
Miouyouyou (Myy) <myy@miouyouyou.fr>
2020-12-02 15:21:10 +0100

    Moved the desktop selection menus inside configuration.sh
    The whole problem being that, in order to provide a custom menu, for
    the selected board and architecture, I need to know the actual
    architecture of the selected board, which is not easy since the
    board definition files depends from other generic information files...
    
    The whole architecture sampling is done during configuration.sh, along
    with some default variables configuration just in case the board files
    didn't contain all the information.
    Trying to move the part that sample the ARCH information from the board
    definitions files inside main.sh, while leaving the default values
    initialization code in configuration.sh, left me with compiling an arm32
    image for an arm64 board, because the parsed variables were replaced
    by the default ones.
    
    So, the whole desktop selections menus have been moved inside
    configuration.sh, just after sampling the selected board information
    just like before, in order to provide only options that catter to the
    current build, and also provide actual error messages when invalid
    elements were selected.
    
    There's still errors to catch here and there, notably for appgroups,
    but the generated image works, which is the most important part.
    
    Signed-off-by: Miouyouyou (Myy) <myy@miouyouyou.fr>

--- lib/configuration.sh
+++ lib/configuration.sh
@@ -95,6 +95,256 @@ ATF_COMPILE=yes
 
 # single ext4 partition is the default and preferred configuration
 #BOOTFS_TYPE=''
+[[ ! -f ${SRC}/config/sources/families/$LINUXFAMILY.conf ]] && \
+	exit_with_error "Sources configuration not found" "$LINUXFAMILY"
+
+source "${SRC}/config/sources/families/${LINUXFAMILY}.conf"
+
+if [[ -f $USERPATCHES_PATH/sources/families/$LINUXFAMILY.conf ]]; then
+	display_alert "Adding user provided $LINUXFAMILY overrides"
+	source "$USERPATCHES_PATH/sources/families/${LINUXFAMILY}.conf"
+fi
+
+# load architecture defaults
+source "${SRC}/config/sources/${ARCH}.conf"
+
+# Myy : Menu configuration for choosing desktop configurations
+
+show_menu() {
+	provided_title=$1
+	provided_backtitle=$2
+	provided_menuname=$3
+	# Myy : I don't know why there's a TTY_Y - 8... 
+	#echo "Provided title : $provided_title"
+	#echo "Provided backtitle : $provided_backtitle"
+	#echo "Provided menuname : $provided_menuname"
+	#echo "Provided options : " "${@:4}"
+	#echo "TTY X: $TTY_X Y: $TTY_Y"
+	dialog --stdout --title "$provided_title" --backtitle "${provided_backtitle}" \
+	--menu "$provided_menuname" $TTY_Y $TTY_X $((TTY_Y - 8)) "${@:4}"
+}
+
+# Myy : FIXME Factorize
+show_select_menu() {
+	provided_title=$1
+	provided_backtitle=$2
+	provided_menuname=$3
+	dialog --stdout --title "${provided_title}" --backtitle "${provided_backtitle}" \
+	--checklist "${provided_menuname}" $TTY_Y $TTY_X $((TTY_Y - 8)) "${@:4}"
+}
+
+# Myy : Once we got a list of selected groups, parse the PACKAGE_LIST inside configuration.sh
+
+DESKTOP_ELEMENTS_DIR="${SRC}/config/desktop/${RELEASE}"
+DESKTOP_CONFIGS_DIR="${DESKTOP_ELEMENTS_DIR}/environments"
+DESKTOP_CONFIG_PREFIX="config_"
+DESKTOP_APPGROUPS_DIR="${DESKTOP_ELEMENTS_DIR}/appgroups"
+
+desktop_element_available_for_arch() {
+	local desktop_element_path="${1}"
+	local targeted_arch="${2}"
+
+	local arch_limitation_file="${1}/only_for"
+
+	display_alert "Checking if ${desktop_element_path} is available for ${targeted_arch} in ${arch_limitation_file}"
+	if [[ -f "${arch_limitation_file}" ]]; then
+		grep -- "${targeted_arch}" "${arch_limitation_file}"
+		return $?
+	else
+		return 0
+	fi
+}
+
+desktop_element_supported() {
+	local desktop_element_path="${1}"
+	
+	local support_level_filepath="${desktop_element_path}/support"
+	if [[ -f "${support_level_filepath}" ]]; then
+		local support_level="$(cat "${support_level_filepath}")"
+		if [[ "${support_level}" != "supported" && "${EXPERT}" != "yes" ]]; then
+			return 65
+		fi
+
+		desktop_element_available_for_arch "${desktop_element_path}" "${ARCH}"
+		if [[ $? -ne 0 ]]; then
+			return 66
+		fi
+	else
+		return 64
+	fi
+
+	return 0
+}
+
+# Expected environment variables :
+# - options
+# - ARCH
+desktop_environments_prepare_menu() {
+	for desktop_env_dir in "${DESKTOP_CONFIGS_DIR}/"*; do
+		local desktop_env_name=$(basename ${desktop_env_dir})
+		local expert_infos=""
+		[[ "${EXPERT}" == "yes" ]] && expert_infos="[$(cat "${desktop_env_dir}/support")]"
+		desktop_element_supported "${desktop_env_dir}" "${ARCH}" && options+=("${desktop_env_name}" "${desktop_env_name^} desktop environment ${expert_infos}")
+	done
+}
+
+if [[ $BUILD_DESKTOP == "yes" && -z $DESKTOP_ENVIRONMENT ]]; then
+	options=()
+	desktop_environments_prepare_menu
+
+	if [[ "${options[0]}" == "" ]]; then
+		exit_with_error "No desktop environment seems to be available for your board ${BOARD} (ARCH : ${ARCH} - EXPERT : ${EXPERT})"
+	fi
+
+	DESKTOP_ENVIRONMENT=$(show_menu "Choose a desktop environment" "$backtitle" "Select the default desktop environment to bundle with this image" "${options[@]}")
+
+	unset options
+
+	if [[ -z "${DESKTOP_ENVIRONMENT}" ]]; then
+		exit_with_error "No desktop environment selected..."
+	fi
+fi
+
+desktop_environment_check_if_valid() {
+	local error_msg=""
+	desktop_element_supported "${DESKTOP_ENVIRONMENT_DIRPATH}" "${ARCH}"
+	local retval=$?
+	
+	if [[ ${retval} == 0 ]]; then
+		return
+	elif [[ ${retval} == 64 ]]; then
+		error_msg+="Either the desktop environment ${DESKTOP_ENVIRONMENT} does not exist "
+		error_msg+="or the file ${DESKTOP_ENVIRONMENT_DIRPATH}/support is missing"
+	elif [[ ${retval} == 65 ]]; then
+		error_msg+="Only experts can build an image with the desktop environment \"${DESKTOP_ENVIRONMENT}\", since the Armbian team won't offer any support for it (EXPERT=${EXPERT})"
+	elif [[ ${retval} == 66 ]]; then
+		error_msg+="The desktop environment \"${DESKTOP_ENVIRONMENT}\" has no packages for your targeted board architecture (BOARD=${BOARD} ARCH=${ARCH}). "
+		error_msg+="The supported boards architectures are : "
+		error_msg+="$(cat "${DESKTOP_ENVIRONMENT_DIRPATH}/only_for")"
+	fi
+
+	exit_with_error "${error_msg}"
+}
+
+DESKTOP_ENVIRONMENT_DIRPATH="${DESKTOP_CONFIGS_DIR}/${DESKTOP_ENVIRONMENT}"
+
+desktop_environment_check_if_valid
+
+if [[ $BUILD_DESKTOP == "yes" && -z $DESKTOP_ENVIRONMENT_CONFIG_NAME ]]; then
+	# FIXME Check for empty folders, just in case the current maintainer
+	# messed up
+	# Note, we could also ignore it and don't show anything in the previous
+	# menu, but that hides information and make debugging harder, which I
+	# don't like. Adding desktop environments as a maintainer is not a
+	# trivial nor common task.
+
+	options=()
+	for configuration in "${DESKTOP_ENVIRONMENT_DIRPATH}/${DESKTOP_CONFIG_PREFIX}"*; do
+		config_filename=$(basename ${configuration})
+		config_name=${config_filename#"${DESKTOP_CONFIG_PREFIX}"}
+		options+=("${config_filename}" "${config_name} configuration")
+	done
+
+	DESKTOP_ENVIRONMENT_CONFIG_NAME=$(show_menu "Choose the desktop environment config" "$backtitle" "Select the configuration for this environment.\nThese are sourced from ${desktop_environment_config_dir}" "${options[@]}")
+	unset options
+
+	if [[ -z $DESKTOP_ENVIRONMENT_CONFIG_NAME ]]; then
+		exit_with_error "No desktop configuration selected... Do you really want a desktop environment ?"
+	fi
+fi
+
+DESKTOP_ENVIRONMENT_PACKAGE_LIST_DIRPATH="${DESKTOP_ENVIRONMENT_DIRPATH}/${DESKTOP_ENVIRONMENT_CONFIG_NAME}"
+DESKTOP_ENVIRONMENT_PACKAGE_LIST_FILEPATH="${DESKTOP_ENVIRONMENT_PACKAGE_LIST_DIRPATH}/packages"
+
+# "-z ${VAR+x}" allows to check for unset variable
+# Technically, someone might want to build a desktop with no additional
+# appgroups.
+if [[ $BUILD_DESKTOP == "yes" && -z ${DESKTOP_APPGROUPS_SELECTED+x} ]]; then
+
+	options=()
+	for appgroup_path in "${DESKTOP_APPGROUPS_DIR}/"*; do
+		appgroup="$(basename "${appgroup_path}")"
+		options+=("${appgroup}" "${appgroup^}" off)
+	done
+
+	DESKTOP_APPGROUPS_SELECTED=$(\
+		show_select_menu \
+		"Choose desktop softwares to add" \
+		"$backtitle" \
+		"Select which kind of softwares you'd like to add to your build" \
+		"${options[@]}")
+
+	unset options
+fi
+
+#exit_with_error 'Testing'
+
+# Expected variables :
+# - potential_paths
+# - BOARD
+# - DESKTOP_ENVIRONMENT
+# - DESKTOP_APPGROUPS_SELECTED
+# - DESKTOP_APPGROUPS_DIR
+# Example usage :
+# local potential_paths=""
+# get_all_potential_paths_for debian/postinst
+# echo $potential_paths
+
+get_all_potential_paths_for() {
+	looked_up_subpath="${1}"
+	potential_paths+=" ${DESKTOP_ENVIRONMENT_DIRPATH}/${looked_up_subpath}"
+	potential_paths+=" ${DESKTOP_ENVIRONMENT_PACKAGE_LIST_DIRPATH}/${looked_up_subpath}"
+	potential_paths+=" ${DESKTOP_ENVIRONMENT_DIRPATH}/custom/boards/${BOARD}/${looked_up_subpath}"
+	potential_paths+=" ${DESKTOP_ENVIRONMENT_PACKAGE_LIST_DIRPATH}/custom/boards/${BOARD}/${looked_up_subpath}"
+	for appgroup in ${DESKTOP_APPGROUPS_SELECTED}; do
+		appgroup_dirpath="${DESKTOP_APPGROUPS_DIR}/${appgroup}"
+		potential_paths+=" ${appgroup_dirpath}/${looked_up_subpath}"
+		potential_paths+=" ${appgroup_dirpath}/custom/desktops/${DESKTOP_ENVIRONMENT}/${looked_up_subpath}"
+		potential_paths+=" ${appgroup_dirpath}/custom/boards/${BOARD}/${looked_up_subpath}"
+		potential_paths+=" ${appgroup_dirpath}/custom/boards/${BOARD}/custom/desktops/${DESKTOP_ENVIRONMENT}/${looked_up_subpath}"
+	done
+}
+
+# Expected variables
+# - aggregated_content
+# - potential_paths
+# - separator
+# Write to variables :
+# - aggregated_content
+aggregate_content() {
+	echo "Potential paths : ${potential_paths}"
+
+	for filepath in ${potential_paths}; do
+		echo "$filepath exist ?"
+		if [[ -f "${filepath}" ]]; then
+			echo "Yes !"
+			aggregated_content+=$(cat "${filepath}")
+			aggregated_content+="${separator}"
+		else
+			echo "Nope :C"
+		fi
+
+	done
+}
+
+# Expected variables
+# - aggregated_content
+# - BOARD
+# - DESKTOP_ENVIRONMENT
+# - DESKTOP_APPGROUPS_SELECTED
+# - DESKTOP_APPGROUPS_DIR
+# Write to variables :
+# - aggregated_content
+aggregate_all() {
+	looked_up_subpath="${1}"
+	separator="${2}"
+
+	local potential_paths=""
+	get_all_potential_paths_for "${looked_up_subpath}"
+
+	aggregate_content
+
+}
 
 # set unique mounting directory
 SDCARD="${SRC}/.tmp/rootfs-${BRANCH}-${BOARD}-${RELEASE}-${DESKTOP_ENVIRONMENT:+"$DESKTOP_ENVIRONMENT-"}${BUILD_DESKTOP}-${SELECTED_CONFIGURATION}"
@@ -126,8 +376,6 @@ fi
 
 CLI_CONFIG_PATH="${SRC}/config/cli/${RELEASE}"
 DEBOOTSTRAP_CONFIG_PATH="${CLI_CONFIG_PATH}/debootstrap"
-
-desktop_element_available_for_arch "${DESKTOP_CONFIGS_DIR}/${DESKTOP_ENVIRONMENT}" "${ARCH}"
 
 if [[ $? != 0 ]]; then
 	exit_with_error "The desktop environment ${DESKTOP_ENVIRONMENT} is not available for your architecture ${ARCH}"
