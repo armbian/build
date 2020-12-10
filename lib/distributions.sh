1f53a51be9abc7ad1e851e41ac2c69d3812bd8a6
Igor Pecovnik <igor.pecovnik@gmail.com>
2020-12-10 16:12:41 +0100

    The directive After=graphical.target needs to be in the [Unit] section instead of the [Service]section.

--- lib/distributions.sh
+++ lib/distributions.sh
@@ -130,11 +130,12 @@ install_common()
 	mkdir -p "${SDCARD}"/etc/systemd/system/getty@.service.d/
 	mkdir -p "${SDCARD}"/etc/systemd/system/serial-getty@.service.d/
 	cat <<-EOF > "${SDCARD}"/etc/systemd/system/serial-getty@.service.d/override.conf
+	[Unit]
+	After=graphical.target
 	[Service]
 	ExecStartPre=/bin/sh -c 'exec /bin/sleep 10'
 	ExecStart=
 	ExecStart=-/sbin/agetty --noissue --autologin root %I $TERM
-	After=graphical.target
 	Type=idle
 	EOF
 	cp "${SDCARD}"/etc/systemd/system/serial-getty@.service.d/override.conf "${SDCARD}"/etc/systemd/system/getty@.service.d/override.conf
