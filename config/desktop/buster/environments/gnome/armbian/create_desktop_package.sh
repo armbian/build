fc71f9ffc03ea9847d908ca19e1ba48cf244425b
richneese <r.neese@gmail.com>
2020-12-20 11:01:38 -0500

    updated pkgs   removed unused pkgs from list

--- config/desktop/buster/environments/gnome/armbian/create_desktop_package.sh
+++ config/desktop/buster/environments/gnome/armbian/create_desktop_package.sh
@@ -2,6 +2,10 @@
 mkdir -p "${destination}"/etc/skel
 cp -R "${SRC}"/packages/blobs/desktop/skel/. "${destination}"/etc/skel
 
+# install logo for login screen
+mkdir -p "${destination}"/usr/share/pixmaps/armbian
+cp "${SRC}"/packages/blobs/desktop/icons/armbian.png "${destination}"/usr/share/pixmaps/armbian
+
 # install wallpapers
 mkdir -p "${destination}"/usr/share/backgrounds/gnome/
 cp "${SRC}"/packages/blobs/desktop/wallpapers/armbian*.jpg "${destination}"/usr/share/backgrounds/gnome/
