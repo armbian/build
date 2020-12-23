fdd305e52f0f3aa93235a1b5fba7dc5ef85eb6f3
Igor Pecovnik <igor.pecovnik@gmail.com>
2020-12-23 18:19:30 +0100

    Several backend fixes and improvemetens
    Affected:
    
    - repository old packages purging
    - multiple image making

--- lib/image-helpers.sh
+++ lib/image-helpers.sh
@@ -145,7 +145,7 @@ install_deb_chroot()
 	[[ $NO_APT_CACHER != yes ]] && local apt_extra="-o Acquire::http::Proxy=\"http://${APT_PROXY_ADDR:-localhost:3142}\" -o Acquire::http::Proxy::localhost=\"DIRECT\""
 	# when building in bulk from remote, lets make sure we have up2date index
 	[[ $BUILD_ALL == yes && ${variant} == remote ]] && chroot "${SDCARD}" /bin/bash -c "DEBIAN_FRONTEND=noninteractive apt-get $apt_extra -yqq update"
-	chroot "${SDCARD}" /bin/bash -c "DEBIAN_FRONTEND=noninteractive apt-get -yqq \
-		$apt_extra --no-install-recommends install $name" >> "${DEST}"/debug/install.log 2>&1
+	chroot "${SDCARD}" /bin/bash -c "DEBIAN_FRONTEND=noninteractive apt-get -yqq $apt_extra --no-install-recommends install $name" >> "${DEST}"/debug/install.log 2>&1
+	[[ $? -ne 0 ]] && exit_with_error "Installation of $name failed" "@host"
 	[[ ${variant} == remote && ${transfer} == yes ]] && rsync -rq "${SDCARD}"/var/cache/apt/archives/*.deb ${DEB_STORAGE}/
 }
