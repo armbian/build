2155f9f6f826d1e8e5df7b17a9adce4a00c33cea
richneese <r.neese@gmail.com>
2020-12-27 10:49:44 -0500

    fixed browesr changes

--- config/desktop/focal/appgroups/browsers/armbian/create_desktop_package.sh
+++ config/desktop/focal/appgroups/browsers/armbian/create_desktop_package.sh
@@ -1,4 +1,4 @@
 # install optimized browser configurations
-cp "${SRC}"/packages/blobs/desktop/chromium.conf "${destination}"/etc/armbian
-cp "${SRC}"/packages/blobs/desktop/firefox.conf  "${destination}"/etc/armbian
-cp -R "${SRC}"/packages/blobs/desktop/chromium "${destination}"/etc/armbian
+cp "${SRC}"/config/desktop/focaal/appgroups/browsers/configs/chromium.conf "${destination}"/etc/armbian
+cp "${SRC}"/config/desktop/focaal/appgroups/browsers/configs/firefox.conf  "${destination}"/etc/armbian
+cp -R "${SRC}"/config/desktop/focaal/appgroups/browsers/chromium "${destination}"/etc/armbian
