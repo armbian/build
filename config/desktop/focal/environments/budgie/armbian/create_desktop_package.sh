068a53685e6fe6eb7860d373d07eb183bfd2a49a
richneese <r.neese@gmail.com>
2020-12-27 10:30:24 -0500

    moved  packages/blobs/desktop to new dir conig/desktop/desktop-extras
    moved dirs structure and made code chaanges to fix.

--- config/desktop/focal/environments/budgie/armbian/create_desktop_package.sh
+++ config/desktop/focal/environments/budgie/armbian/create_desktop_package.sh
@@ -1,17 +1,17 @@
 # install lightdm greeter
-cp -R "${SRC}"/packages/blobs/desktop/lightdm "${destination}"/etc/armbian
+cp -R "${SRC}"/config/desktop/desktop-extras/lightdm "${destination}"/etc/armbian
 
 # install default desktop settings
-mkdir -p "${destination}"/etc/skel
-cp -R "${SRC}"/packages/blobs/desktop/skel/. "${destination}"/etc/skel
+#mkdir -p "${destination}"/etc/skel
+#cp -R "${SRC}"/config/desktop/focal/enviroments/budgie/skel/. "${destination}"/etc/skel
 
 # install logo for login screen
 mkdir -p "${destination}"/usr/share/pixmaps/armbian
-cp "${SRC}"/packages/blobs/desktop/icons/armbian.png "${destination}"/usr/share/pixmaps/armbian
+cp "${SRC}"config/desktop/desktop-extras/icons/armbian.png "${destination}"/usr/share/pixmaps/armbian
 
 # install wallpapers
 mkdir -p "${destination}"/usr/share/backgrounds/armbian/
-cp "${SRC}"/packages/blobs/desktop/wallpapers/armbian*.jpg "${destination}"/usr/share/backgrounds/armbian/
+cp "${SRC}"/config/desktop/desktop-extras/wallpapers/armbian*.jpg "${destination}"/usr/share/backgrounds/armbian/
 
 mkdir -p "${destination}"/usr/share/gnome-background-properties
 cat <<-EOF > "${destination}"/usr/share/gnome-background-properties/armbian.xml
--- config/desktop/focal/environments/gnome/armbian/create_desktop_package.sh
+++ config/desktop/focal/environments/gnome/armbian/create_desktop_package.sh
@@ -1,14 +1,14 @@
 # install default desktop settings
-mkdir -p "${destination}"/etc/skel
-cp -R "${SRC}"/packages/blobs/desktop/skel/. "${destination}"/etc/skel
+#mkdir -p "${destination}"/etc/skel
+#cp -R "${SRC}"/config/desktop/focal/evviroments.deepin/skel/. "${destination}"/etc/skel
 
 # install logo for login screen
 mkdir -p "${destination}"/usr/share/pixmaps/armbian
-cp "${SRC}"/packages/blobs/desktop/icons/armbian.png "${destination}"/usr/share/pixmaps/armbian
+cp "${SRC}"/config/desktop/desktop-extras/icons/armbian.png "${destination}"/usr/share/pixmaps/armbian
 
 # install wallpapers
 mkdir -p "${destination}"/usr/share/backgrounds/armbian/
-cp "${SRC}"/packages/blobs/desktop/wallpapers/armbian*.jpg "${destination}"/usr/share/backgrounds/armbian/
+cp "${SRC}"/config/desktop/desktop-extras/wallpapers/armbian*.jpg "${destination}"/usr/share/backgrounds/armbian/
 mkdir -p "${destination}"/usr/share/gnome-background-properties
 cat <<-EOF > "${destination}"/usr/share/gnome-background-properties/armbian.xml
 <?xml version="1.0"?>
