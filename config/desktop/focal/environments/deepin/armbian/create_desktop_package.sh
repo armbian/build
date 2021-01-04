9115f1629f02aa05cc38792fd6ddcbb22bf05be3
Richard Neese <r.neese@gmail.com>
2021-01-04 11:33:21 -0500

    fix dde/deepin sel build issue (#2530)

--- config/desktop/focal/environments/deepin/armbian/create_desktop_package.sh
+++ config/desktop/focal/environments/deepin/armbian/create_desktop_package.sh
@@ -1,10 +1,6 @@
 # install lightdm greeter
 cp -R "${SRC}"/packages/blobs/desktop/lightdm "${destination}"/etc/armbian
 
-# install default desktop settings
-mkdir -p "${destination}"/etc/skel
-cp -R "${SRC}"/packages/blobs/desktop/skel_dde/. "${destination}"/etc/skel
-
 # install logo for login screen
 mkdir -p "${destination}"/usr/share/pixmaps/armbian
 cp "${SRC}"/packages/blobs/desktop/icons/armbian.png "${destination}"/usr/share/pixmaps/armbian
