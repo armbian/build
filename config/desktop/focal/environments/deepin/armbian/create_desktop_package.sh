667c8df4322967248eb7b0179803589eb8c45332
richneese <r.neese@gmail.com>
2021-01-13 07:10:18 -0500

    fixiing deepin to use backgrounds now wallpapers like other desktops

--- config/desktop/focal/environments/deepin/armbian/create_desktop_package.sh
+++ config/desktop/focal/environments/deepin/armbian/create_desktop_package.sh
@@ -5,6 +5,6 @@ cp -R "${SRC}"/packages/blobs/desktop/lightdm "${destination}"/etc/armbian
 mkdir -p "${destination}"/usr/share/pixmaps/armbian
 cp "${SRC}"/packages/blobs/desktop/icons/armbian.png "${destination}"/usr/share/pixmaps/armbian
 
-# install wallpapers
-mkdir -p "${destination}"/usr/share/wallpapers/armbian/
-cp "${SRC}"/packages/blobs/desktop/wallpapers/armbian*.jpg "${destination}"/usr/share/wallpapers/armbian/
+# install backgrounds
+mkdir -p "${destination}"/usr/share/backgrounds/armbian/
+cp "${SRC}"/packages/blobs/desktop/wallpapers/armbian*.jpg "${destination}"/usr/share/backgrounds/armbian/
