484a892a11047d304e7cfee5bca6d8e13658a1c2
richneese <r.neese@gmail.com>
2021-01-27 20:08:59 -0500

    working on mate themeing

--- config/desktop/focal/environments/mate/armbian/create_desktop_package.sh
+++ config/desktop/focal/environments/mate/armbian/create_desktop_package.sh
@@ -1,6 +1,3 @@
-# install lightdm greeter
-cp -R "${SRC}"/packages/blobs/desktop/lightdm "${destination}"/etc/armbian
-
 # install default desktop settings
 mkdir -p "${destination}"/etc/skel
 cp -R "${SRC}"/packages/blobs/desktop/skel/. "${destination}"/etc/skel
@@ -12,7 +9,6 @@ cp "${SRC}"/packages/blobs/desktop/icons/armbian.png "${destination}"/usr/share/
 # install wallpapers
 mkdir -p "${destination}"/usr/share/backgrounds/armbian/
 cp "${SRC}"/packages/blobs/desktop/wallpapers/armbian*.jpg "${destination}"/usr/share/backgrounds/armbian/
-
 mkdir -p "${destination}"/usr/share/gnome-background-properties
 cat <<-EOF > "${destination}"/usr/share/gnome-background-properties/armbian.xml
 <?xml version="1.0"?>
@@ -32,12 +28,19 @@ cat <<-EOF > "${destination}"/usr/share/gnome-background-properties/armbian.xml
     <pcolor>#ffffff</pcolor>
     <scolor>#000000</scolor>
   </wallpaper>
+    <wallpaper deleted="false">
+    <name>Armbian uc</name>
+    <filename>/usr/share/backgrounds/armbian/armbian-full-undeer-construction-3840-2160.jpg</filename>
+    <options>zoom</options>
+    <pcolor>#ffffff</pcolor>
+    <scolor>#000000</scolor>
+  </wallpaper>
+    <wallpaper deleted="false">
+    <name>Armbian clear</name>
+    <filename>/usr/share/backgrounds/armbian/Armbian-clear-rounded-bakcground-3840-2160.jpg</filename>
+    <options>zoom</options>
+    <pcolor>#ffffff</pcolor>
+    <scolor>#000000</scolor>
+  </wallpaper>
 </wallpapers>
-EOF
-
-mkdir -p "${destination}"/usr/share/glib-2.0/schemas
-cat <<-EOF > "${destination}"/usr/share/glib-2.0/schemas/org.gnome.desktop.background.gschema.override
-[org.gnome.desktop.background]
-picture-uri='file:///usr/share/backgrounds/armbian/armbian03-Dre0x-Minum-dark-3840x2160.jpg'
-show-desktop-icons=true
 EOF
