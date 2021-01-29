8f0734e9811a18df2835ce454bc828c651ac9051
richneese <r.neese@gmail.com>
2021-01-28 19:10:53 -0500

    fixing lightdm on gnome missing line for skel cp

--- config/desktop/focal/environments/gnome/armbian/create_desktop_package.sh
+++ config/desktop/focal/environments/gnome/armbian/create_desktop_package.sh
@@ -1,3 +1,6 @@
+# install lightdm greeter
+cp -R "${SRC}"/packages/blobs/desktop/lightdm "${destination}"/etc/armbian
+
 # install default desktop settings
 mkdir -p "${destination}"/etc/skel
 cp -R "${SRC}"/packages/blobs/desktop/skel/. "${destination}"/etc/skel
