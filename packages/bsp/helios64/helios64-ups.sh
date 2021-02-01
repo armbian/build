ce9fda4fbec7c45d60a131bef97f6781f93e146e
Evaldas Palaima <evaldas.palaima@gmail.com>
2021-02-01 17:06:33 +0200

    Helios64 UPS. System shutdown when battery reach 7.0V.

---
+++ packages/bsp/helios64/helios64-ups.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+#7.0V 	916 	Recommended threshold to force shutdown system
+TH=916
+
+val=$(cat /sys/bus/iio/devices/iio\:device0/in_voltage2_raw)
+sca=$(cat /sys/bus/iio/devices/iio\:device0/in_voltage_scale)
+adc=$(echo "$val * $sca / 1" | bc)
+
+if [[ $adc -le $TH ]]; then
+	echo "Shutdown"
+	/usr/sbin/poweroff
+fi
