daaf72c1e63ba58f56b09c3568e89dc1c2665dc0
Igor Pecovnik <igor.pecovnik@gmail.com>
2021-03-06 20:24:15 +0100

    Distribution support status was not read when creating only BSP package

--- lib/general.sh
+++ lib/general.sh
@@ -632,15 +632,15 @@ function distros_options() {
 
 function set_distribution_status() {
 
-  local distro_support_desc_filepath="${DISTRIBUTIONS_DESC_DIR}/${RELEASE}/support"
+	local distro_support_desc_filepath="${SRC}/${DISTRIBUTIONS_DESC_DIR}/${RELEASE}/support"
 	if [[ ! -f "${distro_support_desc_filepath}" ]]; then
 		exit_with_error "Distribution ${distribution_name} does not exist"
 	else
 		DISTRIBUTION_STATUS="$(cat "${distro_support_desc_filepath}")"
 	fi
-	
+
 	[[ "${DISTRIBUTION_STATUS}" != "supported" ]] && [[ "${EXPERT}" != "yes" ]] && exit_with_error "Armbian ${RELEASE} is unsupported and, therefore, only available to experts (EXPERT=yes)"
-	 
+
 }
 
 adding_packages()
