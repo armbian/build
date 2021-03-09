57ce7aaa6fb3e9c7cf85a32d635ecccd26c159c5
Richard Neese <r.neese@gmail.com>
2021-03-09 12:21:59 -0500

    fixing missing cmd when building pinebook pro (#2702)

--- config/optional/boards/pinebook-pro/_config/desktop/_all_distributions/environments/_all_environments/armbian/create_desktop_package.sh
+++ config/optional/boards/pinebook-pro/_config/desktop/_all_distributions/environments/_all_environments/armbian/create_desktop_package.sh
@@ -1,4 +1,5 @@
 # install custom xorg for pinebook-pro
+mkdir -p "${destination}"/etc/X11/xorg.conf.d/
 cp -R "${SRC}"/packages/bsp/pinebook-pro/xorg.conf "${destination}"/etc/X11/xorg.conf.d/
 
 # install custom asound state for pinebook-pro
