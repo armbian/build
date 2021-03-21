d5bb65f8f8df4d73a5eb1109b2d15273dc3f586b
balbes150 <balbes-150@yandex.ru>
2021-03-21 13:36:20 +0300

    fix minor station

--- lib/compilation-prepare.sh
+++ lib/compilation-prepare.sh
@@ -62,7 +62,7 @@ compilation_prepare()
 	fi
 
 	if [[ "${version}" == "4.4."* ]] && \
-	[[ "$LINUXFAMILY" == rockchip64 ]] || [[ "$LINUXFAMILY" == station* ]]; then
+	[[ "$LINUXFAMILY" == rockchip64 || "$LINUXFAMILY" == station* ]]; then
 		display_alert "Adjustin" "packaging" "info"
 		cd "$kerneldir" || exit
 		process_patch_file "${SRC}/patch/misc/general-packaging-4.4.y-rockchip64.patch" "applying"
