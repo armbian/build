bdc8135a24d08ed28ff047b56f5ee76463974600
Richard Neese <r.neese@gmail.com>
2021-05-09 08:50:27 -0400

    Update create_desktop_package.sh
    cleaning up spacing

--- config/optional/boards/pinebook-pro/_config/desktop/_all_distributions/environments/_all_environments/armbian/create_desktop_package.sh
+++ config/optional/boards/pinebook-pro/_config/desktop/_all_distributions/environments/_all_environments/armbian/create_desktop_package.sh
@@ -9,6 +9,7 @@ cp -R "${SRC}"/packages/blobs/asound.state/asound.state.pinebook-pro "${destinat
 mkdir -p "${destination}"/etc/X11/xorg.conf.d/
 # from https://github.com/ayufan-rock64/linux-package/tree/master/root-pinebookpro
 cp "${SRC}"/packages/bsp/pinebook-pro/40-pinebookpro-touchpad.conf "${destination}"/etc/X11/xorg.conf.d/
+
 #keyboard hwdb
 mkdir -p "${destination}"/etc/udev/hwdb.d/
 cp "${SRC}"/packages/bsp/pinebook-pro/10-usb-kbd.hwdb "${destination}"/etc/udev/hwdb.d/
