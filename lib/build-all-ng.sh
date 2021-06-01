e23133fb997b196a83d1f3b300db3600ae4b2af9
Igor Pecovnik <igor.pecovnik@gmail.com>
2021-06-01 18:51:47 +0200

    Improve patch hashing method
    When running script inside Github runner, hashing is different due to slightly different sort ...

--- lib/build-all-ng.sh
+++ lib/build-all-ng.sh
@@ -211,7 +211,7 @@ function check_hash()
 	[[ -z $LINUXFAMILY ]] && LINUXFAMILY=$BOARDFAMILY
 	[[ -z ${KERNELPATCHDIR} ]] && KERNELPATCHDIR=$LINUXFAMILY-$BRANCH
 	[[ -z ${LINUXCONFIG} ]] && LINUXCONFIG=linux-$LINUXFAMILY-$BRANCH
-	hash_watch_1=$(find "${SRC}/patch/kernel/${KERNELPATCHDIR}"/ -maxdepth 1 -printf '%s %P\n' 2> /dev/null | sort)
+	hash_watch_1=$(ls -l "${SRC}/patch/kernel/${KERNELPATCHDIR}"/ | awk '{print $9,$5}' | sort -r | xargs)
 	hash_watch_2=$(cat "${SRC}/config/kernel/${LINUXCONFIG}.config" 2> /dev/null)
 	patch_hash=$(echo "${hash_watch_1}${hash_watch_2}" | improved_git hash-object --stdin)
 
