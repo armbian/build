cda929a1b6383d5bdf767a94dea8379df8318bec
iav <iav@iav.lv>
2021-06-04 11:53:45 +0300

    Make builder dockerfile to be x64 and aarch64 compatible (#2867)
    * Make builder dockerfile to be x64 and aarch64 compatible
    and building speed optimization
    
    * remove hardcoded amd64
    currently docker from edge not work, stable works, then stable instead edge
    
    * libc6-dev-armhf-cross need to build rockchip uboot for helios64 under arm64
    
    * need to add architecture to avoid blame for no docker for i386 arch
    
    * hirsute uses same names for qemu-user-static
    
    * let's only x64 use aptly from dev

--- lib/general.sh
+++ lib/general.sh
@@ -1025,7 +1025,7 @@ prepare_host()
 	local hostdeps="wget ca-certificates device-tree-compiler pv bc lzop zip binfmt-support build-essential ccache debootstrap ntpdate \
 	gawk gcc-arm-linux-gnueabihf qemu-user-static u-boot-tools uuid-dev zlib1g-dev unzip libusb-1.0-0-dev fakeroot \
 	parted pkg-config libncurses5-dev whiptail debian-keyring debian-archive-keyring f2fs-tools libfile-fcntllock-perl rsync libssl-dev \
-	nfs-kernel-server btrfs-progs ncurses-term p7zip-full kmod dosfstools libc6-dev-armhf-cross imagemagick \
+	nfs-kernel-server btrfs-progs ncurses-term p7zip-full kmod dosfstools libc6-amd64-cross libc6-dev-armhf-cross imagemagick \
 	curl patchutils liblz4-tool libpython2.7-dev linux-base swig aptly acl python3-dev \
 	locales ncurses-base pixz dialog systemd-container udev libfdt-dev libc6 qemu \
 	bison libbison-dev flex libfl-dev cryptsetup gpg gnupg1 cpio aria2 pigz dirmngr python3-distutils"
