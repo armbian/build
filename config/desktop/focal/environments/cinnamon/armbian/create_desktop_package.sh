f44114b278c9930142b65eaaee4603d5b845a07c
Richard Neese <r.neese@gmail.com>
2021-06-29 07:12:35 -0400

    new cinnamon desktop themeing (#2934)

--- config/desktop/focal/environments/cinnamon/armbian/create_desktop_package.sh
+++ config/desktop/focal/environments/cinnamon/armbian/create_desktop_package.sh
@@ -9,6 +9,10 @@ cp -R "${SRC}"/packages/blobs/desktop/skel/. "${destination}"/etc/skel
 mkdir -p "${destination}"/usr/share/pixmaps/armbian
 cp "${SRC}"/packages/blobs/desktop/icons/armbian.png "${destination}"/usr/share/pixmaps/armbian
 
+i#install cinnamon desktop bar icons
+mkdir -p "${destination}"/usr/share/icons/armbian
+cp "${SRC}"/packages/blobs/desktop/desktop-icons/*.png "${destination}"/usr/share/icons/armbian
+
 # install wallpapers
 mkdir -p "${destination}"/usr/share/backgrounds/armbian/
 cp "${SRC}"/packages/blobs/desktop/wallpapers/*.jpg "${destination}"/usr/share/backgrounds/armbian/
