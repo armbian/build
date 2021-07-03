eac1c50993bdbc980920accd983a2a0b133fdc1f
Rich <r.neese@gmail.com>
2021-07-02 22:22:34 -0400

    new budgie updates fixing pkg list and slick greeter

--- config/desktop/focal/environments/budgie/armbian/create_desktop_package.sh
+++ config/desktop/focal/environments/budgie/armbian/create_desktop_package.sh
@@ -9,7 +9,7 @@ cp -R "${SRC}"/packages/blobs/desktop/skel/. "${destination}"/etc/skel
 mkdir -p "${destination}"/usr/share/pixmaps/armbian
 cp "${SRC}"/packages/blobs/desktop/icons/armbian.png "${destination}"/usr/share/pixmaps/armbian
 
-#install cinnamon desktop bar icons
+#install desktop bar icons
 mkdir -p "${destination}"/usr/share/icons/armbian
 cp "${SRC}"/packages/blobs/desktop/desktop-icons/*.png "${destination}"/usr/share/icons/armbian
 
@@ -18,7 +18,7 @@ mkdir -p "${destination}"/usr/share/backgrounds/armbian/
 cp "${SRC}"/packages/blobs/desktop/wallpapers/*.jpg "${destination}"/usr/share/backgrounds/armbian/
 
 mkdir -p "${destination}"/usr/share/cinnamon-background-properties
-cat <<-EOF > "${destination}"/usr/share/cinnamon-background-properties/armbian.xml
+cat <<EOF > "${destination}"/usr/share/cinnamon-background-properties/armbian.xml
 <?xml version="1.0"?>
 <!DOCTYPE wallpapers SYSTEM "cinnamon-wp-list.dtd">
 <wallpapers>
@@ -36,20 +36,20 @@ cat <<-EOF > "${destination}"/usr/share/cinnamon-background-properties/armbian.x
     <pcolor>#ffffff</pcolor>
     <scolor>#000000</scolor>
   </wallpaper>
-    <wallpaper deleted="false">
+  <wallpaper deleted="false">
     <name>Armbian uc</name>
     <filename>/usr/share/backgrounds/armbian/armbian-full-undeer-construction-3840-2160.jpg</filename>
     <options>zoom</options>
     <pcolor>#ffffff</pcolor>
     <scolor>#000000</scolor>
   </wallpaper>
-    <wallpaper deleted="false">
+  <wallpaper deleted="false">
     <name>Armbian clear</name>
     <filename>/usr/share/backgrounds/armbian/Armbian-clear-rounded-bakcground-3840-2160.jpg</filename>
     <options>zoom</options>
     <pcolor>#ffffff</pcolor>
     <scolor>#000000</scolor>
-   </wallpaper>
+  </wallpaper>
   <wallpaper deleted="false">
     <name>Armbian black_pyscho</name>
     <filename>/usr/share/backgrounds/armbian/armbian_4k_black_psycho.jpg</filename>
@@ -57,28 +57,28 @@ cat <<-EOF > "${destination}"/usr/share/cinnamon-background-properties/armbian.x
     <pcolor>#ffffff</pcolor>
     <scolor>#000000</scolor>
   </wallpaper>
-    <wallpaper deleted="false">
+  <wallpaper deleted="false">
     <name>Armbian bluie_monday</name>
     <filename>/usr/share/backgrounds/armbian/armbian_4k_bluie_monday.jpg</filename>
     <options>zoom</options>
     <pcolor>#ffffff</pcolor>
     <scolor>#000000</scolor>
   </wallpaper>
-    <wallpaper deleted="false">
+  <wallpaper deleted="false">
     <name>Armbian grey</name>
     <filename>/usr/share/backgrounds/armbian/armbian_4k_gray.jpg</filename>
     <options>zoom</options>
     <pcolor>#ffffff</pcolor>
     <scolor>#000000</scolor>
   </wallpaper>
-    <wallpaper deleted="false">
+  <wallpaper deleted="false">
     <name>Armbian green</name>
     <filename>/usr/share/backgrounds/armbian/armbian_4k_green.jpg</filename>
     <options>zoom</options>
     <pcolor>#ffffff</pcolor>
     <scolor>#000000</scolor>
   </wallpaper>
-      <wallpaper deleted="false">
+  <wallpaper deleted="false">
     <name>Armbian purplepunk</name>
     <filename>/usr/share/backgrounds/armbian/armbian_4k_purplepunk.jpg</filename>
     <options>zoom</options>
