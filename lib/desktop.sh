d5c6880d437a487ee82438b1820d0e5c55a4b185
Igor Pecovnik <igor.pecovnik@gmail.com>
2021-07-05 17:36:00 +0200

    Add support for desktop board support package
    Extracting from https://github.com/armbian/build/pull/2776 which can be closed after.

--- lib/desktop.sh
+++ lib/desktop.sh
@@ -62,7 +62,7 @@ create_desktop_package ()
 	Installed-Size: 1
 	Section: xorg
 	Priority: optional
-	Recommends: ${DEBIAN_RECOMMENDS//[:space:]+/,}
+	Recommends: ${DEBIAN_RECOMMENDS//[:space:]+/,}, armbian-bsp-desktop
 	Provides: ${CHOSEN_DESKTOP}, armbian-${RELEASE}-desktop
 	Pre-Depends: ${PACKAGE_LIST_PREDEPENDS//[:space:]+/,}
 	Description: Armbian desktop for ${DISTRIBUTION} ${RELEASE}
@@ -106,6 +106,97 @@ create_desktop_package ()
 	mkdir -p "${DEB_STORAGE}/${RELEASE}"
 	cd "${destination}"; cd ..
 	fakeroot dpkg-deb -b "${destination}" "${DEB_STORAGE}/${RELEASE}/${CHOSEN_DESKTOP}_${REVISION}_all.deb"  >/dev/null
+
+	# cleanup
+	rm -rf "${tmp_dir}"
+
+	unset aggregated_content
+
+}
+
+
+
+
+copy_all_packages_files_for()
+{
+	local package_name="${1}"
+	for package_src_dir in ${PACKAGES_SEARCH_ROOT_ABSOLUTE_DIRS};
+	do
+		local package_dirpath="${package_src_dir}/${package_name}"
+		if [ -d "${package_dirpath}" ];
+		then
+			cp -r "${package_dirpath}/"* "${destination}/"
+			echo "${package_dirpath}"
+			echo ${package_dirpath} >> "${DEST}"/debug/copy.log
+		fi
+	done
+}
+
+create_bsp_desktop_package ()
+{
+
+	local package_name="${BSP_DESKTOP_PACKAGE_FULLNAME}"
+
+	local destination tmp_dir
+	tmp_dir=$(mktemp -d)
+	destination=${tmp_dir}/${BOARD}/${BSP_DESKTOP_PACKAGE_FULLNAME}
+	rm -rf "${destination}"
+	mkdir -p "${destination}"/DEBIAN
+
+	copy_all_packages_files_for "bsp-desktop"
+
+	# set up control file
+	cat <<-EOF > "${destination}"/DEBIAN/control
+	Package: armbian-bsp-desktop-${BOARD}
+	Version: $REVISION
+	Architecture: all
+	Maintainer: $MAINTAINER <$MAINTAINERMAIL>
+	Installed-Size: 1
+	Section: xorg
+	Priority: optional
+	Provides: armbian-bsp-desktop, armbian-bsp-desktop-${BOARD}
+	Description: Armbian Board Specific Packages for desktop users using ${BOARD} machines
+	EOF
+
+	#display_alert "Showing ${destination}/DEBIAN/control"
+	cat "${destination}"/DEBIAN/control >> "${DEST}"/debug/install.log
+
+	# Recreating the DEBIAN/postinst file
+	echo "#!/bin/sh -e" > "${destination}/DEBIAN/postinst"
+
+	local aggregated_content=""
+
+	aggregate_all_desktop "debian/armbian-bsp-desktop/postinst" $'\n'
+
+	echo "${aggregated_content}" >> "${destination}/DEBIAN/postinst"
+	echo "exit 0" >> "${destination}/DEBIAN/postinst"
+
+	chmod 755 "${destination}"/DEBIAN/postinst
+
+	#display_alert "Showing ${destination}/DEBIAN/postinst"
+	cat "${destination}/DEBIAN/postinst" >> "${DEST}"/debug/install.log
+
+	# Armbian create_desktop_package scripts
+
+	unset aggregated_content
+
+	# Myy : I'm preparing the common armbian folders, in advance, since the scripts are now splitted
+	mkdir -p "${destination}"/etc/armbian
+
+	local aggregated_content=""
+
+	aggregate_all_desktop "debian/armbian-bsp-desktop/prepare.sh" $'\n'
+
+	# display_alert "Showing the user scripts executed in create_desktop_package"
+	echo "${aggregated_content}" >> "${DEST}"/debug/install.log
+	eval "${aggregated_content}"
+
+	# create board DEB file
+	display_alert "Building desktop package" "${package_name}" "info"
+
+	mkdir -p "${DEB_STORAGE}/${RELEASE}"
+	cd "${destination}"; cd ..
+	fakeroot dpkg-deb -b "${destination}" "${DEB_STORAGE}/${RELEASE}/${package_name}.deb"  >/dev/null
 
 	# cleanup
 	rm -rf "${tmp_dir}"
