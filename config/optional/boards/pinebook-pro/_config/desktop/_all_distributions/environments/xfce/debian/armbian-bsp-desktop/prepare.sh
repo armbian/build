2ca0c74b7cf1858bb27dbf97fb5c216ce0a8b0de
Igor Pecovnik <igor.pecovnik@gmail.com>
2021-07-18 15:34:21 +0200

    Adjust "

--- config/optional/boards/pinebook-pro/_config/desktop/_all_distributions/environments/xfce/debian/armbian-bsp-desktop/prepare.sh
+++ config/optional/boards/pinebook-pro/_config/desktop/_all_distributions/environments/xfce/debian/armbian-bsp-desktop/prepare.sh
@@ -1,3 +1,3 @@
-# powerconfig, touchpad, and special keys
-mkdir -p $destination/etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml/
-cp $SRC/packages/bsp/pinebook-pro/pointers.xml                 $destination/etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml/
+# special keys
+mkdir -p "$(destination)"/etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml/
+cp $SRC/packages/bsp/pinebook-pro/pointers.xml  "$(destination)"/etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml/
