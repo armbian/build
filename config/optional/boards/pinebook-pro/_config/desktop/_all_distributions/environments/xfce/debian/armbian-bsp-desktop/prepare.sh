a46eeafa8672a080fbba4e9d49eaca11ff5246e6
lanefu <lanefu@users.noreply.github.com>
2021-07-19 02:14:34 -0400

    move pinebook tweaks (#3033)
    * move pinebook tweaks
    
    * fix dest
    
    * Add also special keys definitions
    
    * Clear out tech dept
    
    * Delete prepare.sh
    
    Co-authored-by: Igor Pečovnik <igorpecovnik@users.noreply.github.com>

--- config/optional/boards/pinebook-pro/_config/desktop/_all_distributions/environments/xfce/debian/armbian-bsp-desktop/prepare.sh
+++
@@ -1,3 +0,0 @@
-# special keys
-mkdir -p "$(destination)"/etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml/
-cp $SRC/packages/bsp/pinebook-pro/pointers.xml  "$(destination)"/etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml/
