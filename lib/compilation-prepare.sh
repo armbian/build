fb68bf6d480c258578f87e70163fcc203feab569
Vyacheslav Bocharov <adeep@lexina.in>
2021-10-19 17:15:13 +0300

    Add gpio power reset for hci_h5 controllers in h5_btrtl_open

--- lib/compilation-prepare.sh
+++ lib/compilation-prepare.sh
@@ -590,6 +590,7 @@ compilation_prepare()
 		display_alert "Adding" "Bluetooth support for Realtek 8822CS (hci_ver 0x8) chipsets" "info"
 
 		process_patch_file "${SRC}/patch/misc/bluetooth-rtl8822cs-hci_ver-0x8.patch" "applying"
+		process_patch_file "${SRC}/patch/misc/Bluetooth-hci_h5-Add-power-reset-via-gpio-in-h5_btrt.patch" "applying"
 
 	fi
 
