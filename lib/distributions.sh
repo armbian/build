a4fc36b6aa7eefdb5abcd555ad69aff5bc575228
Ricardo Pardini <ricardo@pardini.net>
2021-10-24 19:57:40 +0200

    fix security repo for bullseye
    - with bullseye, comes a security repo layout change
    - see https://www.debian.org/releases/bullseye/arm64/release-notes/ch-information.en.html#security-archive
    
    Signed-off-by: Ricardo Pardini <ricardo@pardini.net>

--- lib/distributions.sh
+++ lib/distributions.sh
@@ -603,8 +603,8 @@ install_distribution_specific()
 			[[ $(grep -L "VERSION_ID=" "${SDCARD}"/etc/os-release) ]] && echo 'VERSION_ID="11"' >> "${SDCARD}"/etc/os-release
 			[[ $(grep -L "VERSION=" "${SDCARD}"/etc/os-release) ]] && echo 'VERSION="11 (bullseye)"' >> "${SDCARD}"/etc/os-release
 
-			# remove security updates repository since it does not exists yet
-			sed '/security/ d' -i "${SDCARD}"/etc/apt/sources.list
+			# Bullseye changed the security repo layout, see https://www.debian.org/releases/bullseye/arm64/release-notes/ch-information.en.html#security-archive
+			sed -i "s/${RELEASE}\/updates/${RELEASE}-security/g" "${SDCARD}"/etc/apt/sources.list
 
 		;;
 	bionic|focal|hirsute|impish)
