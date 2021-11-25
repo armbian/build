f0541c3b4b2d1fc926e08f734801af06d0abd9a9
Igor Pecovnik <igor.pecovnik@gmail.com>
2021-11-25 14:39:15 +0100

    Add internal switch which allows to skip building certain kernel families even conditions are met.
    This helps when we have no time to work on certain branch patches right now, while all others builds without a problem.
    
    We create a file this way:
    https://github.com/armbian/scripts/commit/c0c6fd3a0252784bdc91eaeb8b74c3d34141697f

--- lib/build-all-ng.sh
+++ lib/build-all-ng.sh
@@ -210,8 +210,6 @@ function check_hash()
 		ref_name=HEAD
 	else
 		ref_name=${KERNELBRANCH##*:}
-		# handle sunxi exceptions
-		[[ $ref_name != "*.y" ]] && ref_name=$ref_name".y"
 	fi
 	[[ -z $LINUXFAMILY ]] && LINUXFAMILY=$BOARDFAMILY
 	[[ -z ${KERNELPATCHDIR} ]] && KERNELPATCHDIR=$LINUXFAMILY-$BRANCH
@@ -305,7 +303,6 @@ function build_all()
 		[[ ${BOARDFAMILY} == sun*iw* ]] && BOARDFAMILY=sunxi64
 		[[ ${BOARDFAMILY} == meson8b ]] && BOARDFAMILY=meson
 		[[ ${BOARDFAMILY} == meson-* ]] && BOARDFAMILY=meson64
-
 		# small optimisation. we only (try to) build needed kernels
 		if [[ $KERNEL_ONLY == yes ]]; then
 			LINUXFAMILY="${BOARDFAMILY}"
@@ -324,6 +321,9 @@ function build_all()
 		[[ ${BUILD_TARGET} == "desktop" && ${BSP_BUILD} != "yes" ]] && BUILD_DESKTOP="yes"
 		[[ ${BUILD_TARGET} == "minimal" ]] && BUILD_MINIMAL="yes"
 		[[ ${BSP_BUILD} == yes ]] && BUILD_STABILITY=$STABILITY
+
+		# create a file and put grep style list of the one that must be skipped: sunxi\|sunxi64
+		[[ -f userpatches/family.skip ]] && grep -qw "$BOARDFAMILY" userpatches/family.skip && continue
 
 		# create beta or stable
 		if [[ "${BUILD_STABILITY}" == "${STABILITY}" ]]; then
@@ -438,6 +438,7 @@ else
 
 # display what will be build
 echo ""
+[[ -f userpatches/family.skip ]] && display_alert "userpatches/family.skip exists and familes noted there will be skipped" ""  "wrn"
 display_alert "Building all targets" "$STABILITY $(if [[ $KERNEL_ONLY == "yes" ]] ; then echo "kernels"; \
 else echo "images"; fi)" "info"
 
