d4c86aa391516c7c9f2ccf35a60f140a3f1a941f
The-going <48602507+The-going@users.noreply.github.com>
2021-12-04 15:19:36 +0300

    Compare architectures before starting compilation.
    Signed-off-by: The-going <48602507+The-going@users.noreply.github.com>

--- lib/compilation.sh
+++ lib/compilation.sh
@@ -411,11 +411,16 @@ compile_kernel()
 
 	display_alert "Compiling $BRANCH kernel" "$version" "info"
 
-	# if _building_ on amd64, find the toolchain, but not if _targeting_ x86 itself. in that case just use system gcc
-	if [[ $(dpkg --print-architecture) == amd64 ]] && [[ "${ARCHITECTURE}" != "x86" ]]; then
+	# compare with the architecture of the current Debian node
+	# if it matches we use the system compiler
+	if $(dpkg-architecture -e $ARCH); then
+		display_alert "Native compilation"
+	elif [[ $(dpkg --print-architecture) == amd64 ]]; then
 		local toolchain
 		toolchain=$(find_toolchain "$KERNEL_COMPILER" "$KERNEL_USE_GCC")
 		[[ -z $toolchain ]] && exit_with_error "Could not find required toolchain" "${KERNEL_COMPILER}gcc $KERNEL_USE_GCC"
+	else
+		exit_with_error "Architecture [$ARCH] is not supported"
 	fi
 
 	display_alert "Compiler version" "${KERNEL_COMPILER}gcc $(eval env PATH="${toolchain}:${PATH}" "${KERNEL_COMPILER}gcc" -dumpversion)" "info"
