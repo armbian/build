e4c3b9916cebc1ca38d37525d06e802e6465a27a
Oleg <balbes-150@yandex.ru>
2022-01-22 21:49:06 +0300

    fix bootsplash and move set PKG_PREFIX to arm64.conf (#3410)

--- lib/general.sh
+++ lib/general.sh
@@ -705,7 +705,7 @@ display_alert "Building kernel splash logo" "$RELEASE" "info"
 	THROBBER_HEIGHT=$(identify $THROBBER | head -1 | cut -d " " -f 3 | cut -d x -f 2)
 	convert -alpha remove -background "#000000"	$LOGO "${SDCARD}"/tmp/logo.rgb
 	convert -alpha remove -background "#000000" $THROBBER "${SDCARD}"/tmp/throbber%02d.rgb
-	${SRC}/packages/blobs/splash/bootsplash-packer \
+	$PKG_PREFIX${SRC}/packages/blobs/splash/bootsplash-packer \
 	--bg_red 0x00 \
 	--bg_green 0x00 \
 	--bg_blue 0x00 \
