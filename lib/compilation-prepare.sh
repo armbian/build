4034a8f53578a6cf5a02e8993d32f21d3c293be4
iav <iav@iav.lv>
2022-02-07 10:52:39 +0200

    switch RTL8188EU USB WiFi driver with 5.15+ kernel to mainline (#3464)
    https://www.phoronix.com/scan.php?page=news_item&px=Linux-5.15-Staging

--- lib/compilation-prepare.sh
+++ lib/compilation-prepare.sh
@@ -467,7 +467,9 @@ compilation_prepare()
 
 	# Wireless drivers for Realtek 8188EU 8188EUS and 8188ETV chipsets
 
-	if linux-version compare "${version}" ge 3.14 && [ "$EXTRAWIFI" == yes ]; then
+	if linux-version compare "${version}" ge 3.14 \
+		&& linux-version compare "${version}" lt 5.15 \
+		&& [ "$EXTRAWIFI" == yes ]; then
 
 		# attach to specifics tag or branch
 		local rtl8188euver="branch:v5.7.6.1"
