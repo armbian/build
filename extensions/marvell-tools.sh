0c692279e1b8728a7f46bd80dc7e27c0eaeaa498
Heisath <jannis@imserv.org>
2022-02-16 15:25:55 +0100

    Add cryptopp to marvell tools

--- extensions/marvell-tools.sh
+++ extensions/marvell-tools.sh
@@ -2,4 +2,5 @@ function fetch_sources_tools__marvell_tools() {
 	fetch_from_repo "https://github.com/MarvellEmbeddedProcessors/A3700-utils-marvell" "marvell-tools" "branch:master"
 	fetch_from_repo "https://github.com/MarvellEmbeddedProcessors/mv-ddr-marvell.git" "marvell-ddr" "branch:master"
 	fetch_from_repo "https://github.com/MarvellEmbeddedProcessors/binaries-marvell" "marvell-binaries" "branch:binaries-marvell-armada-SDK10.0.1.0"
+	fetch_from_repo "https://github.com/weidai11/cryptopp.git" "cryptopp" "branch:master"
 }
