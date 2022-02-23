e399387c4004d3b7e5f7a84d629efa88eab1fbce
Ricardo Pardini <ricardo@pardini.net>
2022-02-24 00:25:30 +0100

    armbian-next: sunxi-tools: fix logging for sunxi-tools compilation

--- extensions/sunxi-tools.sh
+++ extensions/sunxi-tools.sh
@@ -6,12 +6,12 @@ function build_host_tools__compile_sunxi_tools() {
 	# Compile and install only if git commit hash changed
 	cd "${SRC}"/cache/sources/sunxi-tools || exit
 	# need to check if /usr/local/bin/sunxi-fexc to detect new Docker containers with old cached sources
-	if [[ ! -f .commit_id || $(improved_git rev-parse @ 2> /dev/null) != $(< .commit_id) || ! -f /usr/local/bin/sunxi-fexc ]]; then
+	if [[ ! -f .commit_id || $(git rev-parse @ 2> /dev/null) != $(< .commit_id) || ! -f /usr/local/bin/sunxi-fexc ]]; then
 		display_alert "Compiling" "sunxi-tools" "info"
-		make -s clean > /dev/null
-		make -s tools > /dev/null
+		run_host_command_logged make -s clean
+		run_host_command_logged make -s tools
 		mkdir -p /usr/local/bin/
-		make install-tools > /dev/null 2>&1
-		improved_git rev-parse @ 2> /dev/null > .commit_id
+		run_host_command_logged make install-tools
+		git rev-parse @ 2> /dev/null > .commit_id
 	fi
 }
