897674aa74bce0326ed7fe06f5336bf4709a8a1f
Igor Pečovnik <igorpecovnik@users.noreply.github.com>
2022-05-03 08:27:32 +0200

    Bump and freeze kernel at last known working versions (#3736)
    * Freeze kernel at last known working versions
    
    * Fix media kernel
    
    * Update rockchip64 to 5.17.y
    
    * Move patches up
    
    * Re-ad aufs
    
    * Fix rpi kernels

--- lib/compilation-prepare.sh
+++ lib/compilation-prepare.sh
@@ -114,7 +114,6 @@ compilation_prepare()
 	if linux-version compare "${version}" ge 5.10 && [ $SKIP_BOOTSPLASH != yes ]; then
 
 		display_alert "Adding" "Kernel splash file" "info"
-
 		if linux-version compare "${version}" ge 5.11; then
 			process_patch_file "${SRC}/patch/misc/bootsplash-5.16.y-0000-Revert-fbcon-Avoid-cap-set-but-not-used-warning.patch" "applying"
 		fi
@@ -167,7 +166,7 @@ compilation_prepare()
 	#
 	# Older versions have AUFS support with a patch
 
-	if linux-version compare "${version}" ge 5.1 && linux-version compare "${version}" le 5.18 && [ "$AUFS" == yes ]; then
+	if linux-version compare "${version}" ge 5.1 && linux-version compare "${version}" lt 5.15 && [ "$AUFS" == yes ]; then
 
 		# attach to specifics tag or branch
 		local aufstag
