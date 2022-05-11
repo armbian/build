f56f20711e520992594daf68d6e19d7ae3dd635e
Igor Pecovnik <igor.pecovnik@gmail.com>
2022-05-11 09:51:53 +0200

    Replace duplicate query

--- lib/general.sh
+++ lib/general.sh
@@ -663,7 +663,7 @@ fingerprint_image()
 	Title:			${VENDOR} $REVISION ${BOARD^} $BRANCH
 	Kernel:			Linux $VER
 	Build date:		$(date +'%d.%m.%Y')
-	Builder rev:	$(git rev-parse HEAD)
+	Builder rev:		$BUILD_REPOSITORY_COMMIT
 	Maintainer:		$MAINTAINER <$MAINTAINERMAIL>
 	Authors:		https://www.armbian.com/authors
 	Sources: 		https://github.com/armbian/
