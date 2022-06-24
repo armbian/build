97db2fe06892a371cccada35671f068a1492b734
Ricardo Pardini <ricardo@pardini.net>
2022-06-24 18:06:56 +0200

    armbian-next: allow overriding rkbins repo URL with `RKBIN_GIT_URL`

--- extensions/rkbin-tools.sh
+++ extensions/rkbin-tools.sh
@@ -1,5 +1,5 @@
 function fetch_sources_tools__rkbin_tools() {
-	fetch_from_repo "https://github.com/armbian/rkbin" "rkbin-tools" "branch:master"
+	fetch_from_repo "${RKBIN_GIT_URL:-"https://github.com/armbian/rkbin"}" "rkbin-tools" "branch:master"
 }
 
 function build_host_tools__install_rkbin_tools() {
