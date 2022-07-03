71aaf9b3d20af16e77186b7a8e1675ac1d9dc387
Igor Pecovnik <igor.pecovnik@gmail.com>
2022-07-03 23:04:43 +0200

    Working debsum check

--- lib/debootstrap.sh
+++ lib/debootstrap.sh
@@ -359,9 +359,14 @@ create_rootfs_cache()
 		# stage: remove downloaded packages
 		chroot $SDCARD /bin/bash -c "apt-get -y autoremove; apt-get clean"
 
-                # stage: remove downloaded packages
-                chroot $SDCARD /bin/bash -c "dpkg -l | awk '/^ii/ { print $2 }' | xargs debsums | grep -vE 'OK$'"
-echo $?
+		# stage: check md5 sum of installed packages. Just in case.
+		display_alert "Check md5 sum of installed packages" "info"
+		eval "LC_ALL=C LANG=C sudo chroot $SDCARD /bin/bash -e -c 'dpkg-query -f "'\${binary:Package}\\n'" -W | xargs debsums'" \
+			${PROGRESS_LOG_TO_FILE:+' | tee -a $DEST/${LOG_SUBPATH}/debootstrap.log'} \
+			${OUTPUT_DIALOG:+' | dialog --backtitle "$backtitle" --progressbox "Installing Armbian desktop packages..." $TTY_Y $TTY_X'} \
+			${OUTPUT_VERYSILENT:+' >/dev/null 2>/dev/null'} ';EVALPIPE=(${PIPESTATUS[@]})'
+
+		[[ ${EVALPIPE[0]} -ne 0 ]] && exit_with_error "MD5 sums of installed Debian packages failed"
 
 		# DEBUG: print free space
 		local freespace=$(LC_ALL=C df -h)
