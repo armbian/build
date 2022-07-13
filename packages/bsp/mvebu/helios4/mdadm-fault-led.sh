1b025fb383dca911e170b7a8d813560e9fcbeb31
Rosen Penev <rosenp@gmail.com>
2022-07-13 16:38:58 -0700

    mdadm-fault-led: port to dash
    Also ran through shellcheck. dash is faster and uses less RAM than bash.
    
    Signed-off-by: Rosen Penev <rosenp@gmail.com>

--- packages/bsp/mvebu/helios4/mdadm-fault-led.sh
+++ packages/bsp/mvebu/helios4/mdadm-fault-led.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 #
 # Make Red Fault LED (LED2) reports mdadm error events.
 #
@@ -7,33 +7,33 @@ EVENT=$1
 # RED Fault LED trigger
 # trigger none = LED not-blinking if LED on
 # trigger timer = LED blinking if LED on
-TRIGGER=/sys/class/leds/helios4\:red\:fault/trigger
+TRIGGER=/sys/class/leds/helios4:red:fault/trigger
 
 # RED Fault LED brightness
 # britghness 0 = LED off
 # britghness 1 = LED on
-BRIGHTNESS=/sys/class/leds/helios4\:red\:fault/brightness
+BRIGHTNESS=/sys/class/leds/helios4:red:fault/brightness
 
 # Active component device of an array has been marked as faulty OR A newly noticed array appears to be degraded.
-if [[ $EVENT == "Fail" || $EVENT == "DegradedArray" ]]; then
+if [ "$EVENT" = "Fail" -o "$EVENT" = "DegradedArray" ]; then
     echo none > $TRIGGER
     echo 1 > $BRIGHTNESS
 fi
 
 # An md array started reconstruction
-if [ $EVENT == "RebuildStarted" ]; then
+if [ "$EVENT" = "RebuildStarted" ]; then
     echo timer > $TRIGGER
     echo 1 > $BRIGHTNESS
 fi
 
 # An md array that was rebuilding, isn't any more, either because it finished normally or was aborted. 
-if [ $EVENT == "RebuildFinished" ]; then
+if [ "$EVENT" = "RebuildFinished" ]; then
     echo none > $TRIGGER
     echo 0 > $BRIGHTNESS
 fi
 
 # Test RED Fault LED
-if [ $EVENT == "TestMessage" ]; then
+if [ "$EVENT" = "TestMessage" ]; then
     echo timer > $TRIGGER
     echo 1 > $BRIGHTNESS
     sleep 5
