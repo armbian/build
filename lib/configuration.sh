923af84a1e41acdb1ae8c264842d9fd48782183e
Igor Pecovnik <igor.pecovnik@gmail.com>
2022-08-13 09:48:33 +0200

    Add proper fallback since anonymous API calls to GitHub are very limited

--- lib/configuration.sh
+++ lib/configuration.sh
@@ -33,7 +33,9 @@ cd "${SRC}" || exit
 # if variable not provided, check which is current version in the cache storage
 if [[ -z "${ROOTFSCACHE_VERSION}" ]]; then
 	ROOTFSCACHE_VERSION=$(curl https://api.github.com/repos/armbian/cache/releases/latest -s | jq .tag_name -r || true)
-	ROOTFSCACHE_VERSION=${ROOTFSCACHE_VERSION:-"0003"}
+	[[ "$ROOTFSCACHE_VERSION" == null ]] && unset ROOTFSCACHE_VERSION
+	# anonymous API access is very limited which is why we need a fallback
+	ROOTFSCACHE_VERSION=${ROOTFSCACHE_VERSION:-$(curl -L --silent https://cache.armbian.com/rootfs/latest --fail)}
 fi
 
 [[ -z "${CHROOT_CACHE_VERSION}" ]] && CHROOT_CACHE_VERSION=7
