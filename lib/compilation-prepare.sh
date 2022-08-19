da598a449f44013e22da25d1869fba7756752e24
Vyacheslav Bocharov <adeep@lexina.in>
2022-08-19 16:43:23 +0300

    Update to re-enable RTL88x2CS driver for 5.19.2+

--- lib/compilation-prepare.sh
+++ lib/compilation-prepare.sh
@@ -579,7 +579,7 @@ compilation_prepare()
 
 	# Wireless drivers for Realtek 88x2cs chipsets
 
-	if linux-version compare "${version}" ge 5.9 && linux-version compare "${version}" lt 5.19.2 && [ "$EXTRAWIFI" == yes ]; then
+	if linux-version compare "${version}" ge 5.9 && [ "$EXTRAWIFI" == yes ]; then
 
 		# attach to specifics tag or branch
 		local rtl88x2csver="branch:tune_for_jethub"
