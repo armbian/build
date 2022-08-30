2c6f0c70e6857e2ef18d9dbdc0c4f25df658ecd5
Igor Pečovnik <igorpecovnik@users.noreply.github.com>
2022-08-30 07:52:06 +0200

    Re-enable Kernel bootsplash for 5.15.y or higher (#4132)
    Tested on Meson64 and Rockchip64
    
    Wasn't that hard after all :)

--- lib/compilation-prepare.sh
+++ lib/compilation-prepare.sh
@@ -122,25 +122,24 @@ compilation_prepare()
 
 	# disable it.
 	# todo: cleanup logo generation code and bring in plymouth
-	SKIP_BOOTSPLASH=yes
 
-	if linux-version compare "${version}" ge 5.10 && linux-version compare "${version}" lt 5.19 && [ $SKIP_BOOTSPLASH != yes ]; then
+	if linux-version compare "${version}" ge 5.15 && [ $SKIP_BOOTSPLASH != yes ]; then
 
 		display_alert "Adding" "Kernel splash file" "info"
-		if linux-version compare "${version}" ge 5.11; then
+#		if linux-version compare "${version}" ge 5.11; then
 			process_patch_file "${SRC}/patch/misc/bootsplash-5.16.y-0000-Revert-fbcon-Avoid-cap-set-but-not-used-warning.patch" "applying"
-		fi
+#		fi
 
-		if ( linux-version compare "${version}" ge 5.18.18 && linux-version compare "${version}" lt 5.19 ) \
-			|| ( linux-version compare "${version}" ge 5.15.61 && linux-version compare "${version}" lt 5.16 ) ; then
+#		if ( linux-version compare "${version}" ge 5.18.18 && linux-version compare "${version}" lt 5.19 ) \
+#			|| ( linux-version compare "${version}" ge 5.15.61 && linux-version compare "${version}" lt 5.16 ) ; then
 			process_patch_file "${SRC}/patch/misc/0001-Revert-fbcon-Fix-accelerated-fbdev-scrolling-while-logo-is-still-shown.patch" "applying"
-		fi
+#		fi
 
 		process_patch_file "${SRC}/patch/misc/bootsplash-5.16.y-0001-Revert-fbcon-Add-option-to-enable-legacy-hardware-ac.patch" "applying"
 
-		if linux-version compare "${version}" ge 5.15; then
+#		if linux-version compare "${version}" ge 5.15; then
 			process_patch_file "${SRC}/patch/misc/bootsplash-5.16.y-0002-Revert-vgacon-drop-unused-vga_init_done.patch" "applying"
-		fi
+#		fi
 
 		process_patch_file "${SRC}/patch/misc/bootsplash-5.16.y-0003-Revert-vgacon-remove-software-scrollback-support.patch" "applying"
 		process_patch_file "${SRC}/patch/misc/bootsplash-5.16.y-0004-Revert-drivers-video-fbcon-fix-NULL-dereference-in-f.patch" "applying"
