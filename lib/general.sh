f90fac256177a73c4ccec4254579ed1ece0af63c
hzy <h1260071766@126.com>
2022-09-13 08:39:45 +0800

    Bugfix: verify never fail

--- lib/general.sh
+++ lib/general.sh
@@ -1772,6 +1772,7 @@ download_and_verify()
 		echo ""
 	fi
 
+	local verified=false
 	if [[ -f ${localdir}/${filename}.asc ]]; then
 
 		if grep -q 'BEGIN PGP SIGNATURE' "${localdir}/${filename}.asc"; then
