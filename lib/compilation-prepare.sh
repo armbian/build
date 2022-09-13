eca79f2a01c2d17025c266dd0a4685e44edcc4fc
Igor Pečovnik <igorpecovnik@users.noreply.github.com>
2022-09-13 15:14:06 +0200

    Switch to existing upstream brasnch / commit (#4175)

--- lib/compilation-prepare.sh
+++ lib/compilation-prepare.sh
@@ -555,7 +555,7 @@ compilation_prepare()
 	if linux-version compare "${version}" ge 5.0 && [ "$EXTRAWIFI" == yes ]; then
 
 		# attach to specifics tag or branch
-		local rtl88x2buver="branch:fix-6.0"
+		local rtl88x2buver="commit:00f51d93fe8309b0e23782ad621a038c98c7f031"
 
 		display_alert "Adding" "Wireless drivers for Realtek 88x2bu chipsets ${rtl88x2buver}" "info"
 
