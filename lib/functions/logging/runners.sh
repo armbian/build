e6f2f3b623f115f8a6843ee6d9823d17872eda15
Ricardo Pardini <ricardo@pardini.net>
2022-10-16 11:47:47 +0200

    armbian-next: retry 3 times for loop device's size, then give up; introduce `RETRY_RUNS` in `do_with_retries()`

--- lib/functions/logging/runners.sh
+++ lib/functions/logging/runners.sh
@@ -251,12 +251,14 @@ function do_with_retries() {
 	local counter=0
 	while [[ $counter -lt $retries ]]; do
 		counter=$((counter + 1))
+		declare -i RETRY_RUNS=${counter}
 		"$@" && return 0 # execute and return 0 if success; if not, let it loop;
 		if [[ "${silent_retry}" == "yes" ]]; then
 			: # do nothing
 		else
 			display_alert "Command failed, retrying in ${sleep_seconds}s" "$*" "warn"
 		fi
+		unset RETRY_RUNS
 		sleep ${sleep_seconds}
 	done
 	display_alert "Command failed ${counter} times, giving up" "$*" "warn"
