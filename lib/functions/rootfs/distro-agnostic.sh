df7a603553550987e6ac104d0e4da236d41749fd
Igor Pečovnik <igorpecovnik@users.noreply.github.com>
2022-10-24 09:09:12 +0200

    Resolv check was done for host while it should be done for chroot (#4325)

--- lib/functions/rootfs/distro-agnostic.sh
+++ lib/functions/rootfs/distro-agnostic.sh
@@ -509,7 +509,7 @@ FAMILY_TWEAKS
 	cp "${SDCARD}"/etc/armbian-release "${SDCARD}"/etc/armbian-image-release
 
 	# DNS fix. package resolvconf is not available everywhere
-	if [ -d /etc/resolvconf/resolv.conf.d ] && [ -n "$NAMESERVER" ]; then
+	if [ -d "${SDCARD}"/etc/resolvconf/resolv.conf.d ] && [ -n "$NAMESERVER" ]; then
 		echo "nameserver $NAMESERVER" > "${SDCARD}"/etc/resolvconf/resolv.conf.d/head
 	fi
 
