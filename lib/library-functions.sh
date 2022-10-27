dc59ec4b6aa9cdc188c8283d915708ad404e3518
Ricardo Pardini <ricardo@pardini.net>
2022-10-28 00:13:12 +0200

    armbian-next: `cli`: `distccd` launcher for build farm
    - relaunched under docker
    - possibly much nicer if zeroconf enabled?
    - this thing is ignoring signals. can't ctrl-c out of it. why? @TODO
    - pt 1, subpt 3: distccd - distcc-pump - avahi
    - pt 1, subpt 3: distccd + distcc-pump
    - pt 1, subpt 2: distccd + avahi
    - pt 1, subpt 1: distccd

--- lib/library-functions.sh
+++ lib/library-functions.sh
@@ -51,6 +51,15 @@ source "${SRC}"/lib/functions/cli/cli-configdump.sh
 #set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable - one day will be enabled
 set -o errtrace # trace ERR through - enabled
 set -o errexit  ## set -e : exit the script if any statement returns a non-true return value - enabled
+### lib/functions/cli/cli-distccd.sh
+# shellcheck source=lib/functions/cli/cli-distccd.sh
+source "${SRC}"/lib/functions/cli/cli-distccd.sh
+
+# no errors tolerated. invoked before each sourced file to make sure.
+#set -o pipefail  # trace ERR through pipes - will be enabled "soon"
+#set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable - one day will be enabled
+set -o errtrace # trace ERR through - enabled
+set -o errexit  ## set -e : exit the script if any statement returns a non-true return value - enabled
 ### lib/functions/cli/cli-docker.sh
 # shellcheck source=lib/functions/cli/cli-docker.sh
 source "${SRC}"/lib/functions/cli/cli-docker.sh
