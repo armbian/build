53c25569dba6c02b69627a90f8358f16cf4d7f7e
Ricardo Pardini <ricardo@pardini.net>
2022-10-28 22:05:37 +0200

    armbian-next: `distcc`: optionally using `distcc` for kernel build

--- lib/functions/compilation/kernel.sh
+++ lib/functions/compilation/kernel.sh
@@ -4,23 +4,34 @@ function run_kernel_make_internal() {
 	set -e
 	declare -a common_make_params_quoted common_make_envs full_command
 
+	# Prepare distcc, if enabled.
+	declare -a -g DISTCC_EXTRA_ENVS=()
+	declare -a -g DISTCC_CROSS_COMPILE_PREFIX=()
+	declare -a -g DISTCC_MAKE_J_PARALLEL=()
+	prepare_distcc_compilation_config
+
+
 	common_make_envs=(
 		"CCACHE_BASEDIR=\"$(pwd)\""     # Base directory for ccache, for cache reuse # @TODO: experiment with this and the source path to maximize hit rate
 		"PATH=\"${toolchain}:${PATH}\"" # Insert the toolchain first into the PATH.
-		"DPKG_COLORS=always"            # Use colors for dpkg
+		"DPKG_COLORS=always"            # Use colors for dpkg @TODO no dpkg is done anymore, remove?
 		"XZ_OPT='--threads=0'"          # Use parallel XZ compression
 		"TERM='${TERM}'"                # Pass the terminal type, so that 'make menuconfig' can work.
 	)
 
+	# Add the distcc envs, if any.
+	common_make_envs+=("${DISTCC_EXTRA_ENVS[@]}")
+
 	common_make_params_quoted=(
 		# @TODO: introduce O=path/to/binaries, so sources and bins are not in the same dir.
-
-		"$CTHREADS"                    # Parallel compile, "-j X" for X cpus
+		
+		"${DISTCC_MAKE_J_PARALLEL[@]}" # Parallel compile, "-j X" for X cpus; determined by distcc, or is just "$CTHREADS" if distcc is not enabled.
+		
 		"ARCH=${ARCHITECTURE}"         # Key param. Everything depends on this.
 		"LOCALVERSION=-${LINUXFAMILY}" # Change the internal kernel version to include the family. Changing this causes recompiles # @TODO change to "localversion" file
 
-		"CROSS_COMPILE=${CCACHE} ${KERNEL_COMPILER}"                           # added as prefix to every compiler invocation by make
-		"KCFLAGS=-fdiagnostics-color=always -Wno-error=misleading-indentation" # Force GCC colored messages, downgrade misleading indentation to warning
+		"CROSS_COMPILE=${CCACHE} ${DISTCC_CROSS_COMPILE_PREFIX[@]} ${KERNEL_COMPILER}" # added as prefix to every compiler invocation by make
+		"KCFLAGS=-fdiagnostics-color=always -Wno-error=misleading-indentation"         # Force GCC colored messages, downgrade misleading indentation to warning
 
 		"SOURCE_DATE_EPOCH=${kernel_base_revision_ts}"        # https://reproducible-builds.org/docs/source-date-epoch/ and https://www.kernel.org/doc/html/latest/kbuild/reproducible-builds.html
 		"KBUILD_BUILD_TIMESTAMP=${kernel_base_revision_date}" # https://www.kernel.org/doc/html/latest/kbuild/kbuild.html#kbuild-build-timestamp
