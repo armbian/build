ff6c515e3f48304a346bbd2b28457339839cb8b7
Igor Pečovnik <igorpecovnik@users.noreply.github.com>
2022-12-30 20:59:35 +0100

    Add next Ubuntu 23.04 / Lunar lobster (#4625)

--- lib/functions/image/partitioning.sh
+++ lib/functions/image/partitioning.sh
@@ -28,7 +28,7 @@ prepare_partitions() {
 	# parttype[nfs] is empty
 
 	# metadata_csum and 64bit may need to be disabled explicitly when migrating to newer supported host OS releases
-	if [[ $HOSTRELEASE =~ buster|bullseye|focal|jammy|kinetic|sid ]]; then
+	if [[ $HOSTRELEASE =~ buster|bullseye|focal|jammy|kinetic|lunar|sid ]]; then
 		mkopts[ext4]="-q -m 2 -O ^64bit,^metadata_csum"
 	fi
 	# mkopts[fat] is empty
