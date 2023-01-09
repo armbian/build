90e067cd0e2a57cebf9d15731d7e167c920b3303
Ricardo Pardini <ricardo@pardini.net>
2023-01-10 00:17:40 +0100

    armbian-oleg: split hostdeps again, full of ifs, and reduce the minimum set of pkgs for Oleg-conditions while trying to keep Docker full
    - probably breaks a lot of stuff; c++ compilers, distcc, possibly others
    - split into late_prepare_host_dependencies / early_prepare_host_dependencies calling adaptative_prepare_host_dependencies() full of if's
    
    also:
    - calc deps out of configuration
    - reorg checks into obtain_and_check_host_release_and_arch()

--- lib/functions/host/host-release.sh
+++ lib/functions/host/host-release.sh
@@ -1,9 +1,26 @@
-function obtain_and_check_hostrelease() {
+function obtain_and_check_host_release_and_arch() {
 	# obtain the host release either from os-release or debian_version
 	declare -g HOSTRELEASE
 	HOSTRELEASE="$(cat /etc/os-release | grep VERSION_CODENAME | cut -d"=" -f2)"
 	[[ -z $HOSTRELEASE ]] && HOSTRELEASE="$(cut -d'/' -f1 /etc/debian_version)"
 	display_alert "Build host OS release" "${HOSTRELEASE:-(unknown)}" "info"
+
+	# obtain the host arch, from dpkg
+	declare -g HOSTARCH
+	HOSTARCH="$(dpkg --print-architecture)"
+	display_alert "Build host architecture" "${HOSTARCH:-(unknown)}" "info"
+
+	case "${HOSTARCH}" in
+		amd64 | arm64) ;; # officially supported
+		armhf | riscv64)  # experimental
+			display_alert "EXPERIMENTAL build host support" "${HOSTARCH}" "wrn"
+			;;
+		*)
+			display_alert "Please read documentation to set up proper compilation environment"
+			display_alert "https://www.armbian.com/using-armbian-tools/"
+			exit_with_error "Running this tool on '${HOSTARCH}' build host is not supported"
+			;;
+	esac
 
 	# Ubuntu Jammy x86_64 or arm64 is the only fully supported host OS release
 	# Using Docker/VirtualBox is the only supported way to run the build script on other Linux distributions
