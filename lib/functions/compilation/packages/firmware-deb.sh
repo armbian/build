d979c73c1dcc200246b499e843a898c27d650a2e
Ricardo Pardini <ricardo@pardini.net>
2023-01-12 23:21:12 +0100

    armbian-next: split `compilation/debs.sh`; `compile_xilinx_bootgen()` moved to family

---
+++ lib/functions/compilation/packages/firmware-deb.sh
@@ -0,0 +1,53 @@
+compile_firmware() {
+	display_alert "Merging and packaging linux firmware" "@host" "info"
+
+	local firmwaretempdir plugin_dir
+
+	firmwaretempdir=$(mktemp -d) # subject to TMPDIR/WORKDIR, so is protected by single/common error trapmanager to clean-up.
+	chmod 700 "${firmwaretempdir}"
+
+	plugin_dir="armbian-firmware${FULL}"
+	mkdir -p "${firmwaretempdir}/${plugin_dir}/lib/firmware"
+	
+	local ARMBIAN_FIRMWARE_GIT_SOURCE="${ARMBIAN_FIRMWARE_GIT_SOURCE:-"https://github.com/armbian/firmware"}"
+	local ARMBIAN_FIRMWARE_GIT_BRANCH="${ARMBIAN_FIRMWARE_GIT_BRANCH:-"master"}"
+
+	fetch_from_repo "${ARMBIAN_FIRMWARE_GIT_SOURCE}" "armbian-firmware-git" "branch:${ARMBIAN_FIRMWARE_GIT_BRANCH}"
+
+	if [[ -n $FULL ]]; then
+		fetch_from_repo "$MAINLINE_FIRMWARE_SOURCE" "linux-firmware-git" "branch:main"
+		# cp : create hardlinks
+		run_host_command_logged cp -af --reflink=auto "${SRC}"/cache/sources/linux-firmware-git/* "${firmwaretempdir}/${plugin_dir}/lib/firmware/"
+		# cp : create hardlinks for ath11k WCN685x hw2.1 firmware since they are using the same firmware with hw2.0
+		run_host_command_logged cp -af --reflink=auto "${firmwaretempdir}/${plugin_dir}/lib/firmware/ath11k/WCN6855/hw2.0/" "${firmwaretempdir}/${plugin_dir}/lib/firmware/ath11k/WCN6855/hw2.1/"
+	fi
+	# overlay our firmware
+	# cp : create hardlinks
+	cp -af --reflink=auto "${SRC}"/cache/sources/armbian-firmware-git/* "${firmwaretempdir}/${plugin_dir}/lib/firmware/"
+
+	rm -rf "${firmwaretempdir}/${plugin_dir}"/lib/firmware/.git
+	cd "${firmwaretempdir}/${plugin_dir}" || exit
+
+	# set up control file
+	mkdir -p DEBIAN
+	cat <<- END > DEBIAN/control
+		Package: armbian-firmware${FULL}
+		Version: $REVISION
+		Architecture: all
+		Maintainer: $MAINTAINER <$MAINTAINERMAIL>
+		Installed-Size: 1
+		Replaces: linux-firmware, firmware-brcm80211, firmware-ralink, firmware-samsung, firmware-realtek, armbian-firmware${REPLACE}
+		Section: kernel
+		Priority: optional
+		Description: Linux firmware${FULL}
+	END
+
+	cd "${firmwaretempdir}" || exit
+	# pack
+	mv "armbian-firmware${FULL}" "armbian-firmware${FULL}_${REVISION}_all"
+	display_alert "Building firmware package" "armbian-firmware${FULL}_${REVISION}_all" "info"
+	fakeroot_dpkg_deb_build "armbian-firmware${FULL}_${REVISION}_all"
+	mv "armbian-firmware${FULL}_${REVISION}_all" "armbian-firmware${FULL}"
+	run_host_command_logged rsync -rq "armbian-firmware${FULL}_${REVISION}_all.deb" "${DEB_STORAGE}/"
+
+}
---
+++ lib/functions/compilation/packages/zsh-deb.sh
@@ -0,0 +1,68 @@
+compile_armbian-zsh() {
+	local tmp_dir armbian_zsh_dir
+	tmp_dir=$(mktemp -d) # subject to TMPDIR/WORKDIR, so is protected by single/common error trapmanager to clean-up.
+	chmod 700 "${tmp_dir}"
+
+	armbian_zsh_dir=armbian-zsh_${REVISION}_all
+	display_alert "Building deb" "armbian-zsh" "info"
+
+	fetch_from_repo "$GITHUB_SOURCE/ohmyzsh/ohmyzsh" "oh-my-zsh" "branch:master"
+	fetch_from_repo "$GITHUB_SOURCE/mroth/evalcache" "evalcache" "branch:master"
+
+	mkdir -p "${tmp_dir}/${armbian_zsh_dir}"/{DEBIAN,etc/skel/,etc/oh-my-zsh/,/etc/skel/.oh-my-zsh/cache}
+
+	# set up control file
+	cat <<- END > "${tmp_dir}/${armbian_zsh_dir}"/DEBIAN/control
+		Package: armbian-zsh
+		Version: $REVISION
+		Architecture: all
+		Maintainer: $MAINTAINER <$MAINTAINERMAIL>
+		Depends: zsh, tmux
+		Section: utils
+		Priority: optional
+		Description: Armbian improved ZShell
+	END
+
+	# set up post install script
+	cat <<- END > "${tmp_dir}/${armbian_zsh_dir}"/DEBIAN/postinst
+		#!/bin/sh
+
+		# copy cache directory if not there yet
+		awk -F'[:]' '{if (\$3 >= 1000 && \$3 != 65534 || \$3 == 0) print ""\$6"/.oh-my-zsh"}' /etc/passwd | xargs -i sh -c 'test ! -d {} && cp -R --attributes-only /etc/skel/.oh-my-zsh {}'
+		awk -F'[:]' '{if (\$3 >= 1000 && \$3 != 65534 || \$3 == 0) print ""\$6"/.zshrc"}' /etc/passwd | xargs -i sh -c 'test ! -f {} && cp -R /etc/skel/.zshrc {}'
+
+		# fix owner permissions in home directory
+		awk -F'[:]' '{if (\$3 >= 1000 && \$3 != 65534 || \$3 == 0) print ""\$1":"\$3" "\$6"/.oh-my-zsh"}' /etc/passwd | xargs -n2 chown -R
+		awk -F'[:]' '{if (\$3 >= 1000 && \$3 != 65534 || \$3 == 0) print ""\$1":"\$3" "\$6"/.zshrc"}' /etc/passwd | xargs -n2 chown -R
+
+		# add support for bash profile
+		! grep emulate /etc/zsh/zprofile  >/dev/null && echo "emulate sh -c 'source /etc/profile'" >> /etc/zsh/zprofile
+		exit 0
+	END
+
+	cp -R "${SRC}"/cache/sources/oh-my-zsh "${tmp_dir}/${armbian_zsh_dir}"/etc/
+	cp -R "${SRC}"/cache/sources/evalcache "${tmp_dir}/${armbian_zsh_dir}"/etc/oh-my-zsh/plugins
+	cp "${tmp_dir}/${armbian_zsh_dir}"/etc/oh-my-zsh/templates/zshrc.zsh-template "${tmp_dir}/${armbian_zsh_dir}"/etc/skel/.zshrc
+
+	chmod -R g-w,o-w "${tmp_dir}/${armbian_zsh_dir}"/etc/oh-my-zsh/
+
+	# we have common settings
+	sed -i "s/^export ZSH=.*/export ZSH=\/etc\/oh-my-zsh/" "${tmp_dir}/${armbian_zsh_dir}"/etc/skel/.zshrc
+
+	# user cache
+	sed -i "/^export ZSH=.*/a export ZSH_CACHE_DIR=~\/.oh-my-zsh\/cache" "${tmp_dir}/${armbian_zsh_dir}"/etc/skel/.zshrc
+
+	# define theme
+	sed -i 's/^ZSH_THEME=.*/ZSH_THEME="mrtazz"/' "${tmp_dir}/${armbian_zsh_dir}"/etc/skel/.zshrc
+
+	# disable auto update since we provide update via package
+	sed -i "s/^# zstyle ':omz:update' mode disabled.*/zstyle ':omz:update' mode disabled/g" "${tmp_dir}/${armbian_zsh_dir}"/etc/skel/.zshrc
+
+	# define default plugins
+	sed -i 's/^plugins=.*/plugins=(evalcache git git-extras debian tmux screen history extract colorize web-search docker)/' "${tmp_dir}/${armbian_zsh_dir}"/etc/skel/.zshrc
+
+	chmod 755 "${tmp_dir}/${armbian_zsh_dir}"/DEBIAN/postinst
+
+	fakeroot_dpkg_deb_build "${tmp_dir}/${armbian_zsh_dir}"
+	run_host_command_logged rsync --remove-source-files -r "${tmp_dir}/${armbian_zsh_dir}.deb" "${DEB_STORAGE}/"
+}
