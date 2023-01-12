213b83592f5756a513d82a0e52c4b4f9442f5b02
Ricardo Pardini <ricardo@pardini.net>
2023-01-12 23:54:14 +0100

    armbian-oleg: curb logging from building `armbian-firmware`

--- lib/functions/compilation/packages/plymouth-deb.sh
+++ lib/functions/compilation/packages/plymouth-deb.sh
@@ -1,13 +1,12 @@
-# @TODO: code from master via Igor; not yet armbian-next'fied! warning!!
 compile_plymouth_theme_armbian() {
 
-	local tmp_dir work_dir
+	local tmp_dir plymouth_theme_armbian_dir
 	tmp_dir=$(mktemp -d)
 	chmod 700 "${tmp_dir}"
 	plymouth_theme_armbian_dir=armbian-plymouth-theme_${REVISION}_all
 	display_alert "Building deb" "armbian-plymouth-theme" "info"
 
-	mkdir -p "${tmp_dir}/${plymouth_theme_armbian_dir}"/{DEBIAN,usr/share/plymouth/themes/armbian}
+	run_host_command_logged mkdir -p "${tmp_dir}/${plymouth_theme_armbian_dir}"/{DEBIAN,usr/share/plymouth/themes/armbian}
 
 	# set up control file
 	cat <<- END > "${tmp_dir}/${plymouth_theme_armbian_dir}"/DEBIAN/control
@@ -18,37 +17,34 @@ compile_plymouth_theme_armbian() {
 		Depends: plymouth, plymouth-themes
 		Section: universe/x11
 		Priority: optional
-		Description: boot animation, logger and I/O multiplexer - armbian theme
+		Description: boot animation, logger and I/O multiplexer - Armbian theme
 	END
 
-	cp "${SRC}"/packages/plymouth-theme-armbian/debian/{postinst,prerm,postrm} \
+	run_host_command_logged cp -v "${SRC}"/packages/plymouth-theme-armbian/debian/{postinst,prerm,postrm} \
 		"${tmp_dir}/${plymouth_theme_armbian_dir}"/DEBIAN/
 	chmod 755 "${tmp_dir}/${plymouth_theme_armbian_dir}"/DEBIAN/{postinst,prerm,postrm}
 
 	# this requires `imagemagick`
 
-	convert -resize 256x256 \
+	run_host_command_logged convert -resize 256x256 \
 		"${SRC}"/packages/plymouth-theme-armbian/armbian-logo.png \
 		"${tmp_dir}/${plymouth_theme_armbian_dir}"/usr/share/plymouth/themes/armbian/bgrt-fallback.png
 
-	# convert -resize 52x52 \
-	# 	"${SRC}"/packages/plymouth-theme-armbian/spinner.gif \
-	# 	"${tmp_dir}/${plymouth_theme_armbian_dir}"/usr/share/plymouth/themes/armbian/animation-%04d.png
-
-	convert -resize 52x52 \
+	run_host_command_logged convert -resize 52x52 \
 		"${SRC}"/packages/plymouth-theme-armbian/spinner.gif \
 		"${tmp_dir}/${plymouth_theme_armbian_dir}"/usr/share/plymouth/themes/armbian/throbber-%04d.png
 
-	cp "${SRC}"/packages/plymouth-theme-armbian/watermark.png \
+	run_host_command_logged cp -v "${SRC}"/packages/plymouth-theme-armbian/watermark.png \
 		"${tmp_dir}/${plymouth_theme_armbian_dir}"/usr/share/plymouth/themes/armbian/
 
-	cp "${SRC}"/packages/plymouth-theme-armbian/{bullet,capslock,entry,keyboard,keymap-render,lock}.png \
+	run_host_command_logged cp -v "${SRC}"/packages/plymouth-theme-armbian/{bullet,capslock,entry,keyboard,keymap-render,lock}.png \
 		"${tmp_dir}/${plymouth_theme_armbian_dir}"/usr/share/plymouth/themes/armbian/
 
-	cp "${SRC}"/packages/plymouth-theme-armbian/armbian.plymouth \
+	run_host_command_logged cp -v "${SRC}"/packages/plymouth-theme-armbian/armbian.plymouth \
 		"${tmp_dir}/${plymouth_theme_armbian_dir}"/usr/share/plymouth/themes/armbian/
 
-	fakeroot dpkg-deb -b "-Z${DEB_COMPRESS}" "${tmp_dir}/${plymouth_theme_armbian_dir}" > /dev/null
-	rsync --remove-source-files -rq "${tmp_dir}/${plymouth_theme_armbian_dir}.deb" "${DEB_STORAGE}/"
-	rm -rf "${tmp_dir}"
+	fakeroot_dpkg_deb_build "${tmp_dir}/${plymouth_theme_armbian_dir}"
+	run_host_command_logged rsync --remove-source-files -rq "${tmp_dir}/${plymouth_theme_armbian_dir}.deb" "${DEB_STORAGE}/"
+
+	# does not remove the tmpdir; it's under TMPDIR anyway
 }
