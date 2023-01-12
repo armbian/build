e74474f809cee8779dc4ca4cd1123cb0c9f7378a
Ricardo Pardini <ricardo@pardini.net>
2023-01-12 19:00:06 +0100

    armbian-oleg: logging: new logfile format; `SHOW_LOG=yes` default; introduce `DEBUG=yes`
    - default `SHOW_LOG=yes` if user on terminal
    - shortcut: `DEBUG=yes` defaults both `SHOW_LOG=yes` & `SHOW_DEBUG=yes`
    - much simpler logging format for log file
    - skip all display_alert()'s more verbose than DEBUG from logfile
      - set `DEBUG=yes` to log everything again
    - skip Markdown assets from being included in ANSI log
    - use of `ccze`
    - introduce internal var for controlling COMMAND-level log
    - remove unused `run_on_sdcard()` that somehow was left there
    - drop `export_html_logs()` and `EXPORT_HTML_LOG` -- are no more

--- lib/functions/logging/export-logs.sh
+++ lib/functions/logging/export-logs.sh
@@ -59,15 +59,14 @@ function export_ansi_logs() {
 	if [[ -n "$(command -v git)" && -d "${SRC}/.git" ]]; then
 		display_alert "Gathering git info for logs" "Processing git information, please wait..." "debug"
 		cat <<- GIT_ANSI_HEADER >> "${target_file}"
-			----------------------------------------------------------------------------------------------------------------
 			# Last revision:
-			$(LC_ALL=C LANG=C git --git-dir="${SRC}/.git" log -1 --color --format=short --decorate)
+			$(LC_ALL=C LANG=C git --git-dir="${SRC}/.git" log -1 --color --format=short --decorate || true)
 			----------------------------------------------------------------------------------------------------------------
 			# Git status:
-			$(LC_ALL=C LANG=C git -c color.status=always --work-tree="${SRC}" --git-dir="${SRC}/.git" status)
+			$(LC_ALL=C LANG=C git -c color.status=always --work-tree="${SRC}" --git-dir="${SRC}/.git" status || true)
 			----------------------------------------------------------------------------------------------------------------
 			# Git changes:
-			$(LC_ALL=C LANG=C git --work-tree="${SRC}" --git-dir="${SRC}/.git" diff -u --color)
+			$(LC_ALL=C LANG=C git --work-tree="${SRC}" --git-dir="${SRC}/.git" diff -u --color || true)
 			----------------------------------------------------------------------------------------------------------------
 		GIT_ANSI_HEADER
 	fi
@@ -80,71 +79,14 @@ function export_ansi_logs() {
 
 	for logfile_full in "${logfiles_array[@]}"; do
 		local logfile_base="$(basename "${logfile_full}")"
-		cat <<- ANSI_ONE_LOGFILE_NO_CCZE >> "${target_file}"
-			------------------------------------------------------------------------------------------------------------
+		# skip if logfile_base ends in ".md"; those are handled in the markdown version of logs
+		[[ "${logfile_base}" =~ \.md$ ]] && continue
+		cat <<- ANSI_ONE_LOGFILE >> "${target_file}"
 			## ${logfile_base}
 			$(cat "${logfile_full}")
 			------------------------------------------------------------------------------------------------------------
-		ANSI_ONE_LOGFILE_NO_CCZE
+		ANSI_ONE_LOGFILE
 	done
 
-	display_alert "Built ANSI log file" "${target_file}"
-}
-
-# Export logs in HTML format. (EXPORT_HTML_LOG=yes) -- very slow.
-function export_html_logs() {
-	display_alert "Preparing HTML log from" "${LOGDIR}" "debug"
-
-	cat <<- ANSI_HEADER > "${target_file}"
-		<html>
-			<head>
-			<title>Armbian logs for ${ARMBIAN_BUILD_UUID}</title>
-			<style>
-				html, html pre { background-color: black !important; color: white !important; font-family: JetBrains Mono, monospace, cursive !important; }
-				hr { border: 0; border-bottom: 1px dashed silver; }
-			</style>
-			</head>
-		<body>
-			<h2>Armbian build at $(LC_ALL=C LANG=C date) on $(hostname || true)</h2>
-			<h2>${ARMBIAN_ORIGINAL_ARGV[@]@Q}</h2>
-			<hr/>
-
-			$(LC_ALL=C LANG=C git --git-dir="${SRC}/.git" log -1 --color --format=short --decorate | ansi2html --no-wrap --no-header)
-			<hr/>
-
-			$(LC_ALL=C LANG=C git -c color.status=always --work-tree="${SRC}" --git-dir="${SRC}/.git" status | ansi2html --no-wrap --no-header)
-			<hr/>
-
-			$(LC_ALL=C LANG=C git --work-tree="${SRC}" --git-dir="${SRC}/.git" diff -u --color | ansi2html --no-wrap --no-header)
-			<hr/>
-
-	ANSI_HEADER
-
-	# Find and sort the files there, store in array one per logfile
-	declare -a logfiles_array
-	mapfile -t logfiles_array < <(find "${LOGDIR}" -type f | LC_ALL=C sort -h)
-
-	for logfile_full in "${logfiles_array[@]}"; do
-		local logfile_base="$(basename "${logfile_full}")"
-		if [[ -f /usr/bin/ccze ]] && [[ -f /usr/bin/ansi2html ]]; then
-			cat <<- HTML_ONE_LOGFILE_WITH_CCZE >> "${target_file}"
-				<h3>${logfile_base}</h3>
-				<div style="padding: 1em">
-				$(ccze -o nolookups --raw-ansi < "${logfile_full}" | ansi2html --no-wrap --no-header)
-				</div>
-				<hr/>
-			HTML_ONE_LOGFILE_WITH_CCZE
-		else
-			cat <<- ANSI_ONE_LOGFILE_NO_CCZE >> "${target_file}"
-				<h3>${logfile_base}</h3>
-				<pre>$(cat "${logfile_full}")</pre>
-			ANSI_ONE_LOGFILE_NO_CCZE
-		fi
-	done
-
-	cat <<- HTML_FOOTER >> "${target_file}"
-		</body></html>
-	HTML_FOOTER
-
-	display_alert "Built HTML log file" "${target_file}"
+	display_alert "ANSI log file built; inspect it by running:" "less -R ${target_file}"
 }
