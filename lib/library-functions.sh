2369015b1ef2ba2cc1a1ad3bce80444d119d1a82
Ricardo Pardini <ricardo@pardini.net>
2023-01-13 04:17:00 +0100

    armbian-next: kernel: deterministic `.config` mtime handling; kernel build logging sections reorg
    - .config is now compared with previous one byte-wise, diff shown, and mtime preserved directly via `cp -p`
    - this does away with the previous mtime-based BS I had written during the fasthash era (and thus remove file-mtime.sh, regen lib)
    - split some functions, to better control the interactive piece in the _middle_ of kernel config process
    - no more `kernel_config_maybe_interactive()`, now `kernel_config()` that controls its own logging sections
    - extract `kernel_determine_toolchain()` out of config into make
    - introduce internal hook `armbian_kernel_config()` that runs before custom version (for .config defaults, coming soon)

--- lib/library-functions.sh
+++ lib/library-functions.sh
@@ -447,15 +447,6 @@ source "${SRC}"/lib/functions/general/downloads.sh
 #set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable - one day will be enabled
 set -o errtrace # trace ERR through - enabled
 set -o errexit  ## set -e : exit the script if any statement returns a non-true return value - enabled
-### lib/functions/general/file-mtime.sh
-# shellcheck source=lib/functions/general/file-mtime.sh
-source "${SRC}"/lib/functions/general/file-mtime.sh
-
-# no errors tolerated. invoked before each sourced file to make sure.
-#set -o pipefail  # trace ERR through pipes - will be enabled "soon"
-#set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable - one day will be enabled
-set -o errtrace # trace ERR through - enabled
-set -o errexit  ## set -e : exit the script if any statement returns a non-true return value - enabled
 ### lib/functions/general/git.sh
 # shellcheck source=lib/functions/general/git.sh
 source "${SRC}"/lib/functions/general/git.sh
