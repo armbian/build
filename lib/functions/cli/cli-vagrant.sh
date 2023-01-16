aaf391bf19b9f1400881e99bce807da71db37df1
Ricardo Pardini <ricardo@pardini.net>
2023-01-16 14:17:52 +0100

    armbian-next: mark Vagrant CLI as unimplemented

--- lib/functions/cli/cli-vagrant.sh
+++ lib/functions/cli/cli-vagrant.sh
@@ -1,7 +1,8 @@
+# @TODO: Open JIRA. PR's are welcome. Open JIRA!
 function cli_vagrant_pre_run() {
-	:
+	exit_with_error "cli_vagrant_pre_run: not implemented"
 }
 
 function cli_vagrant_run() {
-	:
+	exit_with_error "cli_vagrant_run: not implemented"
 }
