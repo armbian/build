17cbb1dde94f693c2bb8b693f34b28ceae44e953
Ricardo Pardini <ricardo@pardini.net>
2023-01-17 04:02:02 +0100

    armbian-next: `chroot_sdcard_apt_get_update()` to replace and better log `chroot_sdcard_apt_get update`
    - also add retries where it was missing
    - @TODO: desktop_postinstall() is a mess, needs aggregation work

--- lib/functions/rootfs/rootfs-desktop.sh
+++ lib/functions/rootfs/rootfs-desktop.sh
@@ -1,23 +1,24 @@
 #!/usr/bin/env bash
 
 # a-kind-of-hook, called by install_distribution_agnostic() if it's a desktop build
-desktop_postinstall() {
+function desktop_postinstall() {
 
 	# disable display manager for the first run
 	disable_systemd_service_sdcard lightdm.service
 	disable_systemd_service_sdcard gdm3.service
 
-	# update packages index
-	chroot_sdcard_apt_get "update"
+	# @TODO: why? 
+	display_alert "Updating package lists" "for desktop" "info"
+	do_with_retries 3 chroot_sdcard_apt_get_update
 
-	# @TODO: rpardini: this is... missing from aggregation...?
-	# install per board packages
+	# @TODO: rpardini: this is... missing from aggregation...? it is used by 2 boards.
+	# install per board packages, desktop-only, packages.
 	if [[ -n ${PACKAGE_LIST_DESKTOP_BOARD} ]]; then
 		chroot_sdcard_apt_get_install "$PACKAGE_LIST_DESKTOP_BOARD"
 	fi
 
-	# install per family packages
-	if [[ -n ${PACKAGE_LIST_DESKTOP_FAMILY} ]]; then
+	# install per family packages (desktop only)
+	if [[ -n ${PACKAGE_LIST_DESKTOP_FAMILY} ]]; then # @TODO: used by 0 boards
 		chroot_sdcard_apt_get_install "$PACKAGE_LIST_DESKTOP_FAMILY"
 	fi
 
