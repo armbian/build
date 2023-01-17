32d7c5d6495e68fc1cc4251e8617ee24d0d65058
Ricardo Pardini <ricardo@pardini.net>
2023-01-17 04:43:50 +0100

    armbian-next: drop dead/duplicated code for bootsplash and mark kernel-drivers.sh (which includes bootsplash) as dead code

--- lib/library-functions.sh
+++ lib/library-functions.sh
@@ -285,15 +285,6 @@ source "${SRC}"/lib/functions/compilation/patch/drivers_network.sh
 #set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable - one day will be enabled
 set -o errtrace # trace ERR through - enabled
 set -o errexit  ## set -e : exit the script if any statement returns a non-true return value - enabled
-### lib/functions/compilation/patch/kernel-bootsplash.sh
-# shellcheck source=lib/functions/compilation/patch/kernel-bootsplash.sh
-source "${SRC}"/lib/functions/compilation/patch/kernel-bootsplash.sh
-
-# no errors tolerated. invoked before each sourced file to make sure.
-#set -o pipefail  # trace ERR through pipes - will be enabled "soon"
-#set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable - one day will be enabled
-set -o errtrace # trace ERR through - enabled
-set -o errexit  ## set -e : exit the script if any statement returns a non-true return value - enabled
 ### lib/functions/compilation/patch/kernel-drivers.sh
 # shellcheck source=lib/functions/compilation/patch/kernel-drivers.sh
 source "${SRC}"/lib/functions/compilation/patch/kernel-drivers.sh
