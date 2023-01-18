5ee07a281aec065319a21316c0fcf36dafab59f6
Ricardo Pardini <ricardo@pardini.net>
2023-01-18 17:24:19 +0100

    armbian-next: still fighting `tee` leaking under duress, turns out I _hadn't_ really won
    - last resort, use lazy umount.
    - include `lsof` in hostdeps, useful to debug these situations

--- lib/functions/host/prepare-host.sh
+++ lib/functions/host/prepare-host.sh
@@ -245,7 +245,7 @@ function adaptative_prepare_host_dependencies() {
 		kmod        # this causes initramfs rebuild, but is usually pre-installed, so no harm done unless it's an upgrade
 		libbison-dev libelf-dev libfdt-dev libfile-fcntllock-perl libmpc-dev libfl-dev liblz4-tool
 		libncurses-dev libssl-dev libusb-1.0-0-dev
-		linux-base locales
+		linux-base locales lsof
 		ncurses-base ncurses-term # for `make menuconfig`
 		ntpdate
 		patchutils pkg-config pv
