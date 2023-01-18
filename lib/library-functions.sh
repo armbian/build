4bd1bfa5dec986f7b7d0c1f3350b4d4727edb315
Tony <tonymckahan@gmail.com>
2023-01-18 15:45:50 +0100

    armbian-next: officially support WSL2; pester user for UTF-8 terminal
    - new `wsl2.sh` file with WSL2-related functions; checking/validating/pestering user; allow WSL2 (not WSL1)
    
    Co-authored-by: Ricardo Pardini <ricardo@pardini.net>

--- lib/library-functions.sh
+++ lib/library-functions.sh
@@ -573,6 +573,15 @@ source "${SRC}"/lib/functions/host/vagrant.sh
 #set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable - one day will be enabled
 set -o errtrace # trace ERR through - enabled
 set -o errexit  ## set -e : exit the script if any statement returns a non-true return value - enabled
+### lib/functions/host/wsl2.sh
+# shellcheck source=lib/functions/host/wsl2.sh
+source "${SRC}"/lib/functions/host/wsl2.sh
+
+# no errors tolerated. invoked before each sourced file to make sure.
+#set -o pipefail  # trace ERR through pipes - will be enabled "soon"
+#set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable - one day will be enabled
+set -o errtrace # trace ERR through - enabled
+set -o errexit  ## set -e : exit the script if any statement returns a non-true return value - enabled
 ### lib/functions/image/compress-checksum.sh
 # shellcheck source=lib/functions/image/compress-checksum.sh
 source "${SRC}"/lib/functions/image/compress-checksum.sh
