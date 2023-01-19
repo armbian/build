859e21e7167f2270c33e3eec9a434fb3a6d66e1c
Ricardo Pardini <ricardo@pardini.net>
2023-01-19 23:46:49 +0100

    armbian-next: adaptive shallow/full Kernel git bare git (3gb -> 300mb download)
    - allows using pre-shallow-ed bare trees, specific to the KERNEL_MAJOR_MINOR
    - uses shallow by default if storage is "slow" (mmc), or free disk space < 32g (@TODO tweak this)
    - if full tree already there keeps using it
    - allow forcing decision with `KERNEL_GIT=full` or `KERNEL_GIT=shallow`
    - countdown to confirm if decision based on storage type/free space and user is interactive
      - this has some of the worst copy ever written
    - @TODO refactor the device type / free space code, reuse across codebase (3-4 other spots detect free space)
    - @TODO GHA self-hosted (full) vs hosted (shallow) runners decision
    - sprinkle with `wait_for_disk_sync()` around clean/download/extract
    - sprinkle git with debugs, and an info between fetch and checkout (otherwise it seems "fetch" takes a long time)

--- lib/library-functions.sh
+++ lib/library-functions.sh
@@ -186,6 +186,24 @@ source "${SRC}"/lib/functions/compilation/kernel-debs.sh
 #set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable - one day will be enabled
 set -o errtrace # trace ERR through - enabled
 set -o errexit  ## set -e : exit the script if any statement returns a non-true return value - enabled
+### lib/functions/compilation/kernel-git-bundle.sh
+# shellcheck source=lib/functions/compilation/kernel-git-bundle.sh
+source "${SRC}"/lib/functions/compilation/kernel-git-bundle.sh
+
+# no errors tolerated. invoked before each sourced file to make sure.
+#set -o pipefail  # trace ERR through pipes - will be enabled "soon"
+#set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable - one day will be enabled
+set -o errtrace # trace ERR through - enabled
+set -o errexit  ## set -e : exit the script if any statement returns a non-true return value - enabled
+### lib/functions/compilation/kernel-git-oras.sh
+# shellcheck source=lib/functions/compilation/kernel-git-oras.sh
+source "${SRC}"/lib/functions/compilation/kernel-git-oras.sh
+
+# no errors tolerated. invoked before each sourced file to make sure.
+#set -o pipefail  # trace ERR through pipes - will be enabled "soon"
+#set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable - one day will be enabled
+set -o errtrace # trace ERR through - enabled
+set -o errexit  ## set -e : exit the script if any statement returns a non-true return value - enabled
 ### lib/functions/compilation/kernel-git.sh
 # shellcheck source=lib/functions/compilation/kernel-git.sh
 source "${SRC}"/lib/functions/compilation/kernel-git.sh
