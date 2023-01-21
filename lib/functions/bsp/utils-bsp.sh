486ba2953c8a7c79291c3914d05d8d64a677c289
Ricardo Pardini <ricardo@pardini.net>
2023-01-21 05:12:33 +0100

    armbian-next: bsp: use `prepare_temp_dir_in_workdir_and_schedule_cleanup()`; bring back `PACKAGES_SEARCH_ROOT_ABSOLUTE_DIRS`

--- lib/functions/bsp/utils-bsp.sh
+++ lib/functions/bsp/utils-bsp.sh
@@ -3,11 +3,23 @@
 #
 copy_all_packages_files_for() {
 	local package_name="${1}"
+
+	# @TODO: rpardini: this was recovered after being assassinated by some insane person who rewrote aggregation in Python
+	declare PACKAGES_SEARCH_ROOT_ABSOLUTE_DIRS="
+	${SRC}/packages
+	${SRC}/config/optional/_any_board/_packages
+	${SRC}/config/optional/architectures/${ARCH}/_packages
+	${SRC}/config/optional/families/${LINUXFAMILY}/_packages
+	${SRC}/config/optional/boards/${BOARD}/_packages
+	"
+
 	for package_src_dir in ${PACKAGES_SEARCH_ROOT_ABSOLUTE_DIRS}; do
 		local package_dirpath="${package_src_dir}/${package_name}"
 		if [ -d "${package_dirpath}" ]; then
-			cp -r "${package_dirpath}/"* "${destination}/" 2> /dev/null
-			display_alert "Adding files from" "${package_dirpath}"
+			display_alert "Adding found files" "${package_dirpath} for '${package_name}'" "debug"
+			run_host_command_logged cp -r "${package_dirpath}/"* "${destination}/"
+		else
+			display_alert "No files found in" "${package_dirpath} for '${package_name}'" "debug"
 		fi
 	done
 }
