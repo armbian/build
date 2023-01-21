9793e9bb7fa4f0cda256d55c016c4e7c66fbd5a6
Ricardo Pardini <ricardo@pardini.net>
2023-01-21 05:15:19 +0100

    armbian-next: extensions: move 'extensions.sh' into library; replace manual source with init function
    - wraps globals in `extension_manager_declare_globals()` -- and make them actually global
    - whoever wrote this original code is insane, add copyright

--- lib/library-functions.sh
+++ lib/library-functions.sh
@@ -474,6 +474,15 @@ source "${SRC}"/lib/functions/general/downloads.sh
 #set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable - one day will be enabled
 set -o errtrace # trace ERR through - enabled
 set -o errexit  ## set -e : exit the script if any statement returns a non-true return value - enabled
+### lib/functions/general/extensions.sh
+# shellcheck source=lib/functions/general/extensions.sh
+source "${SRC}"/lib/functions/general/extensions.sh
+
+# no errors tolerated. invoked before each sourced file to make sure.
+#set -o pipefail  # trace ERR through pipes - will be enabled "soon"
+#set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable - one day will be enabled
+set -o errtrace # trace ERR through - enabled
+set -o errexit  ## set -e : exit the script if any statement returns a non-true return value - enabled
 ### lib/functions/general/git.sh
 # shellcheck source=lib/functions/general/git.sh
 source "${SRC}"/lib/functions/general/git.sh
