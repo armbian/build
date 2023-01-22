4bacb08ee73be4e87c69543ec3c69060f83f61b2
Ricardo Pardini <ricardo@pardini.net>
2023-01-22 23:19:40 +0100

    armbian-next: use unified methods for tmp dir handling all around (except ATF, which is special)

--- lib/functions/bsp/bsp-desktop.sh
+++ lib/functions/bsp/bsp-desktop.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
-create_desktop_package() {
+function create_desktop_package() {
 	# produced by aggregation.py
 	display_alert "bsp-desktop: AGGREGATED_PACKAGES_DESKTOP_COMMA" "'${AGGREGATED_PACKAGES_DESKTOP_COMMA}'" "debug"
 
-	local destination tmp_dir
-	tmp_dir=$(mktemp -d) # subject to TMPDIR/WORKDIR, so is protected by single/common error trapmanager to clean-up.
-	destination=${tmp_dir}/${BOARD}/${CHOSEN_DESKTOP}_${REVISION}_all
+	declare cleanup_id="" tmp_dir=""
+	prepare_temp_dir_in_workdir_and_schedule_cleanup "bsp-desktop" cleanup_id tmp_dir # namerefs
+
+	declare destination="${tmp_dir}/${BOARD}/${CHOSEN_DESKTOP}_${REVISION}_all"
 	rm -rf "${destination}"
 	mkdir -p "${destination}"/DEBIAN
 
@@ -42,14 +43,18 @@ create_desktop_package() {
 	cd "${destination}" || exit_with_error "Failed to cd to ${destination}"
 	cd ..
 	fakeroot_dpkg_deb_build "${destination}" "${DEB_STORAGE}/${RELEASE}/${CHOSEN_DESKTOP}_${REVISION}_all.deb"
+
+	done_with_temp_dir "${cleanup_id}" # changes cwd to "${SRC}" and fires the cleanup function early
 }
 
-create_bsp_desktop_package() {
+function create_bsp_desktop_package() {
 	display_alert "Creating board support package for desktop" "${package_name}" "info"
+
 	local package_name="${BSP_DESKTOP_PACKAGE_FULLNAME}"
-	local destination tmp_dir
-	tmp_dir=$(mktemp -d) # subject to TMPDIR/WORKDIR, so is protected by single/common error trapmanager to clean-up.
-	destination=${tmp_dir}/${BOARD}/${BSP_DESKTOP_PACKAGE_FULLNAME}
+	declare cleanup_id="" tmp_dir=""
+	prepare_temp_dir_in_workdir_and_schedule_cleanup "bsp-desktop2" cleanup_id tmp_dir # namerefs
+
+	local destination=${tmp_dir}/${BOARD}/${BSP_DESKTOP_PACKAGE_FULLNAME}
 	rm -rf "${destination}"
 	mkdir -p "${destination}"/DEBIAN
 
@@ -84,4 +89,6 @@ create_bsp_desktop_package() {
 	cd "${destination}" || exit_with_error "Failed to cd to ${destination}"
 	cd ..
 	fakeroot_dpkg_deb_build "${destination}" "${DEB_STORAGE}/${RELEASE}/${package_name}.deb"
+
+	done_with_temp_dir "${cleanup_id}" # changes cwd to "${SRC}" and fires the cleanup function early
 }
