872463f2f033b803578b6548469e836390694303
Ricardo Pardini <ricardo@pardini.net>
2023-01-22 17:34:19 +0100

    armbian-next: `aggregation`: dont' require or consider `BOARD` or `LINUXFAMILY` for aggregation anymore
    - it was only used for "bsp-aggregation" of `rk3318-box` which I will come up with something extensions/hooks based later

--- config/optional/boards/rk3318-box/_config/desktop/_all_distributions/environments/_all_environments/armbian/create_desktop_package.sh
+++ config/optional/boards/rk3318-box/_config/desktop/_all_distributions/environments/_all_environments/armbian/create_desktop_package.sh
@@ -3,3 +3,5 @@ if [[ "$BRANCH" != "legacy" ]]; then
 	mkdir -p $destination/etc/X11/xorg.conf.d
         cp $SRC/packages/bsp/rk3328/40-serverflags.conf $destination/etc/X11/xorg.conf.d
 fi
+
+# @TODO: @paolosabatino: this is no longer included in aggregation; it will have to be handled in some kind of extension
