af5d834d9984826a83b14854986becc466a3a11a
Ricardo Pardini <ricardo@pardini.net>
2023-01-22 14:54:48 +0100

    armbian-next: `rootfs`: new CLI command for rootfs cache building; further refactorings
    - _told ya it would all make sense eventually..._
    - introduce new, real, non-alias `rootfs` command in `cli-rootfs` -- this _only_ builds rootfs, nothing else
    - no more `main_default_build_single()`; instead `full_build_packages_rootfs_and_image()`
    - introduce `do_with_default_build()` wrapper, for things that need host prepared + aggregation to run
    - CLI-specific parts of `main_default_build_single()` moved to cli-build
    - put prepare/cleanup pair of `prepare_rootfs_build_params_and_trap()` and `trap_handler_cleanup_rootfs_and_image()` into new `trap-rootfs.sh` (this needs further splitting for the image part)
    - refactor `get_or_create_rootfs_cache_chroot_sdcard()` into `build_rootfs_only()`, `calculate_rootfs_cache_id()`
    - update library with new files

--- lib/functions/main/default-build.sh
+++ lib/functions/main/default-build.sh
@@ -1,8 +1,5 @@
-# This does NOT run under the logging manager. We should invoke the do_with_logging wrapper for
-# strategic parts of this. Attention: almost everything does it's own logging.
-function main_default_build_single() {
-	main_default_start_build # Has its own logging, prepares workdir, does prepare_host, aggregation, and
-
+# This does NOT run under the logging manager. 
+function full_build_packages_rootfs_and_image() {
 	main_default_build_packages # has its own logging sections # requires aggregation
 
 	# build rootfs, if not only kernel. Again, read "KERNEL_ONLY" as if it was "PACKAGES_ONLY"
@@ -11,24 +8,10 @@ function main_default_build_single() {
 		build_rootfs_and_image # old "debootstrap-ng"; has its own logging sections.
 		display_alert "Done building image" "${BOARD}" "target-reached"
 	fi
+}
 
+function do_with_default_build() {
+	main_default_start_build # Has its own logging, prepares workdir, does prepare_host, aggregation, and
+	"${@}"
 	main_default_end_build
-
-	if armbian_is_running_in_container; then
-		BUILD_CONFIG='docker' # @TODO: this is not true, CLI handles this differently, gotta ask the CLI; this whole thing is inconsistent
-	fi
-
-	# Make it easy to repeat build by displaying build options used. Prepare array. @TODO this is inconsistent. Maybe something like the relaunch vars?
-	local -a repeat_args=("./compile.sh" "${BUILD_CONFIG}" " BRANCH=${BRANCH}")
-	[[ -n ${RELEASE} ]] && repeat_args+=("RELEASE=${RELEASE}")
-	[[ -n ${BUILD_MINIMAL} ]] && repeat_args+=("BUILD_MINIMAL=${BUILD_MINIMAL}")
-	[[ -n ${BUILD_DESKTOP} ]] && repeat_args+=("BUILD_DESKTOP=${BUILD_DESKTOP}")
-	[[ -n ${KERNEL_ONLY} ]] && repeat_args+=("KERNEL_ONLY=${KERNEL_ONLY}")
-	[[ -n ${KERNEL_CONFIGURE} ]] && repeat_args+=("KERNEL_CONFIGURE=${KERNEL_CONFIGURE}")
-	[[ -n ${DESKTOP_ENVIRONMENT} ]] && repeat_args+=("DESKTOP_ENVIRONMENT=${DESKTOP_ENVIRONMENT}")
-	[[ -n ${DESKTOP_ENVIRONMENT_CONFIG_NAME} ]] && repeat_args+=("DESKTOP_ENVIRONMENT_CONFIG_NAME=${DESKTOP_ENVIRONMENT_CONFIG_NAME}")
-	[[ -n ${DESKTOP_APPGROUPS_SELECTED} ]] && repeat_args+=("DESKTOP_APPGROUPS_SELECTED=\"${DESKTOP_APPGROUPS_SELECTED}\"")
-	[[ -n ${DESKTOP_APT_FLAGS_SELECTED} ]] && repeat_args+=("DESKTOP_APT_FLAGS_SELECTED=\"${DESKTOP_APT_FLAGS_SELECTED}\"")
-	[[ -n ${COMPRESS_OUTPUTIMAGE} ]] && repeat_args+=("COMPRESS_OUTPUTIMAGE=${COMPRESS_OUTPUTIMAGE}")
-	display_alert "Repeat Build Options" "${repeat_args[*]}" "ext" # * = expand array, space delimited, single-word.
 }
