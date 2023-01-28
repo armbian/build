5ff0452a745dce967d7096976b494499d85e18b0
Ricardo Pardini <ricardo@pardini.net>
2023-01-28 20:03:06 +0100

    armbian-next: move aggregation-had-run check to more strategic places (pre-bsp, pre-rootfs) instead of at the beginning of full_build_packages_rootfs_and_image()

--- lib/functions/main/default-build.sh
+++ lib/functions/main/default-build.sh
@@ -1,14 +1,13 @@
 # This does NOT run under the logging manager.
 function full_build_packages_rootfs_and_image() {
 
-	assert_requires_aggregation # Bombs if aggregation has not run
-
 	main_default_build_packages # has its own logging sections # requires aggregation
 
 	# build rootfs, if not only kernel. Again, read "KERNEL_ONLY" as if it was "PACKAGES_ONLY"
 	if [[ "${KERNEL_ONLY}" != "yes" ]]; then
 		display_alert "Building image" "${BOARD}" "target-started"
-		build_rootfs_and_image # old "debootstrap-ng"; has its own logging sections.
+		assert_requires_aggregation # Bombs if aggregation has not run
+		build_rootfs_and_image      # old "debootstrap-ng"; has its own logging sections.
 		display_alert "Done building image" "${BOARD}" "target-reached"
 	fi
 }
