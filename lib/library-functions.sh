46b48ea21418036b4b11527b76a0c1fad104ed7c
Ricardo Pardini <ricardo@pardini.net>
2023-02-02 14:44:44 +0100

    armbian-next: tooling: introduce helper/downloader for shellcheck; include in Dockerfile

--- lib/library-functions.sh
+++ lib/library-functions.sh
@@ -618,6 +618,15 @@ source "${SRC}"/lib/functions/general/retry.sh
 #set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable - one day will be enabled
 set -o errtrace # trace ERR through - enabled
 set -o errexit  ## set -e : exit the script if any statement returns a non-true return value - enabled
+### lib/functions/general/shellcheck.sh
+# shellcheck source=lib/functions/general/shellcheck.sh
+source "${SRC}"/lib/functions/general/shellcheck.sh
+
+# no errors tolerated. invoked before each sourced file to make sure.
+#set -o pipefail  # trace ERR through pipes - will be enabled "soon"
+#set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable - one day will be enabled
+set -o errtrace # trace ERR through - enabled
+set -o errexit  ## set -e : exit the script if any statement returns a non-true return value - enabled
 ### lib/functions/host/apt-cacher-ng.sh
 # shellcheck source=lib/functions/host/apt-cacher-ng.sh
 source "${SRC}"/lib/functions/host/apt-cacher-ng.sh
