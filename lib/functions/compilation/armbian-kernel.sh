3198b234c5b5db05c620af3ea7a8ee7a754ce7f5
Ricardo Pardini <ricardo@pardini.net>
2023-02-07 15:28:05 +0100

    armbian-next: armbian-kernel: force `CONFIG_LOCALVERSION_AUTO=n` and `DEBUG_INFO=n`

--- lib/functions/compilation/armbian-kernel.sh
+++ lib/functions/compilation/armbian-kernel.sh
@@ -2,13 +2,23 @@
 
 # This is an internal/core extension.
 function armbian_kernel_config__disable_module_compression() {
-	display_alert "Disabling module compression and signing" "armbian-kernel" "debug"
+	display_alert "Disabling module compression and signing / debug / auto version" "armbian-kernel" "debug"
+	# DONE: Disable: signing, and compression of modules, for speed.
 	kernel_config_set_n CONFIG_MODULE_COMPRESS_XZ # No use double-compressing modules
 	kernel_config_set_n CONFIG_MODULE_COMPRESS_ZSTD
 	kernel_config_set_n CONFIG_MODULE_COMPRESS_GZIP
 	kernel_config_set_y CONFIG_MODULE_COMPRESS_NONE
+
 	kernel_config_set_n CONFIG_SECURITY_LOCKDOWN_LSM
 	kernel_config_set_n CONFIG_MODULE_SIG # No use signing modules
+
+	# DONE: Disable: version shenanigans
+	kernel_config_set_n CONFIG_LOCALVERSION_AUTO # This causes a mismatch between what Armbian wants and what make produces.
+
+	# DONE: Disable: debug option
+	kernel_config_set_n DEBUG_INFO # Armbian doesn't know how to package a debug kernel.
+
+	# @TODO: Enable the options for the extrawifi/drivers; so we don't need to worry about them when updating configs
 }
 
 # Helpers for manipulating kernel config. @TODO: hash of changes made
