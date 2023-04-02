c6f451af5b9f0df63d022f92090366b46fb3d45e
Ricardo Pardini <ricardo@pardini.net>
2023-04-02 02:11:37 +0200

    artifact-uboot: add hashes for extension and old-timey `-H`ooks and `-V`ariables hash (`BOOTDELAY` et al)
    - helper for hashing old-timey hooks: `calculate_hash_for_function_bodies()` in `hash-files.sh` cos I'm lazy

--- lib/functions/artifacts/artifact-uboot.sh
+++ lib/functions/artifacts/artifact-uboot.sh
@@ -55,6 +55,34 @@ function artifact_uboot_prepare_version() {
 	patches_hash="${hash_files}"
 	declare uboot_patches_hash_short="${patches_hash:0:${short_hash_size}}"
 
+	# Hash the extension hooks
+	declare -a extension_hooks_to_hash=(
+		"post_uboot_custom_postprocess" "fetch_custom_uboot" "build_custom_uboot"
+		"pre_config_uboot_target" "post_uboot_custom_postprocess" "post_uboot_custom_postprocess"
+	)
+	declare -a extension_hooks_hashed=("$(dump_extension_method_sources_functions "${extension_hooks_to_hash[@]}")")
+	declare hash_hooks="undetermined"
+	hash_hooks="$(echo "${extension_hooks_hashed[@]}" | sha256sum | cut -d' ' -f1)"
+
+	# Hash the old-timey hooks
+	declare hash_functions="undetermined"
+	calculate_hash_for_function_bodies "write_uboot_platform" "write_uboot_platform_mtd" "setup_write_uboot_platform"
+	declare hash_uboot_functions="${hash_functions}"
+
+	# Hash those two together
+	declare hash_hooks_and_functions="undetermined"
+	hash_hooks_and_functions="$(echo "${hash_hooks}" "${hash_uboot_functions}" | sha256sum | cut -d' ' -f1)"
+	declare hash_hooks_and_functions_short="${hash_hooks_and_functions:0:${short_hash_size}}"
+
+	# Hash variables that affect the build and package of u-boot
+	declare -a vars_to_hash=(
+		"${BOOTDELAY}" "${UBOOT_DEBUGGING}"
+	)
+	declare hash_vars="undetermined"
+	hash_vars="$(echo "${vars_to_hash[@]}" | sha256sum | cut -d' ' -f1)"
+	declare vars_config_hash="${hash_vars}"
+	declare var_config_hash_short="${vars_config_hash:0:${short_hash_size}}"
+
 	# get the hashes of the lib/ bash sources involved...
 	declare hash_files="undetermined"
 	calculate_hash_for_files "${SRC}"/lib/functions/compilation/uboot*.sh
@@ -62,12 +90,15 @@ function artifact_uboot_prepare_version() {
 	declare bash_hash_short="${bash_hash:0:${short_hash_size}}"
 
 	# outer scope
-	artifact_version="${artifact_prefix_version}${GIT_INFO_UBOOT[MAKEFILE_VERSION]}-S${short_sha1}-P${uboot_patches_hash_short}-B${bash_hash_short}"
+	artifact_version="${artifact_prefix_version}${GIT_INFO_UBOOT[MAKEFILE_VERSION]}-S${short_sha1}-P${uboot_patches_hash_short}-H${hash_hooks_and_functions_short}-V${var_config_hash_short}-B${bash_hash_short}"
 
 	declare -a reasons=(
 		"version \"${GIT_INFO_UBOOT[MAKEFILE_FULL_VERSION]}\""
 		"git revision \"${GIT_INFO_UBOOT[SHA1]}\""
 		"patches hash \"${patches_hash}\""
+		"Extension hooks hash \"${hash_hooks}\""
+		"uboot functions hash \"${hash_uboot_functions}\""
+		"variables hash \"${vars_config_hash}\""
 		"framework bash hash \"${bash_hash}\""
 	)
 
