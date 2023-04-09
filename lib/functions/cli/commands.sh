dcc6c0658b3383e59c98757c7cd88eb0540fa0dc
Ricardo Pardini <ricardo@pardini.net>
2023-04-09 12:44:59 +0200

    artifacts/cli: introduce CLI command `artifact-config-dump-json` (alias to `artifact`, but sets `CONFIG_DEFS_ONLY=yes`)
    - if CONFIG_DEFS_ONLY during `obtain_complete_artifact()`, just dump JSON to stdout after calculating version/coordinates
    - don't prepare ORAS tooling if CONFIG_DEFS_ONLY
    - don't run in standard build (and thus don't prepare_host()) if under CONFIG_DEFS_ONLY
    - if CONFIG_DEFS_ONLY, don't run `check_basic_host()`

--- lib/functions/cli/commands.sh
+++ lib/functions/cli/commands.sh
@@ -35,7 +35,8 @@ function armbian_register_commands() {
 		["oras-upload"]="oras" # implemented in cli_oras_pre_run and cli_oras_run; up/down/info are the same, see vars below
 
 		# all-around artifact wrapper
-		["artifact"]="artifact" # implemented in cli_artifact_pre_run and cli_artifact_run
+		["artifact"]="artifact"                  # implemented in cli_artifact_pre_run and cli_artifact_run
+		["artifact-config-dump-json"]="artifact" # implemented in cli_artifact_pre_run and cli_artifact_run
 
 		# shortcuts, see vars set below. the use legacy single build, and try to control it via variables
 		["rootfs"]="artifact"
@@ -71,6 +72,8 @@ function armbian_register_commands() {
 		["dockershell"]="DOCKER_SUBCMD='shell'"
 
 		["generate-dockerfile"]="DOCKERFILE_GENERATE_ONLY='yes'"
+
+		["artifact-config-dump-json"]='CONFIG_DEFS_ONLY="yes"'
 
 		# artifact shortcuts
 		["rootfs"]="WHAT='rootfs' ${common_cli_artifact_vars}"
