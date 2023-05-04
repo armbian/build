7c0fb70a2bda08711ee619962cfaa329da6a4890
Ricardo Pardini <ricardo@pardini.net>
2023-05-04 13:21:06 +0200

    configdump: alias `config-dump`, `config-dump-json` and new `config-dump-no-json` (bash declare format)

--- lib/functions/cli/cli-configdump.sh
+++ lib/functions/cli/cli-configdump.sh
@@ -15,6 +15,12 @@ function cli_config_dump_json_run() {
 	# configuration etc - it initializes the extension manager
 	do_capturing_defs config_board_and_remove_useless < /dev/null # this sets CAPTURED_VARS_NAMES and CAPTURED_VARS_ARRAY; the < /dev/null is take away the terminal from stdin
 
+	if [[ "${ARMBIAN_COMMAND}" == "config-dump-no-json" ]]; then
+		# for debugging the bash-declare-to-JSON parser
+		echo "${CAPTURED_VARS_ARRAY[@]}"
+		return 0
+	fi
+
 	# convert to JSON, using python helper; each var is passed via a command line argument; that way we avoid newline/nul-char separation issues
 	display_alert "Dumping JSON" "for ${#CAPTURED_VARS_ARRAY[@]} variables" "ext"
 	python3 "${SRC}/lib/tools/configdump2json.py" "--args" "${CAPTURED_VARS_ARRAY[@]}" # to stdout
