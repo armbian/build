9f03595e308630c196018b03592ede1a626c1150
Ricardo Pardini <ricardo@pardini.net>
2023-05-08 12:21:05 +0200

    rootfs/aggregation/`helios64`: fix: include `PACKAGE_LIST_BOARD_REMOVE` (and FAMILY version) into rootfs' `artifact_input_variables`
    - `PACKAGE_LIST_BOARD_REMOVE` is used only in `helios64` board
    - the fact is `PACKAGE_LIST_BOARD_REMOVE` removes the packages _from the rootfs_ and not _from the image_
      - this only accounts for that fact, but in the future we should actually change how this works

--- lib/functions/artifacts/artifact-rootfs.sh
+++ lib/functions/artifacts/artifact-rootfs.sh
@@ -17,7 +17,7 @@ function artifact_rootfs_config_dump() {
 	artifact_input_variables[DESKTOP_APPGROUPS_SELECTED]="${DESKTOP_APPGROUPS_SELECTED:-"no_DESKTOP_APPGROUPS_SELECTED_set"}"
 	# Hash of the packages added/removed by extensions
 	declare pkgs_hash="undetermined"
-	pkgs_hash="$(echo "${REMOVE_PACKAGES[*]} ${EXTRA_PACKAGES_ROOTFS[*]}" | sha256sum | cut -d' ' -f1)"
+	pkgs_hash="$(echo "${REMOVE_PACKAGES[*]} ${EXTRA_PACKAGES_ROOTFS[*]} ${PACKAGE_LIST_BOARD_REMOVE} ${PACKAGE_LIST_FAMILY_REMOVE}" | sha256sum | cut -d' ' -f1)"
 	artifact_input_variables[EXTRA_PKG_ADD_REMOVE_HASH]="${pkgs_hash}"
 }
 
