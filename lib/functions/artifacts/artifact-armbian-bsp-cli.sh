b4565546107a9bd615910f72f1efd62af7b8991c
Ricardo Pardini <ricardo@pardini.net>
2023-05-13 10:42:44 +0200

    artifacts(debs): rationalize "bash hash" creation with new `calculate_hash_for_bash_deb_artifact()` helper

--- lib/functions/artifacts/artifact-armbian-bsp-cli.sh
+++ lib/functions/artifacts/artifact-armbian-bsp-cli.sh
@@ -72,7 +72,7 @@ function artifact_armbian-bsp-cli_prepare_version() {
 
 	# get the hashes of the lib/ bash sources involved...
 	declare hash_files="undetermined"
-	calculate_hash_for_files "${SRC}"/lib/functions/bsp/armbian-bsp-cli-deb.sh
+	calculate_hash_for_bash_deb_artifact "bsp/armbian-bsp-cli-deb.sh"
 	declare bash_hash="${hash_files}"
 	declare bash_hash_short="${bash_hash:0:${short_hash_size}}"
 
--- lib/functions/artifacts/artifact-kernel.sh
+++ lib/functions/artifacts/artifact-kernel.sh
@@ -154,7 +154,7 @@ function artifact_kernel_prepare_version() {
 
 	# get the hashes of the lib/ bash sources involved...
 	declare hash_files="undetermined"
-	calculate_hash_for_files "${SRC}"/lib/functions/compilation/kernel*.sh
+	calculate_hash_for_bash_deb_artifact "${SRC}"/lib/functions/compilation/kernel*.sh # expansion
 	declare bash_hash="${hash_files}"
 	declare bash_hash_short="${bash_hash:0:${short_hash_size}}"
 
