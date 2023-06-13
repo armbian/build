07c2ae66bdb7873517a4a9347f0d892c873d83da
Patrick Yavitz <pyavitz@gmail.com>
2023-06-13 18:28:02 -0400

    patch: misc: rtw88: `temporary workarounds`
    Review patches for information on the subject.
    Tested on: BPI-CM4, X96-AIR and Tinkerboard R2
    
    Signed-off-by: Patrick Yavitz <pyavitz@gmail.com>

--- lib/functions/compilation/patch/drivers_network.sh
+++ lib/functions/compilation/patch/drivers_network.sh
@@ -412,19 +412,15 @@ driver_rtw88() {
 		process_patch_file "${SRC}/patch/misc/rtw88/${version}/001-rtw88-linux-next.patch" "applying"
 		process_patch_file "${SRC}/patch/misc/rtw88/${version}/002-rtw88-linux-next.patch" "applying"
 		process_patch_file "${SRC}/patch/misc/rtw88/${version}/003-rtw88-add-missing-unwind-goto-for_rtw_download_firmware.patch" "applying"
-		if [[ "$BOARD" == "bananapicm4io" ]]; then
-			display_alert "Adding" "SDIO RX Aggregation Limiting" "info"
-			process_patch_file "${SRC}/patch/misc/rtw88/${version}/HACK/001-rtw88-SDIO-RX-aggregation-limiting.patch" "applying"
-		fi
+		process_patch_file "${SRC}/patch/misc/rtw88/hack/001-revert-rtw88-sdio-size-and-timout-to-rfc-v1.patch" "applying"
+		process_patch_file "${SRC}/patch/misc/rtw88/hack/002-rtw88-usb-make-work-queues-high-priority.patch" "applying"
 	fi
 	if [[ "$version" == "6.4" ]] && [ $EXTRAWIFI == yes ]; then
 		display_alert "Adding" "Upstream wireless RTW88 drivers" "info"
 		process_patch_file "${SRC}/patch/misc/rtw88/${version}/001-rtw88-add-support-for-the-RTL8723DS-SDIO-wifi-chip.patch" "applying"
 		process_patch_file "${SRC}/patch/misc/rtw88/${version}/002-rtw88-add-missing-unwind-goto-for_rtw_download_firmware.patch" "applying"
-		if [[ "$BOARD" == "bananapicm4io" ]]; then
-			display_alert "Adding" "SDIO RX Aggregation Limiting" "info"
-			process_patch_file "${SRC}/patch/misc/rtw88/${version}/HACK/001-rtw88-SDIO-RX-aggregation-limiting.patch" "applying"
-		fi
+		process_patch_file "${SRC}/patch/misc/rtw88/hack/001-revert-rtw88-sdio-size-and-timout-to-rfc-v1.patch" "applying"
+		process_patch_file "${SRC}/patch/misc/rtw88/hack/002-rtw88-usb-make-work-queues-high-priority.patch" "applying"
 	fi
 }
 
