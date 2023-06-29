c13dc497c4f6ed258d988d67699986a1b33a08af
Ricardo Pardini <ricardo@pardini.net>
2023-06-29 13:41:23 +0200

    `mekotronics`: refactor vendor conf/hooks; vendor uboot with new patches; new blobs
    - meko: don't use Radxa's Rock-5A u-boot anymore, switch to rockchip vendor next-dev branch
    - meko: my own (horrible) patches for MAC address stability / defconfig
    - meko: cherry picked a few patches for getting rid of BL32/read Bl31 from env and other fixes from Radxa
    - meko: add OTP node to 3588 dtsi (so we don't need kern-dtb in ITS for working OTP)
    - meko: refactor common code across 3 (soon to be 4) board files into vendor conf and hooks
    - meko: bump DDR/BL31 blobs for all Meko boards

---
+++ config/sources/vendors/mekotronics/mekotronics-rk3588.hooks.sh
@@ -0,0 +1,27 @@
+#
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (c) 2023 Ricardo Pardini <ricardo@pardini.net>
+# This file is a part of the Armbian Build Framework https://github.com/armbian/build/
+#
+
+# Newer, tested-to-work rkbins.
+function post_family_config__blobs_mekotronics() {
+	display_alert "$BOARD" "Configuring Mekotronics ($BOARD) blobs" "info"
+	declare -g DDR_BLOB='rk35/rk3588_ddr_lp4_2112MHz_lp5_2736MHz_v1.11.bin'
+	declare -g BL31_BLOB='rk35/rk3588_bl31_v1.38.elf'
+}
+
+# Vendor u-boot, standard rockchip, plus patches.
+function post_family_config__uboot_mekotronics() {
+	display_alert "$BOARD" "Configuring Mekotronics R58 ($BOARD) u-boot" "info"
+
+	declare -g BOOTSOURCE='https://github.com/rockchip-linux/u-boot.git'
+	#declare -g BOOTBRANCH='branch:next-dev' # disabled, using specific commit below to avoid breakage in the future
+	declare -g BOOTBRANCH="commit:cc781e0266d589b937b5af9e4850fb1e7222f0f1" # specific commit in next-dev branch; tested to work
+
+	declare -g BOOTDIR="u-boot-meko-rk3588"             # do not share u-boot directory
+	declare -g BOOTPATCHDIR="legacy/u-boot-meko-rk3588" # Few patches in there; MAC address & defconfig
+
+	declare -g OVERLAY_PREFIX='rockchip-rk3588'
+	declare -g BOOTDELAY=1 # build injects this into u-boot config. we can then get into UMS mode and avoid the whole rockusb/rkdeveloptool thing
+}
