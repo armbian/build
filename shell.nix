38cc22e99bb39bdd45b9a12e19a2b5114ad617cf
Jacob Hrbek <kreyren@rixotstudio.cz>
2023-09-10 00:11:13 +0200

    Cherrypick https://github.com/armbian/build/pull/5691

--- shell.nix
+++ shell.nix
@@ -1,8 +1,15 @@
 # Configuration file for Nix(OS) to provide a temporary development enviroment
 
 { pkgs ? import <nixpkgs> {} }:
-  pkgs.mkShell {
-    nativeBuildInputs = with pkgs.buildPackages; [
-      util-linux # Needed for uuidgen
-    ];
-}
+let
+	unstable = import <nixpkgs> {};
+in
+	pkgs.mkShell {
+		name = "armbian-build";
+		buildInputs = with pkgs; [
+			bash # For evaluation
+			nil # Language server for nix
+			shellcheck # For linting shell
+			util-linux # Needed for uuidgen
+		];
+	}
