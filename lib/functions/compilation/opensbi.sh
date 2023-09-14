db2af29e6cb6810dc020b901fd375e63f77481fd
Jacob Hrbek <kreyren@rixotstudio.cz>
2023-09-14 04:02:26 +0200

    Turn into spagheti code

--- lib/functions/compilation/opensbi.sh
+++ lib/functions/compilation/opensbi.sh
@@ -2,55 +2,54 @@
 #
 # SPDX-License-Identifier: GPL-2.0
 #
-# Copyright (c) 2023 Jacob Hrbek, kreyren@armbian.com
+# Copyright (c) 2013-2023 Igor Pecovnik, igor@armbian.com
 #
 # This file is a part of the Armbian Build Framework
 # https://github.com/armbian/build/
 
 compile_opensbi() {
-	case "$OPENSBISOURCE" in
-		""|"none") true	;;
-		*)
-			display_alert "Downloading sources" "opensbi" "git"
-			fetch_from_repo "$OPENSBISOURCE" "$OPENSBIDIR" "$OPENSBIBRANCH" "yes"
-	esac
+	if [[ -n "${OPENSBISOURCE}" && "${OPENSBISOURCE}" != "none" ]]; then
+		display_alert "Downloading sources" "opensbi" "git"
+		fetch_from_repo "$OPENSBISOURCE" "$OPENSBIDIR" "$OPENSBIBRANCH" "yes"
+	fi
 
-	# Clean
-	case "$CLEAN_LEVEL" in *make-opensbi*)
-		display_alert "Cleaning" "$OPENSBISOURCEDIR" "info"
+	if [[ $CLEAN_LEVEL == *make-opensbi* ]]; then
+		display_alert "Cleaning OpenSBI tree - CLEAN_LEVEL contains 'make-opensbi'" "$ATFSOURCEDIR" "info"
 		(
-			cd "${SRC}/cache/sources/$OPENSBISOURCEDIR" || exit_with_error "crazy about $OPENSBISOURCEDIR"
+			cd "${SRC}/cache/sources/${OPENSBISOURCEDIR}" || exit_with_error "crazy about ${OPENSBISOURCEDIR}"
 			run_host_command_logged make distclean
 		)
-	esac
+	else
+		display_alert "Not cleaning OpenSBI tree, use CLEAN_LEVEL=make-opensbi if needed" "CLEAN_LEVEL=${CLEAN_LEVEL}" "debug"
+	fi
 
-	# Handle overlays
-	local opensbidir
-	case "$USE_OVERLAYFS" in
-		"yes")
-			opensbidir="$(overlayfs_wrapper "wrap" "$SRC/cache/sources/$OPENSBISOURCEDIR" "opensbi_${LINUXFAMILY}_${BRANCH}")"
-			;;
-		*)
-			opensbidir="$SRC/cache/sources/$OPENSBISOURCEDIR"
-	esac
-
+	local opensbidir="$SRC/cache/sources/$OPENSBISOURCEDIR"
+	if [[ $USE_OVERLAYFS == yes ]]; then
+		opensbidir=$(overlayfs_wrapper "wrap" "$SRC/cache/sources/$OPENSBISOURCEDIR" "atf_${LINUXFAMILY}_${BRANCH}")
+	fi
 	cd "$opensbidir" || exit
 
 	display_alert "Compiling OpenSBI" "" "info"
 
-	# Manage toolchain
-	local toolchain
-	systemarch="$(dpkg --print-architecture)"
-	case "$systemarch" in
-		"amd64")
-			toolchain="$(find_toolchain "$OPENSBI_COMPILER" "$OPENSBI_USE_GCC")"
+	# build aarch64
+	if [[ $(dpkg --print-architecture) == amd64 ]]; then
 
-			[ -n "$toolchain" ] || exit_with_error "Could not find required toolchain" "${OPENSBI_COMPILER}gcc $OPENSBI_USE_GCC"
-			;;
-		*) exit_with_error "This architecture '$systemarch' is not implemented to build OpenSBI for board '$BOARD', fixme?"
-	esac
+		local toolchain
+		toolchain=$(find_toolchain "$OPENSBI_COMPILER" "$OPENSBI_USE_GCC")
+		[[ -z $toolchain ]] && exit_with_error "Could not find required toolchain" "${OPENSBI_COMPILER}gcc $OPENSBI_USE_GCC"
 
-	# FIXME-SECURITY(Krey): Eval is dangerous, rewrite to avoid using it or sanitize
+		if [[ -n $OPENSBI_TOOLCHAIN2 ]]; then
+			local toolchain2_type toolchain2_ver toolchain2
+			toolchain2_type=$(cut -d':' -f1 <<< "${OPENSBI_TOOLCHAIN2}")
+			toolchain2_ver=$(cut -d':' -f2 <<< "${OPENSBI_TOOLCHAIN2}")
+			toolchain2=$(find_toolchain "$toolchain2_type" "$toolchain2_ver")
+			[[ -z $toolchain2 ]] && exit_with_error "Could not find required toolchain" "${toolchain2_type}gcc $toolchain2_ver"
+		fi
+
+		# build aarch64
+	fi
+
+	# @FIXME: Eval is dangerous, rewrite to avoid using it or sanitize
 	display_alert "Compiler version" "${OPENSBI_COMPILER}gcc $(eval env PATH="$toolchain:$PATH" "${OPENSBI_COMPILER}gcc" -dumpfullversion -dumpversion)" "info"
 
 	# Patch handling
@@ -60,7 +59,7 @@ compile_opensbi() {
 	target_patchdir="$(cut -d';' -f2 <<< "$OPENSBI_TARGET_MAP")"
 	target_files="$(cut -d';' -f3 <<< "$OPENSBI_TARGET_MAP")"
 
-	advanced_patch "opensbi" "$OPENSBIPATCHDIR" "$BOARD" "$target_patchdir" "$BRANCH" "$LINUXFAMILY-$BOARD-$BRANCH"
+	advanced_patch "opensbi" "${OPENSBIPATCHDIR}" "$BOARD" "$target_patchdir" "$BRANCH" "${LINUXFAMILY}-${BOARD}-${BRANCH}"
 
 	# create patch for manual source changes
 	[ "$CREATE_PATCHES_OPENSBI" != yes ] || {
@@ -69,6 +68,7 @@ compile_opensbi() {
 	}
 
 	# Perform compilation
+	# @FIXME: Should we integrate '$target_make' here?
 	run_host_command_logged \
 		CCACHE_BASEDIR="$(pwd)" \
 		PATH="$toolchain:$PATH" \
@@ -86,7 +86,7 @@ compile_opensbi() {
 
 	# Finalize
 	opensbitempdir="$(mktemp -d)" # subject to TMPDIR/WORKDIR, so is protected by single/common error trapmanager to clean-up
-	chmod 700 "$opensbitempdir"
+	chmod 700 "${opensbitempdir}"
 
 	# copy files to temp directory
 	for f in $target_files; do
