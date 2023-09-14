b4296e01fbee4f19afb7bad546dc167eb67d332b
Jacob Hrbek <kreyren@rixotstudio.cz>
2023-09-14 02:51:37 +0200

    Revert vscode configuration
    This reverts commit febb196d483d554af4894234b90bc761f1e8916d.

--- shell.nix
+++ shell.nix
@@ -1,15 +1,8 @@
 # Configuration file for Nix(OS) to provide a temporary development enviroment
 
 { pkgs ? import <nixpkgs> {} }:
-let
-	unstable = import <nixpkgs> {};
-in
-	pkgs.mkShell {
-		name = "armbian-build";
-		buildInputs = with pkgs; [
-			bash # For evaluation
-			nil # Language server for nix
-			shellcheck # For linting shell
-			util-linux # Needed for uuidgen
-		];
-	}
+  pkgs.mkShell {
+    nativeBuildInputs = with pkgs.buildPackages; [
+      util-linux # Needed for uuidgen
+    ];
+}
