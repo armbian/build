67e48296b997aad11f5da5618145fc4761cd93c7
zhangyumingsh <492964829@qq.com>
2023-10-06 07:18:28 -0400

    generate armbianEnv.txt

--- lib/functions/image/partitioning.sh
+++ lib/functions/image/partitioning.sh
@@ -20,7 +20,7 @@ function prepare_partitions() {
 
 	# possible partition combinations
 	# /boot: none, ext4, ext2, fat (BOOTFS_TYPE)
-	# root: ext4, btrfs, f2fs, nfs (ROOTFS_TYPE)
+	# root: ext4, btrfs, f2fs, nilfs2, nfs (ROOTFS_TYPE)
 
 	# declare makes local variables by default if used inside a function
 	# NOTE: mountopts string should always start with comma if not empty
@@ -34,17 +34,16 @@ function prepare_partitions() {
 	parttype[fat]=fat16
 	parttype[f2fs]=ext4 # not a copy-paste error
 	parttype[btrfs]=btrfs
+	parttype[nilfs2]=nilfs2
 	parttype[xfs]=xfs
 	# parttype[nfs] is empty
 
-	# metadata_csum and 64bit may need to be disabled explicitly when migrating to newer supported host OS releases
-	if [[ $HOSTRELEASE =~ buster|bullseye|bookworm|sid|focal|impish|hirsute|jammy|kinetic|lunar|ulyana|ulyssa|uma|una|vanessa|vera ]]; then
-		mkopts[ext4]="-q -m 2 -O ^64bit,^metadata_csum"
-	fi
+	mkopts[ext4]="-q -m 2" # for a long time we had '-O ^64bit,^metadata_csum' here
 	# mkopts[fat] is empty
 	mkopts[ext2]=''
 	# mkopts[f2fs] is empty
 	mkopts[btrfs]='-m dup'
+	# mkopts[nilfs2] is empty
 	# mkopts[xfs] is empty
 	# mkopts[nfs] is empty
 
@@ -53,6 +52,7 @@ function prepare_partitions() {
 	mkopts_label[fat]='-n '
 	mkopts_label[f2fs]='-l '
 	mkopts_label[btrfs]='-L '
+	mkopts_label[nilfs2]='-L '
 	mkopts_label[xfs]='-L '
 	# mkopts_label[nfs] is empty
 
@@ -61,6 +61,7 @@ function prepare_partitions() {
 	mkfs[fat]=vfat
 	mkfs[f2fs]=f2fs
 	mkfs[btrfs]=btrfs
+	mkfs[nilfs2]=nilfs2
 	mkfs[xfs]=xfs
 	# mkfs[nfs] is empty
 
@@ -69,6 +70,7 @@ function prepare_partitions() {
 	# mountopts[fat] is empty
 	# mountopts[f2fs] is empty
 	mountopts[btrfs]=',commit=600'
+	# mountopts[nilfs2] is empty
 	# mountopts[xfs] is empty
 	# mountopts[nfs] is empty
 
@@ -231,10 +233,14 @@ function prepare_partitions() {
 
 	# stage: create fs, mount partitions, create fstab
 	rm -f $SDCARD/etc/fstab
+
+	declare root_part_uuid="uninitialized"
+
 	if [[ -n $rootpart ]]; then
 		local rootdevice="${LOOP}p${rootpart}"
 
 		if [[ $CRYPTROOT_ENABLE == yes ]]; then
+			check_loop_device "$rootdevice"
 			display_alert "Encrypting root partition with LUKS..." "cryptsetup luksFormat $rootdevice" ""
 			echo -n $CRYPTROOT_PASSPHRASE | cryptsetup luksFormat $CRYPTROOT_PARAMETERS $rootdevice -
 			echo -n $CRYPTROOT_PASSPHRASE | cryptsetup luksOpen $rootdevice $ROOT_MAPPER -
@@ -251,13 +257,19 @@ function prepare_partitions() {
 			local fscreateopt="-o compress-force=${BTRFS_COMPRESSION}"
 		fi
 		wait_for_disk_sync "after mkfs" # force writes to be really flushed
-		display_alert "Mounting rootfs" "$rootdevice"
+
+		# store in readonly global for usage in later hooks
+		root_part_uuid="$(blkid -s UUID -o value ${LOOP}p${rootpart})"
+		declare -g -r ROOT_PART_UUID="${root_part_uuid}"
+
+		display_alert "Mounting rootfs" "$rootdevice (UUID=${ROOT_PART_UUID})"
 		run_host_command_logged mount ${fscreateopt} $rootdevice $MOUNT/
+
 		# create fstab (and crypttab) entry
 		local rootfs
 		if [[ $CRYPTROOT_ENABLE == yes ]]; then
 			# map the LUKS container partition via its UUID to be the 'cryptroot' device
-			echo "$ROOT_MAPPER UUID=$(blkid -s UUID -o value ${LOOP}p${rootpart}) none luks" >> $SDCARD/etc/crypttab
+			echo "$ROOT_MAPPER UUID=${root_part_uuid} none luks" >> $SDCARD/etc/crypttab
 			rootfs=$rootdevice # used in fstab
 		else
 			rootfs="UUID=$(blkid -s UUID -o value $rootdevice)"
@@ -298,19 +310,13 @@ function prepare_partitions() {
 	if [[ -f $SDCARD/boot/armbianEnv.txt ]]; then
 		display_alert "Found armbianEnv.txt" "${SDCARD}/boot/armbianEnv.txt" "debug"
 		if [[ $CRYPTROOT_ENABLE == yes ]]; then
-			echo "rootdev=$rootdevice cryptdevice=UUID=$(blkid -s UUID -o value ${LOOP}p${rootpart}):$ROOT_MAPPER" >> "${SDCARD}/boot/armbianEnv.txt"
+			echo "rootdev=$rootdevice cryptdevice=UUID=${root_part_uuid}:$ROOT_MAPPER" >> "${SDCARD}/boot/armbianEnv.txt"
 		else
 			echo "rootdev=$rootfs" >> "${SDCARD}/boot/armbianEnv.txt"
 		fi
 		echo "rootfstype=$ROOTFS_TYPE" >> $SDCARD/boot/armbianEnv.txt
 	elif [[ $rootpart != 1 ]] && [[ $SRC_EXTLINUX != yes ]]; then
 		echo "rootfstype=$ROOTFS_TYPE" >> "${SDCARD}/boot/armbianEnv.txt"
-
-		call_extension_method "image_specific_armbian_env_ready" <<- 'IMAGE_SPECIFIC_ARMBIAN_ENV_READY'
-			*during image build, armbianEnv.txt is ready for image-specific customization (not in BSP)*
-			You can write to `"${SDCARD}/boot/armbianEnv.txt"` here, it is guaranteed to exist.
-		IMAGE_SPECIFIC_ARMBIAN_ENV_READY
-
 	elif [[ $rootpart != 1 && $SRC_EXTLINUX != yes && -f "${SDCARD}/boot/${bootscript_dst}" ]]; then
 		local bootscript_dst=${BOOTSCRIPT##*:}
 		sed -i 's/mmcblk0p1/mmcblk0p2/' $SDCARD/boot/$bootscript_dst
@@ -318,16 +324,83 @@ function prepare_partitions() {
 			-e "s/rootfstype \"ext4\"/rootfstype \"$ROOTFS_TYPE\"/" $SDCARD/boot/$bootscript_dst
 	fi
 	
+	echo "######hdmi display config######" >> "${SDCARD}/boot/armbianEnv.txt"
+	echo "overlays=hdmi dsi" >> "${SDCARD}/boot/armbianEnv.txt"
+	echo "" >> "${SDCARD}/boot/armbianEnv.txt"
+	
+	echo "######i2c1 config######" >> "${SDCARD}/boot/armbianEnv.txt"
+	echo "#overlays=i2c1" >> "${SDCARD}/boot/armbianEnv.txt"
+	echo "" >> "${SDCARD}/boot/armbianEnv.txt"
+	echo "######i2c3 config######" >> "${SDCARD}/boot/armbianEnv.txt"
+	echo "#overlays=i2c3" >> "${SDCARD}/boot/armbianEnv.txt"
+	echo "" >> "${SDCARD}/boot/armbianEnv.txt"
+	echo "######i2c4 config######" >> "${SDCARD}/boot/armbianEnv.txt"
+	echo "#overlays=i2c4" >> "${SDCARD}/boot/armbianEnv.txt"
+	echo "" >> "${SDCARD}/boot/armbianEnv.txt"
+	
+	echo "######mcp2515 config######" >> "${SDCARD}/boot/armbianEnv.txt"
+	echo "#overlays=mcp2515" >> "${SDCARD}/boot/armbianEnv.txt"
+	echo "" >> "${SDCARD}/boot/armbianEnv.txt"
+	
+	echo "######spi1 config######" >> "${SDCARD}/boot/armbianEnv.txt"
+	echo "#overlays=spi1" >> "${SDCARD}/boot/armbianEnv.txt"
+	echo "" >> "${SDCARD}/boot/armbianEnv.txt"
+	echo "######spi3 config######" >> "${SDCARD}/boot/armbianEnv.txt"
+	echo "#overlays=spi3" >> "${SDCARD}/boot/armbianEnv.txt"
+	echo "" >> "${SDCARD}/boot/armbianEnv.txt"
+	echo "######tft_35 config######" >> "${SDCARD}/boot/armbianEnv.txt"
+	echo "#overlays=tft_35" >> "${SDCARD}/boot/armbianEnv.txt"
+	echo "" >> "${SDCARD}/boot/armbianEnv.txt"
+	
+	echo "######uart0 config######" >> "${SDCARD}/boot/armbianEnv.txt"
+	echo "#overlays=uart0" >> "${SDCARD}/boot/armbianEnv.txt"
+	echo "" >> "${SDCARD}/boot/armbianEnv.txt"
+	echo "######uart1 config######" >> "${SDCARD}/boot/armbianEnv.txt"
+	echo "#overlays=uart1" >> "${SDCARD}/boot/armbianEnv.txt"
+	echo "" >> "${SDCARD}/boot/armbianEnv.txt"
+	echo "######uart3 config######" >> "${SDCARD}/boot/armbianEnv.txt"
+	echo "#overlays=uart3" >> "${SDCARD}/boot/armbianEnv.txt"
+	echo "" >> "${SDCARD}/boot/armbianEnv.txt"
+	echo "######uart5 config######" >> "${SDCARD}/boot/armbianEnv.txt"
+	echo "#overlays=uart5" >> "${SDCARD}/boot/armbianEnv.txt"
+	echo "" >> "${SDCARD}/boot/armbianEnv.txt"
+	echo "######uart7 config######" >> "${SDCARD}/boot/armbianEnv.txt"
+	echo "#overlays=uart7" >> "${SDCARD}/boot/armbianEnv.txt"
+	
+	echo "" >> "${SDCARD}/boot/armbianEnv.txt"
+	echo "######pwm1 config######" >> "${SDCARD}/boot/armbianEnv.txt"
+	echo "#overlays=pwm1" >> "${SDCARD}/boot/armbianEnv.txt"
+	echo "" >> "${SDCARD}/boot/armbianEnv.txt"
+	echo "######pwm2 config######" >> "${SDCARD}/boot/armbianEnv.txt"
+	echo "#overlays=pwm2" >> "${SDCARD}/boot/armbianEnv.txt"
+	echo "" >> "${SDCARD}/boot/armbianEnv.txt"
+	echo "######pwm4 config######" >> "${SDCARD}/boot/armbianEnv.txt"
+	echo "#overlays=pwm4" >> "${SDCARD}/boot/armbianEnv.txt"
+	echo "" >> "${SDCARD}/boot/armbianEnv.txt"
+	echo "######pwm5 config######" >> "${SDCARD}/boot/armbianEnv.txt"
+	echo "#overlays=pwm5" >> "${SDCARD}/boot/armbianEnv.txt"
+	echo "" >> "${SDCARD}/boot/armbianEnv.txt"
+	echo "######pwm12 config######" >> "${SDCARD}/boot/armbianEnv.txt"
+	echo "#overlays=pwm12" >> "${SDCARD}/boot/armbianEnv.txt"
+	echo "" >> "${SDCARD}/boot/armbianEnv.txt"
+	echo "######pwm14 config######" >> "${SDCARD}/boot/armbianEnv.txt"
+	echo "#overlays=pwm14" >> "${SDCARD}/boot/armbianEnv.txt"
+	echo "" >> "${SDCARD}/boot/armbianEnv.txt"
+	echo "######pwm15 config######" >> "${SDCARD}/boot/armbianEnv.txt"
+	echo "#overlays=pwm15" >> "${SDCARD}/boot/armbianEnv.txt"
+	
 	sudo cp -rf $SRC/lib/functions/rootfs/self_checking.sh     $SDCARD/usr
+	#sudo cp -rf $SRC/lib/functions/rootfs/BCM4345C5.hcd 	   $SDCARD/usr/lib
+	#sudo cp -rf $SRC/lib/functions/rootfs/brcm_patchram_plus1  $SDCARD/usr/lib
 	sudo cp -rf $SRC/lib/functions/rootfs/system.cfg           $SDCARD/boot
-	sudo cp -rf $SRC/lib/functions/rootfs/armbianEnv.txt       $SDCARD/boot
+	
 
 	# if we have boot.ini = remove armbianEnv.txt and add UUID there if enabled
 	if [[ -f $SDCARD/boot/boot.ini ]]; then
 		display_alert "Found boot.ini" "${SDCARD}/boot/boot.ini" "debug"
 		sed -i -e "s/rootfstype \"ext4\"/rootfstype \"$ROOTFS_TYPE\"/" $SDCARD/boot/boot.ini
 		if [[ $CRYPTROOT_ENABLE == yes ]]; then
-			rootpart="UUID=$(blkid -s UUID -o value ${LOOP}p${rootpart})"
+			rootpart="UUID=${root_part_uuid}"
 			sed -i 's/^setenv rootdev .*/setenv rootdev "\/dev\/mapper\/'$ROOT_MAPPER' cryptdevice='$rootpart':'$ROOT_MAPPER'"/' $SDCARD/boot/boot.ini
 		else
 			sed -i 's/^setenv rootdev .*/setenv rootdev "'$rootfs'"/' $SDCARD/boot/boot.ini
@@ -370,6 +443,13 @@ function prepare_partitions() {
 		echo "  append root=$rootfs $SRC_CMDLINE $MAIN_CMDLINE" >> $SDCARD/boot/extlinux/extlinux.conf
 		display_alert "extlinux.conf exists" "removing armbianEnv.txt" "info"
 		[[ -f $SDCARD/boot/armbianEnv.txt ]] && run_host_command_logged rm -v $SDCARD/boot/armbianEnv.txt
+	fi
+
+	if [[ $SRC_EXTLINUX != yes && -f $SDCARD/boot/armbianEnv.txt ]]; then
+		call_extension_method "image_specific_armbian_env_ready" <<- 'IMAGE_SPECIFIC_ARMBIAN_ENV_READY'
+			*during image build, armbianEnv.txt is ready for image-specific customization (not in BSP)*
+			You can write to `"${SDCARD}/boot/armbianEnv.txt"` here, it is guaranteed to exist.
+		IMAGE_SPECIFIC_ARMBIAN_ENV_READY
 	fi
 
 	return 0 # there is a shortcircuit above! very tricky btw!
