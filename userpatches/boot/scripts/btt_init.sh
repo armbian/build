d1b5629cfdd7bff2e426b1935853a5d756232991
zhangyumingsh <492964829@qq.com>
2023-11-21 22:32:28 -0500

    add btt_init.sh

---
+++ userpatches/boot/scripts/btt_init.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo chown biqu:biqu /home/biqu/ -R
+# sudo ntpdate stdtime.gov.hk
+sync
+
+cd /boot/scripts
+
+#./extend_fs.sh &
+
+./system_cfg.sh &
+
+./connect_wifi.sh &
+
+# regular sync to prevent data loss when direct power outage
+./sync.sh &
