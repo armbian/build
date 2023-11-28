cc0db9071c4b1d452d749229dc80af3b0ec048bb
Gunjan Gupta <viraniac@gmail.com>
2023-11-29 03:46:07 +0530

    freeze mainline kernel version

--- config/sources/vendors/microsoft/wsl2.hooks.sh
+++ config/sources/vendors/microsoft/wsl2.hooks.sh
@@ -10,14 +10,14 @@ function post_family_config__wsl2() {
 
 function post_family_config_branch_current__wsl2() {
 	declare -g KERNEL_MAJOR_MINOR="6.1"                                      # Major and minor versions of this kernel. For mainline caching.
-	declare -g KERNELBRANCH="branch:linux-6.1.y"                             # Branch or tag to build from. It should match MAJOR_MINOR
+	declare -g KERNELBRANCH="tag:v6.1.63"                             # Branch or tag to build from. It should match MAJOR_MINOR
 	declare -g KERNELPATCHDIR="archive/${LINUXFAMILY}-${KERNEL_MAJOR_MINOR}" # Microsoft patches
 	display_alert "Using mainline kernel ${KERNELBRANCH} for" "${BOARD}" "info"
 }
 
 function post_family_config_branch_edge__wsl2() {
 	declare -g KERNEL_MAJOR_MINOR="6.6"                                      # Major and minor versions of this kernel. For mainline caching.
-	declare -g KERNELBRANCH="branch:linux-6.6.y"                             # Branch or tag to build from. It should match MAJOR_MINOR
+	declare -g KERNELBRANCH="tag:v6.6.2"                             # Branch or tag to build from. It should match MAJOR_MINOR
 	declare -g KERNELPATCHDIR="archive/${LINUXFAMILY}-${KERNEL_MAJOR_MINOR}" # Microsoft patches
 	display_alert "Using mainline kernel ${KERNELBRANCH} for" "${BOARD}" "info"
 }
