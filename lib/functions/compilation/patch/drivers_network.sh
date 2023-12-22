e45e0e611cf63014b51d98e94afe3a3263781d0d
Gunjan Gupta <viraniac@gmail.com>
2023-12-22 21:54:58 +0530

    Do not set upper limit for uwe5622 driver as this can make the driver get lost during upgrade

--- lib/functions/compilation/patch/drivers_network.sh
+++ lib/functions/compilation/patch/drivers_network.sh
@@ -625,7 +625,7 @@ driver_rtl8822BS() {
 
 driver_uwe5622() {
 	# Unisoc uwe5622 wireless Support
-	if linux-version compare "${version}" ge 5.15 && linux-version compare "${version}" le 6.6 && [[ "$LINUXFAMILY" == sunxi* || "$LINUXFAMILY" == rockchip64 ]]; then
+	if linux-version compare "${version}" ge 5.15 && [[ "$LINUXFAMILY" == sunxi* || "$LINUXFAMILY" == rockchip64 ]]; then
 		display_alert "Adding" "Drivers for Unisoc uwe5622 found on some Allwinner and Rockchip boards" "info"
 
 		if linux-version compare "${version}" ge 6.3; then
