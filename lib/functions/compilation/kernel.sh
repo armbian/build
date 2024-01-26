c47c9372bf6970d6ca8c32bc5f7ec2a1416ab5bd
Gunjan Gupta <viraniac@gmail.com>
2024-01-26 13:10:11 +0530

    Khadas VIM1S & VIM4 - 5.15 kernel bump (#6201)
    * meson-s4t7: bump u-boot to khadas-vims-u-boot-2019.01-v1.6-release
    * Use khadas default bootargs as much as possible
    * Add new hook to allow copying code into kernel
    * meson-s4t7: legacy: Switch to 5.15 kernel
    * meson-s4t7: add kernel-config for 5.15 kernel
    * device tree overlays for 5.15 kernel for vim1s and vim4
    * restructure packaging of bsp files for vim1s/vim4
    * silence vblank warning on boot
    * Remove display workaround as it doesn't work with 5.15 kernel
    * Remove 5.4 kernel patches

--- lib/functions/compilation/kernel.sh
+++ lib/functions/compilation/kernel.sh
@@ -41,6 +41,12 @@ function compile_kernel() {
 	kernel_base_revision_date="$(LC_ALL=C date -d "@${kernel_base_revision_ts}")"
 	display_alert "Using Kernel git revision" "${kernel_git_revision} at '${kernel_base_revision_date}'"
 
+	# Call extension method to prepare extra sources
+	call_extension_method "kernel_copy_extra_sources" <<- 'ARMBIAN_KERNEL_SOURCES_EXTRA'
+		*Hook to copy extra kernel sources to the kernel under compilation*
+	ARMBIAN_KERNEL_SOURCES_EXTRA
+
+
 	# Possibly 'make clean'.
 	LOG_SECTION="kernel_maybe_clean" do_with_logging do_with_hooks kernel_maybe_clean
 
