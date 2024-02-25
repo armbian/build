ba98f6722966ac05bc4ecb44bc1d8b4d730db625
Igor Pecovnik <igor.pecovnik@gmail.com>
2024-02-25 18:53:54 +0100

    Switch kde-plasma desktop package to support sddm instead of lightdm

--- config/desktop/focal/environments/kde-plasma/armbian/create_desktop_package.sh
+++ config/desktop/focal/environments/kde-plasma/armbian/create_desktop_package.sh
@@ -1,21 +1,14 @@
-# install lightdm greeter
-cp -R "${SRC}"/packages/blobs/desktop/lightdm "${destination}"/etc/armbian
+# install sddm greeter theme
+mkdir -p "${destination}"/usr/share/sddm/themes
+cp -R "${SRC}"/packages/blobs/desktop/sddm/themes/plasma-chili/ "${destination}"/usr/share/sddm/themes
 
 # install default desktop settings
 mkdir -p "${destination}"/etc/skel
 cp -R "${SRC}"/packages/blobs/desktop/skel/. "${destination}"/etc/skel
 
-#install cinnamon desktop bar icons
-mkdir -p "${destination}"/usr/share/icons/armbian
-cp "${SRC}"/packages/blobs/desktop/desktop-icons/*.png "${destination}"/usr/share/icons/armbian
-
 # install wallpapers
 mkdir -p "${destination}"/usr/share/backgrounds/armbian/
 cp "${SRC}"/packages/blobs/desktop/desktop-wallpapers/*.jpg "${destination}"/usr/share/backgrounds/armbian
-
-# install wallpapers
-mkdir -p "${destination}"/usr/share/backgrounds/armbian-lightdm/
-cp "${SRC}"/packages/blobs/desktop/lightdm-wallpapers/*.jpg "${destination}"/usr/share/backgrounds/armbian-lightdm
 
 # install logo for login screen
 mkdir -p "${destination}"/usr/share/pixmaps/armbian
