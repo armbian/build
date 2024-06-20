427c04fb55d55d20cdd91b0add8687e39be85d05
ColorfulRhino <131405023+ColorfulRhino@users.noreply.github.com>
2024-06-20 16:31:49 +0200

    net: Use resolved for DNS resolution
    - Use resolved no matter what manages the network (networkd or NetworkManager)
    - Use resolved.conf.d/ directory to set DNS as recommended by resolved itself
    - In armbian-firstrun, remove config specific to mvebu64|mt7623 since this is now done by default

--- extensions/network/net-network-manager.sh
+++ extensions/network/net-network-manager.sh
@@ -19,7 +19,14 @@ function add_host_dependencies__install_network_manager() {
 function pre_install_kernel_debs__configure_network_manager() {
 	display_alert "Extension: ${EXTENSION}: Enabling Network-Manager" "" "info"
 
+	# Enable resolved
+	# Very likely not needed to enable manually since resolved is enabled by default
+	# NetworkManager can use systemd-resolved as a DNS resolver and cache.
+	# systemd-resolved will be used automatically if /etc/resolv.conf is a symlink to /run/systemd/resolve/stub-resolv.conf (source: https://wiki.archlinux.org/title/NetworkManager)
+	chroot_sdcard systemctl enable systemd-resolved.service || display_alert "Failed to enable systemd-resolved.service" "" "wrn"
+
 	# We can't disable/mask systemd-networkd.service since it is required by Netplan
+	# There shouldn't be any conflicts since Netplan is configured in such a way that NetworkManager manages all network devices
 
 	# Most likely we don't need to wait for nm to get online
 	chroot_sdcard systemctl disable NetworkManager-wait-online.service
