8ccec185bebe32cf225689487fe2515b12d16ee1
Igor Pecovnik <igor@armbian.com>
2024-06-22 12:34:28 +0200

    Replace add_packages_to_rootfs with add_packages_to_image as we don't want to make caches for each variant
    - also fixes a problem of generating caches all over again
    - change cache date scheme name to force rebuild them all

--- extensions/network/net-chrony.sh
+++ extensions/network/net-chrony.sh
@@ -2,6 +2,6 @@
 # Extension to manage network time synchronization with Chrony
 #
 function add_host_dependencies__install_chrony() {
-        display_alert "Extension: ${EXTENSION}: Installing additional packages" "chrony" "info"
-        add_packages_to_rootfs chrony
+	display_alert "Extension: ${EXTENSION}: Installing additional packages" "chrony" "info"
+	add_packages_to_image chrony
 }
