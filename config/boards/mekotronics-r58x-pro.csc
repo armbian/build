5b9f08712b4f1d97b5108363b0fe9ddebebee4ef
Ricardo Pardini <ricardo@pardini.net>
2024-07-07 23:07:36 +0200

    mekotronics: u-boot: join rockchip-rk3588's default `u-boot-radxa-rk35xx` scheme
    - rebased 2 patches
    - moved null patches for dts & defconfigs to `dt` / `defconfig` dirs
    - rename defconfigs since we're at it; use board-specific only
    - defconfigs re-saved

--- config/boards/mekotronics-r58x-pro.csc
+++ config/boards/mekotronics-r58x-pro.csc
@@ -3,11 +3,9 @@ declare -g BOARD_NAME="Mekotronics R58X-Pro"
 declare -g BOARDFAMILY="rockchip-rk3588"
 declare -g BOARD_MAINTAINER=""
 declare -g KERNEL_TARGET="vendor"
+declare -g BOOTCONFIG="mekotronics_r58x-rk3588_defconfig"                       # vendor u-boot; with NVMe and a DTS
 declare -g BOOT_FDT_FILE="rockchip/rk3588-blueberry-edge-v12-maizhuo-linux.dtb" # Specific to this board
 declare -g UEFI_EDK2_BOARD_ID="r58x"                                            # This _only_ used for uefi-edk2-rk3588 extension
 
 # Source vendor-specific configuration
 source "${SRC}/config/sources/vendors/mekotronics/mekotronics-rk3588.conf.sh"
-
-# Board-specific override
-declare -g BOOTCONFIG="rk3588_meko_r58x_defconfig" # specific, with nvme
--- config/sources/vendors/mekotronics/mekotronics-rk3588.hooks.sh
+++ config/sources/vendors/mekotronics/mekotronics-rk3588.hooks.sh
@@ -4,18 +4,9 @@
 # This file is a part of the Armbian Build Framework https://github.com/armbian/build/
 #
 
-# Vendor u-boot, standard rockchip, plus patches.
+# Vendor u-boot; use the default family (rockchip-rk3588) u-boot. See config/sources/families/rockchip-rk3588.conf
 function post_family_config__vendor_uboot_mekotronics() {
-	display_alert "$BOARD" "Configuring Mekotronics R58 ($BOARD) u-boot" "info"
-
-	declare -g BOOTSOURCE='https://github.com/radxa/u-boot.git'
-	#declare -g BOOTBRANCH='branch:next-dev' # disabled, using specific commit below to avoid breakage in the future
-	declare -g BOOTBRANCH="commit:609a77ef6e99c56aacd4b8d8f9c3056378f9c761" # specific commit in next-dev branch; tested to work
-
-	declare -g BOOTDIR="u-boot-meko-rk3588"             # do not share u-boot directory
-	declare -g BOOTPATCHDIR="legacy/u-boot-meko-rk3588" # Few patches in there; MAC address & defconfig
-
-	declare -g OVERLAY_PREFIX='rockchip-rk3588'
+	display_alert "$BOARD" "Configuring $BOARD vendor u-boot" "info"
 	declare -g BOOTDELAY=1 # build injects this into u-boot config. we can then get into UMS mode and avoid the whole rockusb/rkdeveloptool thing
 }
 
