da86909e3d6246221244ace8eb9e2adc937ecb0f
igorpecovnik <6281704+igorpecovnik@users.noreply.github.com>
2024-07-18 13:51:18 +0000

    tools: Bump `SHELLCHECK_VERSION` from 0.9.0 to 0.10.0

--- lib/functions/general/shellcheck.sh
+++ lib/functions/general/shellcheck.sh
@@ -49,7 +49,7 @@ function shellcheck_debian_control_scripts() {
 
 function run_tool_shellcheck() {
 	# Default version
-	SHELLCHECK_VERSION=${SHELLCHECK_VERSION:-0.9.0} # https://github.com/koalaman/shellcheck/releases
+	SHELLCHECK_VERSION=${SHELLCHECK_VERSION:-0.10.0} # https://github.com/koalaman/shellcheck/releases
 
 	declare non_cache_dir="/armbian-tools/shellcheck" # To deploy/reuse cached SHELLCHECK in a Docker image.
 
