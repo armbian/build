db0202ac37a0f9bb7e150cc1068a9765707ac222
Igor Pecovnik <igor@armbian.com>
2024-09-08 19:43:55 +0200

    `Userspace`: Switch armbian-config to new generation. It has very small footprint, thus it will be installed by default

--- lib/functions/rootfs/distro-agnostic.sh
+++ lib/functions/rootfs/distro-agnostic.sh
@@ -335,9 +335,7 @@ function install_distribution_agnostic() {
 
 	# install armbian-config
 	if [[ "${PACKAGE_LIST_RM}" != *armbian-config* ]]; then
-		if [[ $BUILD_MINIMAL != yes ]]; then
-			install_artifact_deb_chroot "armbian-config"
-		fi
+		install_artifact_deb_chroot "armbian-config"
 	fi
 
 	# install armbian-zsh
