armbian/build

commit db2b432d1897308391969456e6cfe5a6d73f9735

BSP: Expand distribution status with possible upgrade information

lib/functions/bsp/armbian-bsp-cli-deb.sh
@@ -172,10 +172,11 @@ function compile_armbian-bsp-cli() {
 		activate update-initramfs
 	EOF
 
-	# copy distribution support status # @TODO: why? this changes over time and will be out of date
+	# copy distribution support and upgrade status
+	# this information is used in motd to show status and within armbian-config to perform upgrades
 	local releases=($(find ${SRC}/config/distributions -mindepth 1 -maxdepth 1 -type d))
 	for i in "${releases[@]}"; do
-		echo "$(echo $i | sed 's/.*\///')=$(cat $i/support)" >> "${destination}"/etc/armbian-distribution-status
+		echo "$(echo $i | sed 's/.*\///')=$(cat $i/support)$(echo ";upgrade" | sed 's/.*\///')=$(cat $i/upgrade)" >> "${destination}"/etc/armbian-distribution-status
 	done
 
 	# execute $LINUXFAMILY-specific tweaks

config/distributions/bookworm/upgrade
@@ -1,0 +1,1 @@
+trixie,testing

config/distributions/bullseye/upgrade
@@ -1,0 +1,1 @@
+bookworm,trixie,testing

config/distributions/buster/upgrade
@@ -1,0 +1,1 @@
+bullseye,bookworm,trixie,testing

config/distributions/focal/upgrade
@@ -1,0 +1,1 @@
+jammy,noble,oracular

config/distributions/jammy/upgrade
@@ -1,0 +1,1 @@
+noble,oracular

config/distributions/noble/upgrade
@@ -1,0 +1,1 @@
+oracular

config/distributions/oracular/upgrade
@@ -1,0 +1,1 @@
+none

commit 2426c0f05bc9ff8ccb664c2706aba8a0a8e69ddd

armbian-bsp-cli-deb: fix shellcheck breakage; fixes #7303
- also, shellfmt

Fixes: db2b432d1897308391969456e6cfe5a6d73f9735

lib/functions/bsp/armbian-bsp-cli-deb.sh
@@ -176,9 +176,10 @@ function compile_armbian-bsp-cli() {
 
 	# copy distribution support and upgrade status
 	# this information is used in motd to show status and within armbian-config to perform upgrades
-	local releases=($(find ${SRC}/config/distributions -iname '*order*' -exec echo {} \; -exec cat {} \; | xargs -n2 -d'\n' | sort -nk2 | sed "s/\/order.*//g"))
+	declare -a releases=()
+	mapfile -t releases < <(for relorder in "${SRC}"/config/distributions/*/order; do echo "${relorder} $(xargs echo < "${relorder}")"; done | sort -nk2 | sed "s/\/order.*//g")
 	for i in "${releases[@]}"; do
-		echo "$(echo $i | sed 's/.*\///')=$(cat $i/support)$(echo ";upgrade" | sed 's/.*\///')=$(cat $i/upgrade)" >> "${destination}"/etc/armbian-distribution-status
+		echo "$(echo "$i" | sed 's/.*\///')=$(cat "$i"/support)$(echo ";upgrade" | sed 's/.*\///')=$(cat "$i"/upgrade)" >> "${destination}"/etc/armbian-distribution-status
 	done
 
 	# execute $LINUXFAMILY-specific tweaks
@@ -216,12 +217,12 @@ function compile_armbian-bsp-cli() {
 	# if freeze variable is removed, upgrade becomes possible again
 	if [[ "${BETA}" != "yes" ]]; then
 		for pin_variants in $(echo $KERNEL_UPGRADE_FREEZE | sed "s/,/ /g"); do
-		extracted_pins=(${pin_variants//@/ })
+			extracted_pins=(${pin_variants//@/ })
 			if [[ "${BRANCH}-${LINUXFAMILY}" == "${extracted_pins[0]}" ]]; then
 				cat <<- EOF >> "${destination}"/etc/apt/preferences.d/frozen-armbian
-				Package: linux-*-${extracted_pins[0]}
-				Pin: version ${extracted_pins[1]}
-				Pin-Priority: 999
+					Package: linux-*-${extracted_pins[0]}
+					Pin: version ${extracted_pins[1]}
+					Pin-Priority: 999
 				EOF
 			fi
 		done
@@ -455,7 +456,7 @@ function board_side_bsp_cli_postinst_finish() {
 	if [ ! -f "/etc/default/armbian-zram-config" ] && [ -f /etc/default/armbian-zram-config.dpkg-dist ]; then
 		mv /etc/default/armbian-zram-config.dpkg-dist /etc/default/armbian-zram-config
 	fi
-    if [ ! -f "/etc/default/armbian-firstrun" ]; then
+	if [ ! -f "/etc/default/armbian-firstrun" ]; then
 		mv /etc/default/armbian-firstrun.dpkg-dist /etc/default/armbian-firstrun
 	fi
 