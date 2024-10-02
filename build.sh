77a05f9c9358c0c1458eb27db6359da88ce52c4b
hexzhen3x7 <hexzhen3x7@codehunterz.world>
2024-10-02 09:13:55 +0200

    Added

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# build.sh
+
+echo -e "Console > Crafting!...::::..::.::.."
+
+
+./compile.sh BOARD=bananapim2ultra BRANCH=current RELEASE=jammy BUILD_MINIMAL=yes BUILD_DESKTOP=no ERNEL_CONFIGURE=no
