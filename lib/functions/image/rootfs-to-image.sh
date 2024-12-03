590b75fd6fd1683b89aa6faca505aeb202365be9
M.Schmidt <markus262@web.de>
2024-12-03 21:47:11 +0100

    improve(lvm+cryptroot extensions): enable possibility for LVM on LUKS

--- lib/functions/image/rootfs-to-image.sh
+++ lib/functions/image/rootfs-to-image.sh
@@ -117,9 +117,8 @@ function create_image_from_sdcard_rootfs() {
 	fi
 
 	wait_for_disk_sync "before umount MOUNT"
-
+	
 	umount_chroot_recursive "${MOUNT}" "MOUNT"
-	[[ $CRYPTROOT_ENABLE == yes ]] && cryptsetup luksClose "$CRYPTROOT_MAPPER"
 
 	call_extension_method "post_umount_final_image" "config_post_umount_final_image" <<- 'POST_UMOUNT_FINAL_IMAGE'
 		*allow config to hack into the image after the unmount*
