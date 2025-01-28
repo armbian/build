f98216d2c35dabde89efd899fcd801a7e4fff0ee
Andrew Davis <afd@ti.com>
2025-01-28 09:59:57 -0600

    K3: Update to latest U-Boot v2025.01
    Now that upstream U-Boot has support for building K3 boot artifacts
    we can drop using the ti-u-boot fork. Switch to v2025.01 and
    adjust file names and add new dependencies as needed.

--- config/bootscripts/boot-k3.cmd
+++ config/bootscripts/boot-k3.cmd
@@ -4,4 +4,4 @@ finduuid=part uuid \${boot} 1:2 uuid
 name_rd=uInitrd
 get_rd_mmc=load mmc ${bootpart} ${rdaddr} ${bootdir}/${name_rd}
 
-uenvcmd=run get_rd_${boot}; env set rd_spec ${rdaddr}:${filesize}
+uenvcmd=run get_rd_${boot}; env set rd_spec ${rdaddr}:${filesize}; setexpr fdtfile sub ti/ti ti; run bootcmd_ti_mmc
