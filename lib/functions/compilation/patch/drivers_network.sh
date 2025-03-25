47aa9a2967b635eb1074a742d41d935160544b3c
Ricardo Pardini <ricardo@pardini.net>
2025-03-25 09:33:49 -0300

    drivers_network: bump rtl88x2cs for 6.14.y
    - ref https://github.com/jethome-ru/rtl88x2cs/pull/13 (done, thanks adeepv)

--- lib/functions/compilation/patch/drivers_network.sh
+++ lib/functions/compilation/patch/drivers_network.sh
@@ -413,7 +413,7 @@ driver_rtl88x2cs() {
 	if linux-version compare "${version}" ge 5.9 && [[ "$LINUXFAMILY" == meson64 ]]; then
 
 		# Attach to specific commit (track branch:tune_for_jethub)
-		local rtl88x2csver='commit:e2fce3c61059d1977f0ae65e648db2720dcba5f5' # Commit date: Jan 13, 2025 (please update when updating commit ref)
+		local rtl88x2csver='commit:0986d5734e7630efea218dae9c16d11a10231819' # Commit date: Mar 25, 2025 (please update when updating commit ref)
 
 		display_alert "Adding" "Wireless drivers for Realtek 88x2cs chipsets ${rtl88x2csver}" "info"
 
