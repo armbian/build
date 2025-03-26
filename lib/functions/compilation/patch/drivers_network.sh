5b863be6b3298bf2fc6375ae83ef266e704cae2d
Paolo Sabatino <paolo.sabatino@gmail.com>
2025-03-26 19:15:27 +0100

    bump rockchip 32bit edge kernel to v6.14

--- lib/functions/compilation/patch/drivers_network.sh
+++ lib/functions/compilation/patch/drivers_network.sh
@@ -610,6 +610,11 @@ driver_rtl8723cs() {
 		process_patch_file "${SRC}/patch/misc/wireless-rtl8723cs/8723cs-Port-to-6.13.patch" "applying"
 	fi
 
+	if linux-version compare "${version}" ge 6.14; then
+		process_patch_file "${SRC}/patch/misc/wireless-rtl8723cs/8723cs-Port-to-6.14.patch" "applying"
+	fi
+
+
 }
 
 ###  The vendor's RTL8723DS driver is still required for RockPI-S support because
