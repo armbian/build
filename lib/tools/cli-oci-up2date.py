5d712c242aa679a1803e192721eb43830709c716
Ricardo Pardini <ricardo@pardini.net>
2025-04-12 09:28:47 +0200

    lib/tools: Python CLI for ORAS (bump to 0.2.y, api change, not too bad)

--- lib/tools/cli-oci-up2date.py
+++ lib/tools/cli-oci-up2date.py
@@ -21,7 +21,6 @@ log.info(f"OCI client version: {client.version()}")
 
 oci_target = "ghcr.io/armsurvivors/armbian-release/uboot-rockpro64-edge:2025.01-S6d41-P295c-Ha5a3-V9ecd-B1e5e-R448a"
 
-container = client.remote.get_container(oci_target)
-client.remote.load_configs(container)
-manifest = client.remote.get_manifest(container)
-log.debug(f"Got manifest for '{oci_target}'.")
+container = client.get_container(oci_target)
+manifest = client.get_manifest(container)
+log.debug(f"Got manifest for existing '{oci_target}'.")
