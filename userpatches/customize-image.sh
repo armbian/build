9beede9315e90a13757cecb99073196fea40a4bb
Robert Mordzon <robertmordzon@gmail.com>
2025-06-13 12:16:30 +0200

    Add rc.local file and service

--- userpatches/customize-image.sh
+++ userpatches/customize-image.sh
@@ -17,3 +17,38 @@ LINUXFAMILY=$2
 BOARD=$3
 BUILD_DESKTOP=$4
 
+# Path to a file indicating that the operations have already been executed
+INIT_FLAG_CUSTOMIZE_IMAGE_SH="/root/.customize-image.sh.firstrun.done"
+
+## CHECKS ###################################################################################
+# If the inode number for '/' differs from the real root, we are inside chroot
+if [ "$(stat -c %i /)" != "$(stat -c %i /proc/1/root/.)" ]; then
+    echo "Running in chroot environment."
+else
+    echo "Running on a regular host system."
+    exit 0
+fi
+
+# Check if the script has already been run
+if [ ! -f "$INIT_FLAG_CUSTOMIZE_IMAGE_SH" ]; then
+    echo "Running customize-image.sh in chroot for the first time."
+else
+    echo "One-time tasks have already been completed. Skipping customize-image.sh."
+    exit 0
+fi
+#--------------------------------------------------------------------------------------------
+
+## Misc #####################################################################################
+# ToDo: Setting hostname here doesn't work, probably overwritten later.
+# hostname -b "w3p-staking-1"   # Set the hostname to w3p-staking-1
+rm /root/.not_logged_in_yet     # Remove any first-login instructions
+chmod +x /etc/update-motd.d/*   # Enable motd
+#--------------------------------------------------------------------------------------------
+
+## rc.local ############################################################################################################
+# Add rc.local file and rc-local.service
+cp "/tmp/overlay/rc.local" /etc/rc.local
+chmod +x /etc/rc.local
+cp "/tmp/overlay/rc-local.service" /etc/systemd/system/rc-local.service
+systemctl enable rc-local.service
+#----------------------------------------------------------------------------------------------------------------------
