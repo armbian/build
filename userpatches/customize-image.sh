1a63bfc5b89fe2b4f3a687ea957b822834eb1280
Robert Mordzon <robertmordzon@gmail.com>
2025-06-13 11:40:16 +0200

    Add basic configuration for Web3 Pi Staking OS

---
+++ userpatches/customize-image.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# arguments: $RELEASE $LINUXFAMILY $BOARD $BUILD_DESKTOP
+#
+# This is the image customization script
+
+# NOTE: It is copied to /tmp directory inside the image
+# and executed there inside chroot environment
+# so don't reference any files that are not already installed
+
+# NOTE: If you want to transfer files between chroot and host
+# userpatches/overlay directory on host is bind-mounted to /tmp/overlay in chroot
+# The sd card's root path is accessible via $SDCARD variable.
+
+RELEASE=$1
+LINUXFAMILY=$2
+BOARD=$3
+BUILD_DESKTOP=$4
+
