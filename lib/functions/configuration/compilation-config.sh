12e9c44e7a5eab205d565e768a5780ab68686b0a
Andrei Aldea <andrei1998@gmail.com>
2025-07-02 22:28:42 +0300

    Add option for number of threads used by build system
    Attempt to fix https://github.com/armbian/build/issues/6907

--- lib/functions/configuration/compilation-config.sh
+++ lib/functions/configuration/compilation-config.sh
@@ -23,12 +23,36 @@ function prepare_compilation_vars() {
 
 	# moved from config: this does not belong in configuration. it's a compilation thing.
 	# optimize build time with 100% CPU usage
+	# Decide thread count - casual = 50%, normal = 100%, extreme = 150%
+	# For legacy reasons - "yes" == extreme == default, "no" == 1 thread
 	CPUS=$(grep -c 'processor' /proc/cpuinfo)
-	if [[ $USEALLCORES != no ]]; then
-		CTHREADS="-j$((CPUS + CPUS / 2))"
-	else
-		CTHREADS="-j1"
-	fi
+
+	case "$USEALLCORES" in
+    	casual)
+        	CTHREADS="-j$(( (CPUS + 1) / 2 ))"
+        	;;
+    	normal)
+        	CTHREADS="-j$CPUS"
+        	;;
+    	extreme|yes|"")
+        	CTHREADS="-j$((CPUS + CPUS / 2))"
+        	;;
+    	no)
+        	CTHREADS="-j1"
+        	;;
+    	*[!0-9]*)
+        	echo "Invalid USEALLCORES: $USEALLCORES. Use 'no', 'casual', 'normal', 'extreme', or a number." >&2
+        	CTHREADS="-j1"
+        	;;
+    	*)
+		if (( USEALLCORES > 0 )); then
+    		CTHREADS="-j$USEALLCORES"
+		else
+    	echo "Invalid USEALLCORES: $USEALLCORES. Must be a positive integer." >&2
+    		CTHREADS="-j1"
+		fi
+	esac
+
 
 	call_extension_method "post_determine_cthreads" "config_post_determine_cthreads" <<- 'POST_DETERMINE_CTHREADS'
 		*give config a chance modify CTHREADS programatically. A build server may work better with hyperthreads-1 for example.*
@@ -37,6 +61,9 @@ function prepare_compilation_vars() {
 
 	# readonly, global
 	declare -g -r CTHREADS="${CTHREADS}"
+
+	# Debug output
+	echo "Using $CTHREADS threads for parallel jobs"
 
 	return 0
 }
