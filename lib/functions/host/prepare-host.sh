f72bd55b12090913c14d92566f379d7805bc6796
Rolf Leggewie <foss@leggewie.org>
2025-07-20 10:03:38 +0800

    lib / prepare-host: make installation of gcc-arm-linux-gnueabi conditional
    * do not install gcc-arm-linux-gnueabi unless targetting arm64 arch
      suggested by coderabbitAI (https://github.com/armbian/build/pull/8260#discussion_r2118608963)
      reviewed and implemented by leggewie

--- lib/functions/host/prepare-host.sh
+++ lib/functions/host/prepare-host.sh
@@ -203,7 +203,6 @@ function adaptative_prepare_host_dependencies() {
 		udev # causes initramfs rebuild, but is usually pre-installed.
 		uuid-dev
 		zlib1g-dev
-		gcc-arm-linux-gnueabi # necessary for rockchip64 (and maybe other too) ATF compilation
 
 		# by-category below
 		file tree expect                         # logging utilities; expect is needed for 'unbuffer' command
@@ -256,7 +255,9 @@ function adaptative_prepare_host_dependencies() {
 	fi
 
 	if [[ "${wanted_arch}" == "arm64" || "${wanted_arch}" == "all" ]]; then
-		host_dependencies+=("gcc-aarch64-linux-gnu") # from crossbuild-essential-arm64
+		# gcc-aarch64-linux-gnu: from crossbuild-essential-arm64
+		# gcc-arm-linux-gnueabi: necessary for rockchip64 (and maybe other too) ATF compilation
+		host_dependencies+=("gcc-aarch64-linux-gnu" "gcc-arm-linux-gnueabi")
 	fi
 
 	if [[ "${wanted_arch}" == "armhf" || "${wanted_arch}" == "all" ]]; then
