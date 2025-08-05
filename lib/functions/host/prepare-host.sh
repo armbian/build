ca5e61f7774b8de9af640802ff9ce46c7931c12a
Jianfeng Liu <liujianfeng1994@gmail.com>
2025-08-05 10:10:44 +0800

    fix build dep gcc-loongarch64-linux-gnu introduced by new arch loong64

--- lib/functions/host/prepare-host.sh
+++ lib/functions/host/prepare-host.sh
@@ -269,7 +269,7 @@ function adaptative_prepare_host_dependencies() {
 		host_dependencies+=("debian-archive-keyring")
 	fi
 
-	if [[ "${wanted_arch}" == "loong64" || "${wanted_arch}" == "all" ]]; then
+	if [[ "${wanted_arch}" == "loong64" ]]; then
 		host_dependencies+=("gcc-loongarch64-linux-gnu") # crossbuild-essential-loongarch64 is not even available "yet"
 		host_dependencies+=("debian-ports-archive-keyring")
 	fi
