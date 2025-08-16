c5f47143f6baaf9c9966c513b6ca1df056e009e4
Rolf Leggewie <foss@leggewie.org>
2025-08-16 21:00:39 +0800

    extensions: make handling of INSTALL_RECOMMENDS in APA more robust

--- extensions/apa.sh
+++ extensions/apa.sh
@@ -11,8 +11,8 @@ function custom_apt_repo__add_apa() {
 
 function post_armbian_repo_customize_image__install_from_apa() {
 	# do not install armbian recommends for minimal images
-	[[ "${BUILD_MINIMAL,,}" =~ ^(true|yes)$ ]] && INSTALL_RECOMMENDS="no" || INSTALL_RECOMMENDS="yes"
-	chroot_sdcard_apt_get --install-recommends=$INSTALL_RECOMMENDS install "armbian-common armbian-bsp"
+	[[ "${BUILD_MINIMAL,,}" =~ ^(true|yes)$ ]] && INSTALL_RECOMMENDS="no-install-recommends" || INSTALL_RECOMMENDS="install-recommends"
+	chroot_sdcard_apt_get --$INSTALL_RECOMMENDS install "armbian-common armbian-bsp"
 
 	# install desktop environmnent if requested
 	case ${DESKTOP_ENVIRONMENT^^} in
