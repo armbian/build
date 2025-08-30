2b166f37f319d632e5687563d02c6d506d55c1b3
retro98boy <retro98boy@qq.com>
2025-08-30 09:40:31 +0800

    meson64: bump kernel to 6.16

--- lib/functions/compilation/patch/drivers_network.sh
+++ lib/functions/compilation/patch/drivers_network.sh
@@ -430,7 +430,7 @@ driver_rtl88x2cs() {
 	if linux-version compare "${version}" ge 5.9 && [[ "$LINUXFAMILY" == meson64 ]]; then
 
 		# Attach to specific commit (track branch:tune_for_jethub)
-		local rtl88x2csver='commit:d019d700aaceb74559be2809dd015ce7e6957fb5' # Commit date: May 15, 2025 (please update when updating commit ref)
+		local rtl88x2csver='commit:0ef9ddd619d2a386df90fd7c32b65958b0d675ed' # Commit date: Aug 30, 2025 (please update when updating commit ref)
 
 		display_alert "Adding" "Wireless drivers for Realtek 88x2cs chipsets ${rtl88x2csver}" "info"
 
