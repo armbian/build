67774770a315e7c6eca53754953e0ca28040c4b4
tabris <tabris@tabris.net>
2025-12-13 18:31:24 -0500

    set +e will break exit_with_error, so refactor

--- lib/functions/general/schedule_deletion.sh
+++ lib/functions/general/schedule_deletion.sh
@@ -14,19 +14,30 @@ function schedule_deletion() {
 }
 
 function pre_umount_final_image__schedule_deletion_delete_now() {
-	local file source func line message deletion_info
+	local file source func line deletion_info
+	local had_errexit=0
+	case $- in *e*) had_errexit=1;; esac
 	for deletion_info in "${schedule_deletion_files_to_delete[@]}"; do
 		IFS=';' read -r file source func line <<< "$deletion_info"
-		message="scheduled from $source $func $line"
+		local reason="scheduled from $source $func line#$line"
+		local message=""
+		local failure=0
 
-		set +e # don't bail out, let us bail out ourselves more verbosely
+		[[ $had_errexit ]] && set +e # don't bail out, let us bail out ourselves more verbosely
 		if [[ ! -e "$file" ]]; then
-			exit_with_error "FILE DELETION FAILED (missing): '${file}'; ${message}"
+			message="FILE DELETION FAILED (missing): '${file}'"
+			((failure++))
 		elif rm -- "$file"; then
-			display_alert "deleted file '${file}'" "$message" "info"
+			message="deleted file '${file}'"
 		else
-			exit_with_error "FILE DELETION FAILED: '${file}'; ${message}"
+			message="FILE DELETION FAILED: '${file}'"
+			((failure++))
 		fi
-		set -e # restore the previous behaviour
+		[[ $had_errexit ]] && set -e # restore the previous behaviour
+		if [[ $failure ]]; then
+			exit_with_error "$message; $reason"
+		else
+			display_alert "$message" "$reason" "info"
+		fi
 	done
 }
