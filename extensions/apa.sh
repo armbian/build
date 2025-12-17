0e606d64cd65c30feec78db8a224d933903ba3cd
tabris <tabris@tabris.net>
2025-12-17 15:29:18 -0500

    extensions/apa: new hook rootfs_customize_package_list

--- extensions/apa.sh
+++ extensions/apa.sh
@@ -11,7 +11,7 @@ function custom_apt_repo__add_apa() {
 
 # this variable is a temporary hack, remove as soon as it's not needed
 declare -g apa_additional_packages="libpam-systemd dbus-user-session curl iw less locales"
-function post_debootstrap_install_additional_packages__install_from_apa_stage1() { #FIXME: we need a better hook that fits into the extensions system
+function post_debootstrap_install_additional_packages__install_from_apa_stage1() { #FIXME: we need a better hook name that fits into the extensions system
 	[[ $APA_IS_ACTIVE ]] || return 0
 
 	# do not install armbian recommends for minimal images
@@ -27,6 +27,29 @@ function post_debootstrap_install_additional_packages__install_from_apa_stage1()
 			chroot_sdcard_apt_get install --install-recommends=yes "armbian-desktop-${DESKTOP_ENVIRONMENT,,}"
 			;;
 	esac
+}
+
+#FIXME: still need a better name, this implies it is possible to delete pkgs
+# NOTE: this is only used for hashing, so it probably isn't that important
+# to delete pkgs from any lists as much as indicate they won't be present.
+# NOTE: /tmp/rootfs_customize_package_list.txt is a special file that all
+# callers of this hook need to append to, as there is no mechanism for
+# returning data from hooks.
+function rootfs_customize_package_list__get_apa_packages() { 
+	[[ $APA_IS_ACTIVE ]] || return 0
+
+	local apa_package_list=""
+
+	# do not install armbian recommends for minimal images
+	[[ "${BUILD_MINIMAL,,}" =~ ^(true|yes)$ ]] && INSTALL_RECOMMENDS="no-install-recommends" || INSTALL_RECOMMENDS="install-recommends"
+	apa_package_list+= "${INSTALL_RECOMMENDS} armbian-common $apa_additional_packages"
+
+	case ${DESKTOP_ENVIRONMENT^^} in
+		XFCE|KDE|GNOME)
+			apa_package_list+= "armbian-desktop-${DESKTOP_ENVIRONMENT,,}"
+			;;
+	esac
+	echo $apa_package_list >> "/tmp/rootfs_customize_package_list.txt"
 }
 
 function post_armbian_repo_customize_image__install_from_apa_stage2() {
