47cd24f4ca23118d92d74d807efb8cce3676c03b
Ricardo Pardini <ricardo@pardini.net>
2026-01-10 02:16:08 +0100

    drop `UBOOT_USE_GCC`, `KERNEL_USE_GCC`, `ATF_USE_GCC` completely
    - also `CRUST_USE_GCC`
    - those don't serve any purpose and cause confusion

--- lib/functions/configuration/main-config.sh
+++ lib/functions/configuration/main-config.sh
@@ -358,10 +358,6 @@ function do_main_configuration() {
 }
 
 function do_extra_configuration() {
-	[[ -n $ATFSOURCE && -z $ATF_USE_GCC ]] && exit_with_error "Error in configuration: ATF_USE_GCC is unset"
-	[[ -z $UBOOT_USE_GCC ]] && exit_with_error "Error in configuration: UBOOT_USE_GCC is unset"
-	[[ -z $KERNEL_USE_GCC ]] && exit_with_error "Error in configuration: KERNEL_USE_GCC is unset"
-
 	declare BOOTCONFIG_VAR_NAME="BOOTCONFIG_${BRANCH^^}" # Branch name, uppercase
 	BOOTCONFIG_VAR_NAME=${BOOTCONFIG_VAR_NAME//-/_}      # Replace dashes with underscores
 	[[ -n ${!BOOTCONFIG_VAR_NAME} ]] && BOOTCONFIG=${!BOOTCONFIG_VAR_NAME}
