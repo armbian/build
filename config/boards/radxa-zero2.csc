armbian/build

commit 332e43bc8c2be6fba0a4c1b499b30a6525d45903

radxa-zero2: u-boot: bump to v2026.01; make fancy

config/boards/radxa-zero2.csc
@@ -12,5 +12,54 @@ BOOT_LOGO="desktop"
 ASOUND_STATE="asound.state.radxa-zero2"
 BOOT_FDT_FILE="amlogic/meson-g12b-radxa-zero2.dtb"
 
-BOOTBRANCH_BOARD="tag:v2024.07"
-BOOTPATCHDIR="v2024.07"
+BOOTBRANCH_BOARD="tag:v2026.01"
+BOOTPATCHDIR="v2026.01"
+
+function post_config_uboot_target__radxa-zero2_fancy_uboot() {
+	display_alert "u-boot for ${BOARD}/${BRANCH}" "u-boot: enable preboot & flash user LED in preboot" "info"
+	run_host_command_logged scripts/config --enable CONFIG_USE_PREBOOT
+	run_host_command_logged scripts/config --set-str CONFIG_PREBOOT "'led green:status on; sleep 0.1; led green:status off;'" # double quotes required due to run_host_command_logged's quirks
+
+	display_alert "u-boot for ${BOARD}/${BRANCH}" "u-boot: enable EFI debugging commands" "info"
+	run_host_command_logged scripts/config --enable CMD_EFIDEBUG
+	run_host_command_logged scripts/config --enable CMD_NVEDIT_EFI
+
+	display_alert "u-boot for ${BOARD}/${BRANCH}" "u-boot: enable more filesystems support" "info"
+	run_host_command_logged scripts/config --enable CONFIG_CMD_BTRFS
+
+	display_alert "u-boot for ${BOARD}/${BRANCH}" "u-boot: enable more compression support" "info"
+	run_host_command_logged scripts/config --enable CONFIG_LZO
+	run_host_command_logged scripts/config --enable CONFIG_BZIP2
+	run_host_command_logged scripts/config --enable CONFIG_ZSTD
+
+	display_alert "u-boot for ${BOARD}/${BRANCH}" "u-boot: enable gpio LED support" "info"
+	run_host_command_logged scripts/config --enable CONFIG_LED
+	run_host_command_logged scripts/config --enable CONFIG_LED_GPIO
+
+	display_alert "u-boot for ${BOARD}/${BRANCH}" "u-boot: enable some USB ethernet drivers" "info"
+	run_host_command_logged scripts/config --enable CONFIG_USB_HOST_ETHER
+	run_host_command_logged scripts/config --enable CONFIG_USB_ETHER_ASIX
+	run_host_command_logged scripts/config --enable CONFIG_USB_ETHER_ASIX88179
+	run_host_command_logged scripts/config --enable CONFIG_USB_ETHER_MCS7830
+	run_host_command_logged scripts/config --enable CONFIG_USB_ETHER_RTL8152
+	run_host_command_logged scripts/config --enable CONFIG_USB_ETHER_SMSC95XX
+
+	display_alert "u-boot for ${BOARD}/${BRANCH}" "u-boot: enable networking cmds" "info"
+	run_host_command_logged scripts/config --enable CONFIG_CMD_NFS
+	run_host_command_logged scripts/config --enable CONFIG_CMD_WGET
+	run_host_command_logged scripts/config --enable CONFIG_CMD_DNS
+	run_host_command_logged scripts/config --enable CONFIG_PROT_TCP
+
+	display_alert "u-boot for ${BOARD}/${BRANCH}" "u-boot: enable LWIP (new networking stack)" "info"
+	run_host_command_logged scripts/config --enable CONFIG_CMD_MII
+	run_host_command_logged scripts/config --enable CONFIG_NET_LWIP
+
+	# UMS, RockUSB, gadget stuff
+	display_alert "u-boot for ${BOARD}/${BRANCH}" "u-boot: enable UMS/RockUSB gadget" "info"
+	declare -a enable_configs=("CONFIG_CMD_USB_MASS_STORAGE" "CONFIG_USB_GADGET" "USB_GADGET_DOWNLOAD" "CONFIG_USB_FUNCTION_ACM")
+	for config in "${enable_configs[@]}"; do
+		run_host_command_logged scripts/config --enable "${config}"
+	done
+	# Auto-enabled by the above, force off...
+	run_host_command_logged scripts/config --disable USB_FUNCTION_FASTBOOT
+}

patch/u-boot/v2024.07/board_radxa-zero2/meson64-boot-usb-nvme-scsi-first.patch
@@ -1,33 +1,0 @@
-From 0000000000000000000000000000000000000000 Mon Sep 17 00:00:00 2001
-From: Ricardo Pardini <ricardo@pardini.net>
-Date: Sun, 14 Jan 2024 13:44:58 +0100
-Subject: meson64: change `BOOT_TARGET_DEVICES` to try to boot USB, NVME and
- SCSI before SD, MMC, PXE, DHCP
-
----
- include/configs/meson64.h | 6 +++---
- 1 file changed, 3 insertions(+), 3 deletions(-)
-
-diff --git a/include/configs/meson64.h b/include/configs/meson64.h
-index 111111111111..222222222222 100644
---- a/include/configs/meson64.h
-+++ b/include/configs/meson64.h
-@@ -99,12 +99,12 @@
- #define BOOT_TARGET_DEVICES(func) \
- 	func(ROMUSB, romusb, na)  \
- 	func(USB_DFU, usbdfu, na)  \
--	func(MMC, mmc, 0) \
--	func(MMC, mmc, 1) \
--	func(MMC, mmc, 2) \
- 	BOOT_TARGET_DEVICES_USB(func) \
- 	BOOT_TARGET_NVME(func) \
- 	BOOT_TARGET_SCSI(func) \
-+	func(MMC, mmc, 0) \
-+	func(MMC, mmc, 1) \
-+	func(MMC, mmc, 2) \
- 	func(PXE, pxe, na) \
- 	func(DHCP, dhcp, na)
- #endif
--- 
-Armbian
-

patch/u-boot/v2026.01/board_radxa-zero2/zero2-boot-sd-usb-nvme.patch
@@ -1,0 +1,31 @@
+From 0000000000000000000000000000000000000000 Mon Sep 17 00:00:00 2001
+From: Ricardo Pardini <ricardo@pardini.net>
+Date: Sat, 15 Nov 2025 00:04:36 +0100
+Subject: zero2: boot order: SD -> USB -> eMMC -> PXE
+
+---
+ include/configs/meson64.h | 6 ++++--
+ 1 file changed, 4 insertions(+), 2 deletions(-)
+
+diff --git a/include/configs/meson64.h b/include/configs/meson64.h
+index f3275b37a51..85b06c0579d 100644
+--- a/include/configs/meson64.h
++++ b/include/configs/meson64.h
+@@ -119,10 +119,12 @@
+ #define BOOT_TARGET_DEVICES(func) \
+ 	func(ROMUSB, romusb, na)  \
+ 	func(USB_DFU, usbdfu, na)  \
+-	BOOT_TARGET_MMC(func) \
+-	BOOT_TARGET_DEVICES_USB(func) \
++	func(MMC, mmc, 1) \
+ 	BOOT_TARGET_NVME(func) \
++	BOOT_TARGET_DEVICES_USB(func) \
+ 	BOOT_TARGET_SCSI(func) \
++	func(MMC, mmc, 2) \
++	func(MMC, mmc, 0) \
+ 	BOOT_TARGET_PXE(func) \
+ 	BOOT_TARGET_DHCP(func)
+ #endif
+-- 
+Armbian
+

commit 6e2b784eed86eb6b0ba739182aa90f76fae4c7fb

radxa-zero2/khadas-vim3l: u-boot: disable LWIP
- LWIP has issues with TFTP (serverip/tftpserverip) on v2026.01
- Kwiboo is already sending fixes, few separate series; check v2026.04 one day
- for now just disable the 2 boards I had enabled it for and use the legacy network stack again
- Fixes: 332e43bc8c2be6fba0a4c1b499b30a6525d45903
- Fixes: d88d32b2485206ce0a29e5c5e0cbccd13bc4249d

config/boards/khadas-vim3l.conf
@@ -89,9 +89,10 @@ function post_config_uboot_target__extra_configs_for_khadas_vim3l() {
 	run_host_command_logged scripts/config --enable CONFIG_CMD_DNS
 	run_host_command_logged scripts/config --enable CONFIG_PROT_TCP
 
-	display_alert "u-boot for ${BOARD}/${BRANCH}" "u-boot: enable LWIP (new networking stack)" "info"
-	run_host_command_logged scripts/config --enable CONFIG_CMD_MII
-	run_host_command_logged scripts/config --enable CONFIG_NET_LWIP
+	# @TODO: disabled; v2026.01 lwip has a few issues; check after v2026.04 as Kwiboo is already at-it
+	#display_alert "u-boot for ${BOARD}/${BRANCH}" "u-boot: enable LWIP (new networking stack)" "info"
+	#run_host_command_logged scripts/config --enable CONFIG_CMD_MII
+	#run_host_command_logged scripts/config --enable CONFIG_NET_LWIP
 
 	# UMS, RockUSB, gadget stuff
 	display_alert "u-boot for ${BOARD}/${BRANCH}" "u-boot: enable UMS/RockUSB gadget" "info"

config/boards/radxa-zero2.csc
@@ -50,9 +50,10 @@ function post_config_uboot_target__radxa-zero2_fancy_uboot() {
 	run_host_command_logged scripts/config --enable CONFIG_CMD_DNS
 	run_host_command_logged scripts/config --enable CONFIG_PROT_TCP
 
-	display_alert "u-boot for ${BOARD}/${BRANCH}" "u-boot: enable LWIP (new networking stack)" "info"
-	run_host_command_logged scripts/config --enable CONFIG_CMD_MII
-	run_host_command_logged scripts/config --enable CONFIG_NET_LWIP
+	# @TODO: disabled; v2026.01 lwip has a few issues; check after v2026.04 as Kwiboo is already at-it
+	#display_alert "u-boot for ${BOARD}/${BRANCH}" "u-boot: enable LWIP (new networking stack)" "info"
+	#run_host_command_logged scripts/config --enable CONFIG_CMD_MII
+	#run_host_command_logged scripts/config --enable CONFIG_NET_LWIP
 
 	# UMS, RockUSB, gadget stuff
 	display_alert "u-boot for ${BOARD}/${BRANCH}" "u-boot: enable UMS/RockUSB gadget" "info"