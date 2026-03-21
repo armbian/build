2d540556f6266024ba83c2b43d55b5a5c3355bf5
HackingGate <i@hackinggate.com>
2026-03-21 15:53:43 +0900

    photonicat-pm: replace wget with curl for DKMS package download

--- extensions/photonicat-pm.sh
+++ extensions/photonicat-pm.sh
@@ -26,7 +26,7 @@ function post_install_kernel_debs__install_photonicat_pm_dkms_package() {
 		photonicat_pm_url=${ghproxy_header}${photonicat_pm_url}
 	fi
 	photonicat_pm_dkms_file_name=photonicat-pm-dkms_${debian_version}_all.deb
-	use_clean_environment="yes" chroot_sdcard "wget ${photonicat_pm_url} -P /tmp"
+	use_clean_environment="yes" chroot_sdcard "curl -fsSL -o /tmp/${photonicat_pm_dkms_file_name} '${photonicat_pm_url}'"
 	display_alert "Install photonicat-pm packages, will build kernel module in chroot" "${EXTENSION}" "info"
 	declare -ag if_error_find_files_sdcard=("/var/lib/dkms/photonicat-pm*/*/build/*.log")
 	use_clean_environment="yes" chroot_sdcard_apt_get_install "/tmp/${photonicat_pm_dkms_file_name}"
