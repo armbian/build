df6da70e7b2fa11b118e155f212d5b8fb0449e7a
Andrei Aldea <andrei1998@gmail.com>
2026-04-15 11:01:45 -0500

    board: add Beaglebadge support
    Adds initial Armbian standard build configuration and low-RAM optimizations for the TI AM62L-based BeagleBadge.
    
    Co-Authored-By: Jonas Wood <47963908+jonaswood01@users.noreply.github.com>

--- config/bootscripts/boot-k3.cmd
+++ config/bootscripts/boot-k3.cmd
@@ -1,7 +1,10 @@
 bootpart=1:1
 bootdir=
 finduuid=part uuid \${boot} 1:2 uuid
+check_beaglebadge=if test "${fdtfile}" = "ti/k3-am62l3-badge.dtb"; then rdaddr=0x84900000; name_overlays=ti/k3-am62l3-badge-eink-gdey042t81.dtbo; fi
 name_rd=uInitrd
 get_rd_mmc=load mmc ${bootpart} ${rdaddr} ${bootdir}/${name_rd}
 
-uenvcmd=run get_rd_${boot}; env set rd_spec ${rdaddr}:${filesize}; setexpr fdtfile sub ti/ti ti; run bootcmd_ti_mmc
+uenvcmd=run check_beaglebadge; run get_rd_${boot}; env set rd_spec ${rdaddr}:${filesize}; setexpr fdtfile sub ti/ti ti; run bootcmd_ti_mmc
+
+optargs=vt.global_cursor_default=0
