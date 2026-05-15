armbian/build

commit c47c9372bf6970d6ca8c32bc5f7ec2a1416ab5bd

Khadas VIM1S & VIM4 - 5.15 kernel bump (#6201)
* meson-s4t7: bump u-boot to khadas-vims-u-boot-2019.01-v1.6-release
* Use khadas default bootargs as much as possible
* Add new hook to allow copying code into kernel
* meson-s4t7: legacy: Switch to 5.15 kernel
* meson-s4t7: add kernel-config for 5.15 kernel
* device tree overlays for 5.15 kernel for vim1s and vim4
* restructure packaging of bsp files for vim1s/vim4
* silence vblank warning on boot
* Remove display workaround as it doesn't work with 5.15 kernel
* Remove 5.4 kernel patches

lib/functions/compilation/kernel.sh
@@ -41,6 +41,12 @@ function compile_kernel() {
 	kernel_base_revision_date="$(LC_ALL=C date -d "@${kernel_base_revision_ts}")"
 	display_alert "Using Kernel git revision" "${kernel_git_revision} at '${kernel_base_revision_date}'"
 
+	# Call extension method to prepare extra sources
+	call_extension_method "kernel_copy_extra_sources" <<- 'ARMBIAN_KERNEL_SOURCES_EXTRA'
+		*Hook to copy extra kernel sources to the kernel under compilation*
+	ARMBIAN_KERNEL_SOURCES_EXTRA
+
+
 	# Possibly 'make clean'.
 	LOG_SECTION="kernel_maybe_clean" do_with_logging do_with_hooks kernel_maybe_clean
 

config/boards/khadas-vim1s.conf
@@ -14,12 +14,6 @@ declare -g KHADAS_OOWOW_BOARD_ID="VIM1S" # for use with EXT=output-image-oowow
 
 OVERLAY_PREFIX='s4-s905y4'
 
-function image_specific_armbian_env_ready__set_vim1s_specific_parameters() {
-    display_alert "Writing vim1s parameters in /boot/armbianEnv.txt"
-    run_host_command_logged echo "earlycon=on" >>${SDCARD}/boot/armbianEnv.txt
-    run_host_command_logged echo "extraboardargs=meson-gx-mmc.caps2_quirks=mmc-hs400" >>${SDCARD}/boot/armbianEnv.txt
-}
-
 function post_family_tweaks_bsp__populate_etc_firmware() {
 	# The hciattach command needs firmware to be placed in /etc/firmware directory.
 	# Populate the same.

config/boards/khadas-vim4.conf
@@ -14,18 +14,6 @@ declare -g KHADAS_OOWOW_BOARD_ID="VIM4" # for use with EXT=output-image-oowow
 
 OVERLAY_PREFIX='t7-a311d2'
 
-# Add modprobe configuration for the ISP, see https://github.com/khadas/fenix/blob/master/archives/filesystem/special/VIM4/etc/modprobe.d/isp.conf
-# This avoids a kernel panic related to the ISP firmware; see https://armbian.atlassian.net/browse/AR-1801?focusedCommentId=11995
-function post_family_tweaks_bsp__kvim4_isp_modprobe() {
-	: "${destination:?}"
-	display_alert "${BOARD}" "Adding modprobe configuration for the ISP" "info"
-	mkdir -p "${destination}"/etc/modprobe.d
-	cat <<- EOD > "${destination}"/etc/modprobe.d/isp.conf
-		options iv009_isp dcam=2
-		softdep iv009_isp pre: iv009_isp_iq iv009_isp_lens iv009_isp_sensor
-	EOD
-}
-
 function post_family_tweaks_bsp__enable_fan_service() {
 	if [[ ${BRANCH} = "legacy" ]]; then
 		run_host_command_logged mkdir -p "${destination}"/etc/systemd/system/mutli-user.target.wants

config/bootscripts/boot-meson-s4t7.cmd
@@ -8,11 +8,8 @@ setenv console "both"
 setenv verbosity "1"
 setenv earlycon "off"
 setenv bootlogo "false"
-setenv earlyconuart "0xfe078000"
 setenv displaymode "1080p60hz"
 setenv force_16x9_display "false"
-
-if test "${board_name}" = "kvim1s"; then setenv earlyconuart "0xfe07a000"; fi
 
 # Show what uboot default fdtfile is
 echo "U-boot default fdtfile: ${fdtfile}"
@@ -26,34 +23,22 @@ if test "${console}" = "both"; then setenv console_serial "true"; setenv console
 if test "${console}" = "serial"; then setenv console_serial "true"; fi
 if test "${console}" = "display"; then setenv console_display "true"; fi
 
-if test "${console_serial}" = "true"; then setenv consoleargs "console=ttyS0,921600"; fi
-if test "${console_display}" = "true"; then setenv consoleargs "console=tty1 ${consoleargs}"; fi
+if test "${console_display}" = "true"; then setenv consoleargs "console=tty0"; fi
+if test "${console_serial}" = "true"; then setenv consoleargs "console=ttyS0,921600 ${consoleargs}"; fi
 
-if test "${earlycon}" = "on"; then setenv consoleargs "earlycon=aml-uart,${earlyconuart} ${consoleargs}"; fi
+if test "${earlycon}" != "on"; then
+	setexpr bootargs sub " earlycon=\\S* " " " "${bootargs}"
+fi
 
 if test "${bootlogo}" = "true"; then
-	setenv consoleargs "splash plymouth.ignore-serial-consoles ${consoleargs}"
+	setenv plymouthargs "splash plymouth.ignore-serial-consoles"
 else
-	setenv consoleargs "splash=verbose ${consoleargs}"
+	setenv plymouthargs "splash=verbose"
 fi
 
-if test -n "${hdmimode}" ; then
-	if test ${display_height} -ge 2160 ; then
-		setenv displaymode "2160p60hz"
-	elif test ${display_height} -ge 1080 ; then
-		setenv displaymode "1080p60hz"
-	else
-		setenv displaymode "720p60hz"
-	fi
-fi
+setexpr bootargs sub "rootfstype=\\S*" "rootfstype=${rootfstype}" "${bootargs}"
 
-if test "${force_16x9_display}" = "true"; then
-	setenv hdmimode ${displaymode}
-fi
-
-setenv displayargs "logo=${display_layer},loaded,${fb_addr} vout=${hdmimode},${vout_init} panel_type=${panel_type} hdmitx=${cecconfig},${colorattribute} hdmimode=${hdmimode} hdmichecksum=${hdmichecksum} dolby_vision_on=${dolby_vision_on} hdr_policy=${hdr_policy} hdr_priority=${hdr_priority} frac_rate_policy=${frac_rate_policy} hdmi_read_edid=${hdmi_read_edid} cvbsmode=${cvbsmode} osd_reverse=${osd_reverse} video_reverse=${video_reverse}"
-
-setenv bootargs "root=${rootdev} rootwait rootfstype=${rootfstype} ${consoleargs} no_console_suspend ${displayargs} loglevel=${verbosity} mac=${eth_mac} khadas.serial=${usid} partition_type=generic ${extraargs} ${extraboardargs}"
+setenv bootargs "root=${rootdev} ${bootargs} ${consoleargs} partition_type=generic loglevel=${verbosity} ${plymouthargs} ${extraargs} ${extraboardargs}"
 
 load ${devtype} ${devnum} ${fdt_addr_r} ${prefix}dtb/${fdtfile}
 fdt addr ${fdt_addr_r}

config/sources/families/meson-s4t7.conf
@@ -15,17 +15,19 @@ enable_extension "bluetooth-hciattach"                                       # E
 case $BRANCH in
 
 	legacy)
-		declare -g KERNEL_MAJOR_MINOR="5.4"                           # Major and minor versions of this kernel. For mainline caching.
+		declare -g KERNEL_MAJOR_MINOR="5.15"                           # Major and minor versions of this kernel. For mainline caching.
 		declare -g KERNELSOURCE="https://github.com/khadas/linux.git" # Khadas kernel
-		declare -g KERNELBRANCH="branch:khadas-vims-5.4.y"            # Branch or tag to build from. It should match MAJOR_MINOR
-		declare -g KERNELPATCHDIR='archive/meson-s4t7-5.4'
+		declare -g KERNELBRANCH="branch:khadas-vims-5.15.y"            # Branch or tag to build from. It should match MAJOR_MINOR
+		declare -g KERNELPATCHDIR="archive/meson-s4t7-5.15"
+		declare -g EXTRAWIFI=no
 		;;
 
 esac
 
 # Lets build uboot from source
 BOOTSOURCE="https://github.com/khadas/u-boot.git"
-BOOTBRANCH="tag:khadas-vims-u-boot-2019.01-v1.5.2-release"
+BOOTBRANCH="tag:khadas-vims-u-boot-2019.01-v1.6-release"
+BOOTPATCHDIR="u-boot-meson-s4t7"
 BOOTENV_FILE='meson.txt'
 BOOTSCRIPT="boot-meson-s4t7.cmd:boot.cmd"
 UBOOT_TARGET_MAP=";;build/u-boot.bin.sd.bin.signed:u-boot.bin.sd.bin.signed build/u-boot.bin.signed:u-boot.bin.signed"
@@ -42,6 +44,29 @@ function add_host_dependencies__khadas_fips_needs_xxd() {
 	declare -g EXTRA_BUILD_DEPS="${EXTRA_BUILD_DEPS} xxd"
 }
 
+function fetch_sources_tools__download_linaro_toolchain() {
+	display_alert "$BOARD" "preparing linaro toolchain for bootloader fip" "info"
+
+	if [[ "${HOSTARCH}" != "amd64" ]]; then
+		exit_with_error "vim1s/vim4 uboot requires amd64 system to build"
+	fi
+
+	declare tversion="7.3.1-2018.05"
+	declare tbasedir="${SRC}/cache/toolchain"
+	declare tdir="${tbasedir}/gcc-linaro-${tversion}-x86_64_aarch64-elf"
+	declare tfile="gcc-linaro-${tversion}-x86_64_aarch64-elf.tar.xz"
+	declare turl="https://dl.armbian.com/_toolchain/${tfile}"
+
+	if [[ ! -d "${tdir}" ]]; then
+		run_host_command_logged wget --progress=dot:giga -O "${tbasedir}/${tfile}" "${turl}"
+		run_host_command_logged tar -C "${tbasedir}" -xf "${tbasedir}/${tfile}"
+		run_host_command_logged rm -fv "${tbasedir}/${tfile}"
+	fi
+
+	# Symlinks are not enough: riscv-none-embed-ld: cannot find -lgcc, So we need to add the toolchain to the PATH
+	export PATH="${tdir}/bin:${PATH}"
+}
+
 # To be able to build the fip blobs, we need a riscv toolchain.
 function pre_config_uboot_target__prepare_riscv_toolchain() {
 	display_alert "$BOARD" "preparing riscv toolchain for bootloader fip" "info"
@@ -53,7 +78,7 @@ function pre_config_uboot_target__prepare_riscv_toolchain() {
 	declare tbasedir="${SRC}/cache/toolchain"
 	declare tdir="${tbasedir}/xpack-riscv-none-embed-gcc-${tversion}"
 	declare tfile="xpack-riscv-none-embed-gcc-${tversion}-linux-${tos}.tar.gz"
-	declare turl="https://github.com/xpack-dev-tools/riscv-none-embed-gcc-xpack/releases/download/v${tversion}/${tfile}"
+	declare turl="https://dl.armbian.com/_toolchain/${tfile}"
 
 	mkdir -p "${tbasedir}"
 
@@ -79,13 +104,38 @@ function post_uboot_custom_postprocess__build_fip() {
 	: "${KHADAS_BOARD_ID:?KHADAS_BOARD_ID not set}"
 	display_alert "$BOARD" "building fip" "info"
 	cd "${uboot_work_dir}" || exit_with_error "cray-cray about ${uboot_work_dir}"
+	export CROSS_COMPILE="aarch64-elf-"
 	run_host_command_logged bash fip/mk_script.sh "${KHADAS_BOARD_ID}" "${uboot_work_dir}"
 	return 0
+}
+
+function armbian_kernel_config__add_khadas_common_drivers() {
+	common_driver_source_loc="${SRC}/cache/sources/khadas_common_drivers/${KERNELBRANCH#*:}"
+	if [[ ! -f .config ]]; then
+		display_alert "$BOARD" "Fetching khadas common drivers" "info"
+		fetch_from_repo "https://github.com/khadas/common_drivers" "khadas_common_drivers" "${KERNELBRANCH}" "yes"
+		common_drivers_hash=$(git -C ${common_driver_source_loc} rev-parse HEAD)
+		# Hack to ensure that we will recreate kernel if common_drivers source changes
+		kernel_config_modifying_hashes+=( "khadas_common_drivers=${common_drivers_hash}" )
+	fi
+}
+
+function kernel_copy_extra_sources__khadas_common_drivers() {
+	display_alert "$BOARD" "Copying khadas common drivers" "info"
+	common_driver_source_loc="${SRC}/cache/sources/khadas_common_drivers/${KERNELBRANCH#*:}"
+	[[ -d common_drivers ]] && rm -rf common_drivers
+	mkdir common_drivers
+	cp -aR ${common_driver_source_loc}/* common_drivers/
 }
 
 function pre_package_kernel_image__copy_meson_s4t7_overlays() {
 	display_alert "Copy meson s4t7 overlays"
 	declare dtbo_destination="${tmp_kernel_install_dirs[INSTALL_DTBS_PATH]}"/amlogic
+
+	# the directory structure is changed between 5.4 and 5.15 kernel
+	# Lets make it consistent
+	run_host_command_logged mkdir -p ${dtbo_destination}
+	run_host_command_logged mv "${tmp_kernel_install_dirs[INSTALL_DTBS_PATH]}"/*.dtb ${dtbo_destination}/
 
 	run_host_command_logged cp -r ${SRC}/patch/kernel/${KERNELPATCHDIR}/overlay ${dtbo_destination}/
 
@@ -99,24 +149,14 @@ function post_family_tweaks_bsp__disable_uinitrd_generation() {
 	fi
 }
 
-function meson_s4t7_change_initrd_compression() {
-	sed -i 's/COMPRESS=.*/COMPRESS=xz/g' /etc/initramfs-tools/initramfs.conf
-}
-
-function post_family_tweaks_bsp__add_postinst_function() {
-	postinst_functions+=('meson_s4t7_change_initrd_compression')
-}
-
-function image_specific_armbian_env_ready__force_16x9_display() {
-	if [[ ${BRANCH} == "legacy" ]]; then
-		display_alert "Forcing 16x9 display"
-		run_host_command_logged echo "force_16x9_display=true" >> ${SDCARD}/boot/armbianEnv.txt
-	fi
+function image_specific_armbian_env_ready__enable_earlycon() {
+    display_alert "Enabling earlycon"
+    run_host_command_logged echo "earlycon=on" >>${SDCARD}/boot/armbianEnv.txt
 }
 
 function post_family_tweaks_bsp__add_fan_service() {
 	if [[ ${BRANCH} = "legacy" ]]; then
-		run_host_command_logged mkdir -p "${destination}"/etc/systemd/system
-		run_host_command_logged cp "${SRC}"/packages/bsp/meson-s4t7/fan.service "${destination}"/etc/systemd/system
+		run_host_command_logged cp -R "${SRC}"/packages/bsp/meson-s4t7/common/* "${destination}"/
+		run_host_command_logged cp -R "${SRC}"/packages/bsp/meson-s4t7/${BOARD}/* "${destination}"/
 	fi
 }

lib/functions/artifacts/artifact-kernel.sh
@@ -143,7 +143,7 @@ function artifact_kernel_prepare_version() {
 	declare var_config_hash_short="${vars_config_hash:0:${short_hash_size}}"
 
 	# Hash the extension hooks
-	declare -a extension_hooks_to_hash=("pre_package_kernel_image")
+	declare -a extension_hooks_to_hash=("pre_package_kernel_image" "kernel_copy_extra_sources")
 	declare -a extension_hooks_hashed=("$(dump_extension_method_sources_functions "${extension_hooks_to_hash[@]}")")
 	declare hash_hooks="undetermined"
 	hash_hooks="$(echo "${extension_hooks_hashed[@]}" | sha256sum | cut -d' ' -f1)"

packages/bsp/meson-s4t7/common/etc/modprobe.d/dhd.conf
@@ -1,0 +1,1 @@
+options dhd firmware_path=./brcm/ nvram_path=./brcm/

commit 9a4e580051c97c2dc4684fa22c79b4232fba3fe0

kernel: run `kernel_copy_extra_sources` hooks after `kernel_maybe_clean`
- otherwise changes done to kernel dir go away if cleaning is requested,
  which has been forcing family and board code to use unrelated hooks to
  workaround it, which in turn requires causes CONFIG_DEFS_ONLY=yes hacks
  which then causes hashes to differ from actual build runs
- with this, now any source-level changes (incl generating patches) can
  now be done using `kernel_copy_extra_sources`; make sure to include
  all hashing-related code and references (eg: source SHA1 from external
  repos) into that hook, so changes there cause a change to the `-HK`
- Fixes: c47c9372bf6970d6ca8c32bc5f7ec2a1416ab5bd "Add new hook to allow copying code into kernel"

lib/functions/compilation/kernel.sh
@@ -41,13 +41,13 @@ function compile_kernel() {
 	kernel_base_revision_date="$(LC_ALL=C date -d "@${kernel_base_revision_ts}")"
 	display_alert "Using Kernel git revision" "${kernel_git_revision} at '${kernel_base_revision_date}'"
 
+	# Possibly 'make clean'.
+	LOG_SECTION="kernel_maybe_clean" do_with_logging do_with_hooks kernel_maybe_clean
+
 	# Call extension method to prepare extra sources
 	call_extension_method "kernel_copy_extra_sources" <<- 'ARMBIAN_KERNEL_SOURCES_EXTRA'
 		*Hook to copy extra kernel sources to the kernel under compilation*
 	ARMBIAN_KERNEL_SOURCES_EXTRA
-
-	# Possibly 'make clean'.
-	LOG_SECTION="kernel_maybe_clean" do_with_logging do_with_hooks kernel_maybe_clean
 
 	# Patching.
 	declare hash pre_patch_version