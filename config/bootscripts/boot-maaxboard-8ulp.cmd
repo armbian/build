d8be0f239ba95e18c39443819061265b6b5c3224
Govind Singh <govind.sk85@gmail.com>
2026-06-16 13:22:38 +0400

    imx8ulp: reduce ATF log level
    Set ATF_LOG_LEVEL=20 to silence BL31 SCMI/upower INFO spam, honor it in
    compile_atf, and hash ATF vars in uboot artifacts. Use quiet loglevel=4
    in uEnv and lower the kernel default console loglevel to 4.

--- config/bootscripts/boot-maaxboard-8ulp.cmd
+++ config/bootscripts/boot-maaxboard-8ulp.cmd
@@ -5,13 +5,14 @@ fdt_addr_r=0x84000000
 fdt_addr=0x84000000
 ramdisk_addr_r=0x85000000
 initrd=initrd
-verbosity=4
-console=ttyLP1,115200 earlycon console=tty1
+# Avnet uEnv pattern: optargs is appended by U-Boot mmcargs
+console=ttyLP1,115200 console=tty1
+optargs=quiet loglevel=4
 mmcdev=0
 mmcpart=1
 loadkernel=fatload mmc ${mmcdev}:${mmcpart} ${loadaddr} Image
 loadfdt=fatload mmc ${mmcdev}:${mmcpart} ${fdt_addr_r} ${fdt_file}
 loadinitrd=fatload mmc ${mmcdev}:${mmcpart} ${ramdisk_addr_r} ${initrd}
 setuproot=part uuid mmc ${mmcdev}:2 uuid; setenv mmcroot PARTUUID=${uuid}
-mmcargs=setenv bootargs console=${console} root=${mmcroot} rootwait rw loglevel=${verbosity}
+mmcargs=setenv bootargs console=${console} root=${mmcroot} rootwait rw ${optargs}
 boot_os=run setuproot; run mmcargs; run loadkernel; run loadfdt; run loadinitrd; booti ${loadaddr} ${ramdisk_addr_r}:${filesize} ${fdt_addr_r}
