646bd522574b3575b338fff1628ee15f4824d6d0
Igor Pecovnik <igor@armbian.com>
2026-08-08 18:48:58 +0200

    applications-kali: drop commented-out VENDOR line
    Signed-off-by: Igor Pecovnik <igor@armbian.com>

--- extensions/applications-kali.sh
+++ extensions/applications-kali.sh
@@ -1,6 +1,5 @@
 function extension_prepare_config__kali() {
 	EXTRA_IMAGE_SUFFIXES+=("-kali") # global array
-	#VENDOR="Armbian_Security"
 	HOST="${HOST:-armbian-security}"
 	display_alert "Target image will have Kali repository preinstalled" "${BOARD}:${RELEASE}-${BRANCH} :: ${EXTENSION}" "info"
 }
